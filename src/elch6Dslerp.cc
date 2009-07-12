8586904098b29c871b0283cbe17e9de6478cca7c
Jochen Sprickerhof <git@jochen.sprickerhof.de>
2009-07-12 17:00:58 +0000

    new (correct) delta distribution
    git-svn-id: svn://svn.code.sf.net/p/slam6d/code/trunk@21 51f1e6e7-7289-49ad-8748-f09e43fc6daa

--- src/elch6Dslerp.cc
+++ src/elch6Dslerp.cc
@@ -60,16 +60,18 @@ void elch6Dslerp::close_loop(const vector <Scan *> &allScans, int first, int las
   }
   Scan *start = new Scan(meta_start, false);
 
+  //static size of metascan
   int offset_last_start = 2;
   int offset_last_end = 0;
+
   vector <Scan *> meta_end;
   for(int i = last - offset_last_start; i <= last + offset_last_end && i < n; i++) {
     meta_end.push_back(allScans[i]);
   }
   Scan *end = new Scan(meta_end, false);
 
-  double p1[16];
-  memcpy(p1, allScans[last]->get_transMat(), 16 * sizeof(double));
+  double Pl0[16];
+  memcpy(Pl0, allScans[last]->get_transMat(), 16 * sizeof(double));
 
   my_icp6D->match(start, end);
 
@@ -77,25 +79,21 @@ void elch6Dslerp::close_loop(const vector <Scan *> &allScans, int first, int las
   delete end;
 
   // store ICP
-  double p1P[16];
-  memcpy(p1P, allScans[last]->get_transMat(), 16 * sizeof(double));
+  double Pp0[16];
+  memcpy(Pp0, allScans[last]->get_transMat(), 16 * sizeof(double));
 
-  // Move Origin
-  double p1P_inv[16];
-  M4inv(p1P, p1P_inv);
-  for(int i = 0; i < n; i++) {
-    allScans[i]->transform(p1P_inv, Scan::INVALID, -1);
-  }
+  //compute Delta
+  double Pf0[16], Pf0_inv[16], tmp1[16], tmp2[16], deltaf[16];
+  memcpy(Pf0, allScans[first]->get_transMat(), 16 * sizeof(double));
+  M4inv(Pf0, Pf0_inv);
 
-  //cout << "0 == " << allScans[last]->get_transMat() << endl;
-
-  double p1N[16];
-  MMult(p1P_inv, p1, p1N);
-  double deltaM[16];
-  M4inv(p1N, deltaM);
+  MMult(Pf0_inv, Pl0, tmp1);
+  M4inv(tmp1, tmp2);
+  MMult(Pp0, tmp2, tmp1);
+  MMult(Pf0_inv, tmp1, deltaf);
 
   double deltaT[3], deltaQ[4];
-  Matrix4ToQuat(deltaM, deltaQ, deltaT);
+  Matrix4ToQuat(deltaf, deltaQ, deltaT);
 
   if(!quiet) {
     double axisangle[4];
@@ -106,30 +104,31 @@ void elch6Dslerp::close_loop(const vector <Scan *> &allScans, int first, int las
     QuatToAA(axisangle);
     cout << deltaT[0] << " " << deltaT[1] << " " << deltaT[2] << " " << axisangle[0] << " " << axisangle[1] << " " << axisangle[2] << " " << axisangle[3] << endl;
   }
+  
+  //transform scans
+  double idQ[4] = {1, 0, 0, 0}, rPos[3], rPosQuat[4], delta0[16];
 
-  double rPos[3], rPosQuat[4], idM[16], idQ[4] = {1, 0, 0, 0};
-  M4identity(idM);
+  rPos[0] = deltaT[0] * weights[0][0];
+  rPos[1] = deltaT[1] * weights[1][0];
+  rPos[2] = deltaT[2] * weights[2][0];
+  slerp(idQ, deltaQ, weights[3][0], rPosQuat);
+  QuatToMatrix4(rPosQuat, rPos, tmp1);
+  M4inv(tmp1, tmp2);
+  MMult(Pf0, tmp2, delta0);
 
-  for(int i = 0; i < n; i++) {
-
-    rPos[0] = deltaT[0] * weights[0][i];
-    rPos[1] = deltaT[1] * weights[1][i];
-    rPos[2] = deltaT[2] * weights[2][i];
-    slerp(idQ, deltaQ, weights[3][i], rPosQuat);
-
-    //cout << weights[0][i] << " " << weights[1][i] << " " << weights[2][i] << " " << weights[3][i] << endl;
-
-    if(i >= last - offset_last_start && i <= last + offset_last_end && i < n) {
-      allScans[i]->transform(idM, Scan::INVALID, -1);
-    } else {
-      allScans[i]->transform(rPosQuat, rPos, Scan::INVALID, -1);
-    }
-  }
-
-  // Move Origin
-  allScans[0]->transform(p1P, Scan::INVALID, -1);
   for(int i = 1; i < n; i++) {
-    allScans[i]->transform(p1P, Scan::LUM, i == n-1 ? 2 : 1);
+    if( i >= last - offset_last_start && i <= last + offset_last_end) {
+      MMult(delta0, Pf0_inv, tmp1);
+    } else {
+      rPos[0] = deltaT[0] * weights[0][i];
+      rPos[1] = deltaT[1] * weights[1][i];
+      rPos[2] = deltaT[2] * weights[2][i];
+      slerp(idQ, deltaQ, weights[3][i], rPosQuat);
+      QuatToMatrix4(rPosQuat, rPos, tmp1);
+      MMult(delta0, tmp1, tmp2);
+      MMult(tmp2, Pf0_inv, tmp1);
+    }
+    allScans[i]->transform(tmp1, Scan::LUM, i == n-1 ? 2 : 1);
   }
 
   for(int i = 0; i < 4; i++) {
