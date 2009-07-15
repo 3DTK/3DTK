384f9c6eaecbfa0b3ed559b6095f3317cb554ff6
jspricke <jspricke@51f1e6e7-7289-49ad-8748-f09e43fc6daa>
2009-07-15 08:35:58 +0000

    python script to convert .frames into the new format converted frames in the dat directory
    git-svn-id: https://svn.code.sf.net/p/slam6d/code/trunk@29 51f1e6e7-7289-49ad-8748-f09e43fc6daa

---
+++ bin/convert.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+#
+# converts all .frames files in a directory into the new format
+
+import os, sys
+
+def main():
+  if len(sys.argv) != 2:
+    print 'converts all .frames files in a directory into the new format'
+    print 'run with %s directory' % sys.argv[0]
+    return
+  directory = sys.argv[1]
+  for name in os.listdir(directory):
+    fullpath = os.path.join(directory, name)
+    if os.path.isfile(fullpath) and fullpath.endswith('.frames'):
+      frame = file(fullpath).read()
+      frame = frame.replace('\n\n', '\n')
+      frame = frame.replace('\n-1 0 0 1\n', '0\n')
+      frame = frame.replace('\n0 0 1 1\n', '1\n')
+      frame = frame.replace('\n1 1 0 1\n', '2\n')
+      frame = frame.replace('\n1 0 0 1\n', '3\n')
+      file(fullpath, 'w').write(frame)
+
+if __name__ == '__main__':
+  main()
