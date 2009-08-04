f3ea5869c4eba032450752993beb34819f64a84a
Jochen Sprickerhof <git@jochen.sprickerhof.de>
2009-08-04 12:26:55 +0000

    New scan IO combining UOS_MAP and UOS_FRAMES
    git-svn-id: svn://svn.code.sf.net/p/slam6d/code/trunk@37 51f1e6e7-7289-49ad-8748-f09e43fc6daa

--- src/show/show.cc
+++ src/show/show.cc
@@ -193,7 +193,7 @@ void usage(char* prog)
 	  << "  -m NR   set the maximal range distance to NR 'units' (unit of scan data, e.g. cm)" << endl
 	  << bold << "  -f" << normal << " F, " << bold << "--format=" << normal << "F" << endl
 	  << "         using shared library F for input" << endl
-	  << "         (chose F from {uos, uos_map, uos_frames, old, rts, rts_map, ifp, riegl, zahn, ply})" << endl << endl
+	  << "         (chose F from {uos, uos_map, uos_frames, uos_map_frames, old, rts, rts_map, ifp, riegl, zahn, ply})" << endl << endl
 	  << endl;
   exit(1);
 }
@@ -250,6 +250,7 @@ int parseArgs(int argc,char **argv, string &dir, int& start, int& end, int& maxD
 	   if (strcasecmp(optarg, "uos") == 0) type = UOS;
 	   else if (strcasecmp(optarg, "uos_map") == 0) type = UOS_MAP;
 	   else if (strcasecmp(optarg, "uos_frames") == 0) type = UOS_FRAMES;
+	   else if (strcasecmp(optarg, "uos_map_frames") == 0) type = UOS_MAP_FRAMES;
 	   else if (strcasecmp(optarg, "old") == 0) type = OLD;
 	   else if (strcasecmp(optarg, "rts") == 0) type = RTS;
 	   else if (strcasecmp(optarg, "rts_map") == 0) type = RTS_MAP;
@@ -366,7 +367,7 @@ void readFrames(string dir, int start, int end, bool readInitial, reader_type &t
 
     MetaMatrix.push_back(Matrices);
 
-    if((type == UOS_MAP || type == RTS_MAP) && fileCounter == start+1) {
+    if((type == UOS_MAP || type == UOS_MAP_FRAMES || type == RTS_MAP) && fileCounter == start+1) {
       MetaAlgoType.push_back(algoTypes);
       MetaMatrix.push_back(Matrices);
     }
