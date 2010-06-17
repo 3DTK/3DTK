50675d7f8bdd7cb141b3a76ae3707a58c9f29abb
jelseberg <jelseberg@51f1e6e7-7289-49ad-8748-f09e43fc6daa>
2010-06-17 15:49:28 +0000

    fixed small bug in the rendering
    git-svn-id: svn+ssh://svn.code.sf.net/p/slam6d/code/trunk@175 51f1e6e7-7289-49ad-8748-f09e43fc6daa

--- src/show/show_Boctree.cc
+++ src/show/show_Boctree.cc
@@ -7,7 +7,7 @@
 
 #include "show_Boctree.h"
 #include "../globals.icc"
-
+#include <omp.h>
 #include "viewcull.h"
 
 #if __GNUC__ > 3 || (__GNUC__ == 3 && __GNUC_MINOR__ >= 4)
@@ -76,6 +76,7 @@ void Show_BOctTree::displayOctTreeAllCulled(bitoct &node, double *center, double
   
   if (res == 2) { // if entirely within frustrum discontinue culling
     displayOctTreeAllCulled(node, center, size);
+    return;
   }
 
   double ccenter[3];
@@ -106,13 +107,56 @@ void Show_BOctTree::displayOctTreeAllCulled(bitoct &node, double *center, double
   }
 
 }
-
 /**
  * Displays octree. Tries to display only targetpts points, but will usually display four times as much
  *
  */
-void Show_BOctTree::displayOctTree(long targetpts, bitoct &node, double *center, double size) {
+void Show_BOctTree::displayOctTree(long targetpts, bitoct &node) {
   if (targetpts <= 0) return; // no need to display anything
+
+  bitunion *children;
+  bitoct::getChildren(node, children);
+
+  for (short i = 0; i < 8; i++) {
+    if (  ( 1 << i ) & node.valid ) {   // if ith node exists
+      if (  ( 1 << i ) & node.leaf ) {   // if ith node is leaf get center
+        pointrep *points = children->points;
+        unsigned int length = points[0].length;
+        double *point = &(points[1].v);  // first point
+        glBegin(GL_POINTS);
+        if (length <= targetpts) {        // more points requested than possible, plot all
+          for(unsigned int iterator = 0; iterator < length; iterator++ ) {
+            glVertex3f( point[0], point[1], point[2]);
+            point+=3;
+          }
+        } else {                         // select points to show
+          // TODO smarter subselection of points here
+          int each = 3 * (length/targetpts);
+          for(unsigned int iterator = 0; iterator < targetpts; iterator++ ) {
+            glVertex3f( point[0], point[1], point[2]);
+            point += each;
+          }
+        }
+        glEnd();
+      } else { // recurse
+        displayOctTree(targetpts/POPCOUNT(node.valid), children->node);
+      }
+      ++children; // next child
+    }
+  }
+}
+
+
+void Show_BOctTree::displayOctTreeCulled(long targetpts, bitoct &node, double *center, double size) {
+  if (targetpts <= 0) return; // no need to display anything
+  
+  int res = QuadInFrustrum2(center[0], center[1], center[2], size, size, size);
+  if (res==0) return;  // culled do not continue with this branch of the tree
+  
+  if (res == 2) { // if entirely within frustrum discontinue culling
+    displayOctTree(targetpts, node);
+    return;
+  }
 
   double ccenter[3];
   bitunion *children;
@@ -120,6 +164,7 @@ void Show_BOctTree::displayOctTree(long targetpts, bitoct &node, double *center,
 
   for (short i = 0; i < 8; i++) {
     if (  ( 1 << i ) & node.valid ) {   // if ith node exists
+      childcenter(center, ccenter, size, i);  // childrens center
       if (  ( 1 << i ) & node.leaf ) {   // if ith node is leaf get center
         // check if leaf is visible
         if ( QuadInFrustrum(ccenter[0], ccenter[1], ccenter[2], size, size, size) ) {
@@ -144,56 +189,10 @@ void Show_BOctTree::displayOctTree(long targetpts, bitoct &node, double *center,
         }
 
       } else { // recurse
-        childcenter(center, ccenter, size, i);  // childrens center
-        displayOctTree(targetpts/POPCOUNT(node.valid), children->node, ccenter, size/2.0);
-      }
-      ++children; // next child
-    }
-  }
-}
-
-
-void Show_BOctTree::displayOctTreeCulled(long targetpts, bitoct &node, double *center, double size) {
-  if (targetpts <= 0) return; // no need to display anything
-  
-  int res = QuadInFrustrum2(center[0], center[1], center[2], size, size, size);
-  if (res==0) return;  // culled do not continue with this branch of the tree
-  
-  if (res == 2) { // if entirely within frustrum discontinue culling
-    displayOctTree(targetpts, node, center, size);
-  }
-
-  double ccenter[3];
-  bitunion *children;
-  bitoct::getChildren(node, children);
-
-  for (short i = 0; i < 8; i++) {
-    if (  ( 1 << i ) & node.valid ) {   // if ith node exists
-      childcenter(center, ccenter, size, i);  // childrens center
-      if (  ( 1 << i ) & node.leaf ) {   // if ith node is leaf get center
-        pointrep *points = children->points;
-        unsigned int length = points[0].length;
-        double *point = &(points[1].v);  // first point
-        glBegin(GL_POINTS);
-        if (length <= targetpts) {        // more points requested than possible, plot all
-          for(unsigned int iterator = 0; iterator < length; iterator++ ) {
-            glVertex3f( point[0], point[1], point[2]);
-            point+=3;
-          }
-        } else {                         // select points to show
-          // TODO smarter subselection of points here
-          int each = 3 * (length/targetpts);
-          for(unsigned int iterator = 0; iterator < targetpts; iterator++ ) {
-            glVertex3f( point[0], point[1], point[2]);
-            point += each;
-          }
-        }
-        glEnd();
-
-      } else { // recurse
         displayOctTreeCulled(targetpts/POPCOUNT(node.valid), children->node, ccenter, size/2.0);
       }
       ++children; // next child
     }
   }
 }
+
