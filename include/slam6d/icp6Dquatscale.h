6961624322cf45f2eff314ed2243f7d086dff539
Andreas Nüchter <nuechter@users.sourceforge.net>
2011-03-28 07:06:39 +0000

    small fixes
    git-svn-id: svn://svn.code.sf.net/p/slam6d/code/trunk@397 51f1e6e7-7289-49ad-8748-f09e43fc6daa

--- include/slam6d/icp6Dquatscale.h
+++ include/slam6d/icp6Dquatscale.h
@@ -1,23 +1,22 @@
 /** @file 
  *  @brief Definition of the ICP error function minimization
- *  @author Kai Lingemann. Institute of Computer Science, University of Osnabrueck, Germany.
- *  @author Andreas Nuechter. Institute of Computer Science, University of Osnabrueck, Germany.
- *  @author Flavia Grosan, Alex Tandrau. Jacobs University Bremen, Germany.
+ *  @author Flavia Grosan, Alex Tandrau. Jacobs University Bremen gGmbH, Germany.
+ *  @author Andreas Nuechter. Jacobs University Bremen gGmbH, Germany.
  */
 
 #ifndef __ICP6DQUATSCALE_H__
 #define __ICP6DQUATSCALE_H__
 
-#include "icp6Dminimizer.h"
+#include "icp6Dquat.h"
 
 /**
  * @brief Implementation of the ICP error function minimization via quaternions
  */
-class icp6D_QUAT_SCALE : public icp6Dminimizer
+class icp6D_QUAT_SCALE : public icp6D_QUAT
 {
 public:
   /** constructor */
-  icp6D_QUAT_SCALE(bool quiet = false) : icp6Dminimizer(quiet) {};
+  icp6D_QUAT_SCALE(bool quiet = false) : icp6D_QUAT(quiet) {};
   /** destructor */
   virtual ~icp6D_QUAT_SCALE() {};                                  
 
@@ -25,13 +24,6 @@ public:
 					  const double centroid_m[3], const double centroid_d[3]);
   inline int getAlgorithmID() { return 9; }; 
 
-private:
-  void   quaternion2matrix(double *q, double m[3][3]);
-  int    ferrari(double a, double b, double c, double d, double rts[4]);
-  int    qudrtc(double b, double c, double rts[4]);
-  double cubic(double p, double q, double r);
-  void   maxEigenVector(double Q[4][4], double ev[4]);  
-  void   characteristicPol(double Q[4][4], double c[4]);    
 };
 
 #endif
