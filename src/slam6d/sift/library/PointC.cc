1300b888800dd4983db763a6a4926f242b3127b1
hhoushiar <hhoushiar@51f1e6e7-7289-49ad-8748-f09e43fc6daa>
2011-06-07 22:40:15 +0000

    adding the PointC.cc file left over from last commit
    git-svn-id: svn+ssh://svn.code.sf.net/p/slam6d/code/trunk@451 51f1e6e7-7289-49ad-8748-f09e43fc6daa

---
+++ src/slam6d/sift/library/PointC.cc
@@ -0,0 +1,12 @@
+/**
+ * @file PointC.cc
+ * @brief 
+ * @author Andreas Nuechter. Jacobs University Bremen gGmbH, Grmany.
+ * @author Darko Makreshanski. Jacobs University Bremen gGmbH, Germany.
+ */
+#include "PointC.h"
+
+PointC::~PointC() 
+{
+	// TODO Auto-generated destructor stub
+}
