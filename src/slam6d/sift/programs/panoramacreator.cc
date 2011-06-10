2525d5f1fe08f0e2895688455591264372b2803b
hhoushiar <hhoushiar@51f1e6e7-7289-49ad-8748-f09e43fc6daa>
2011-06-10 14:09:03 +0000

    improve the sift programs Argument parsing - part1
    git-svn-id: https://svn.code.sf.net/p/slam6d/code/trunk@455 51f1e6e7-7289-49ad-8748-f09e43fc6daa

--- src/slam6d/sift/programs/panoramacreator.cc
+++ src/slam6d/sift/programs/panoramacreator.cc
@@ -57,25 +57,13 @@ void usage(int argc, char** argv)
 }
 
 /**
- *   main - reads the inputs and create the panorama images and map file
+ *   parseArgs - reade the comand line options
  */
-int main(int argc, char** argv)
-{  
-  char *reader = NULL;
-  int method;
-  char *projection = NULL;
-  bool writeMaps = false;
-  bool writeRangeImage = false;
-  bool writeRefelectanceJpeg = false;
-  char *input = NULL;
-  vector<struct size> sizes;
+int parseArgs(int argc, char **argv, string &reader, int &method, string &projection, bool &writeMaps, bool &writeRangeImage, bool &writeRefelectanceJpeg, string &input,vector<struct size> &sizes, double &d, int &n, double &r)
+{
   int c; 
   int width;
   int height;
-  double d = 1;
-  int n = 4;
-  double r = 1;
-  
   opterr = 0;
   //reade the command line and get the options
   while ((c = getopt (argc, argv, "r:mjdR:v:n:s:p:")) != -1)
@@ -136,73 +124,98 @@ int main(int argc, char** argv)
     }
   
   //check the reader type for now only RIEGL
-  if (reader == NULL ||!(strcmp(reader, "RIEGL") == 0))
+  if (reader.empty() || reader.compare("RIEGL") != 0)
     {
       reader = "RIEGL";
     }
  
   //check for projection
-  if (projection == NULL || (!(strcmp(projection, "STANDARD") == 0) && !(strcmp(projection, "CYLINDRICAL") == 0) && !(strcmp(projection, "NEW") == 0) && !(strcmp(projection, "MERCATOR") == 0) && !(strcmp(projection, "RECTILINEAR") == 0) && !(strcmp(projection, "PANNINI") == 0) && !(strcmp(projection, "STEREOGRAPHIC") == 0)))
+  if (projection.empty()|| ( (projection.compare("STANDARD") != 0) && (projection.compare("CYLINDRICAL") != 0) && (projection.compare("NEW") != 0) && (projection.compare("MERCATOR") != 0) && (projection.compare("RECTILINEAR") != 0) && (projection.compare("PANNINI") != 0) && (projection.compare("STEREOGRAPHIC") != 0)))   
     {
       projection = "STANDARD";
     }
-  if((strcmp(projection, "STANDARD") == 0))
+  if(projection.compare("STANDARD") == 0)
     method = STANDARD;
-  if((strcmp(projection, "CYLINDRICAL") == 0))
+  if(projection.compare("CYLINDRICAL") == 0)
     method = CYLINDRICAL;
-  if((strcmp(projection, "NEW") == 0))
+  if(projection.compare("NEW") == 0)
     method = NEW;
-  if((strcmp(projection, "MERCATOR") == 0))
+  if(projection.compare("MERCATOR") == 0)
     method = MERCATOR;
-  if((strcmp(projection, "RECTILINEAR") == 0))
+  if(projection.compare("RECTILINEAR") == 0)
     method = RECTILINEAR;
-  if((strcmp(projection, "PANNINI") == 0))
+  if(projection.compare("PANNINI") == 0)
     method = PANNINI;
-  if((strcmp(projection, "STEREOGRAPHIC") == 0))
+  if(projection.compare("STEREOGRAPHIC") == 0)
     method = STEREOGRAPHIC;
-  //check for image or map option
   if (!writeMaps && !writeRefelectanceJpeg && !writeRangeImage)
     {
       printf("One of the options -m or -j or -d must be included\n");
       usage(argc, argv);
     }
+  input = argv[optind];
+  return 1;
+}
+
+/**
+ *   main - reads the inputs and create the panorama images and map file
+ */
+int main(int argc, char** argv)
+{  
+  string reader;
+  int method;
+  string projection;
+  bool writeMaps = false;
+  bool writeRangeImage = false;
+  bool writeRefelectanceJpeg = false;
+  string input;
+  vector<struct size> sizes;
+  double d = 1;
+  int n = 4;
+  double r = 1;
   
+  parseArgs(argc, argv, reader, method, projection, writeMaps, writeRangeImage, writeRefelectanceJpeg, input, sizes, d, n, r);
+  cout<<endl;
+  cout<<"panoramacreator will proceed with the following parameters: "<<endl;
+  cout<<"Reader: "<<reader<<endl;
+  cout<<"Projection: "<<projection<<endl;
+  cout<<"Maps: "<<writeMaps<<endl;  
+  cout<<"RangeImage: "<<writeRangeImage<<endl;
+  cout<<"RefelectanceImage: "<<writeRefelectanceJpeg<<endl;  
+  cout<<"Input: "<<input<<endl;
+  cout<<endl;
+
   clock_t start, end;
   vector<struct size>::iterator it;
   char outputmap[1000];
   char outputjpg[1000];
-  for (int index = optind ; index < argc ; index++ )
+  PolarPointCloud polarcloud(input.c_str());
+  //creating the map and refelectance and range images
+  for (it = sizes.begin() ; it != sizes.end() ; it++ )
     {
-      input = argv[index];
-      printf("Reading %s\n", input);
-      PolarPointCloud polarcloud(input);
-      //creating the map and refelectance and range images
-      for (it = sizes.begin() ; it != sizes.end() ; it++ )
+      cout << "Creating Map " << it->width << " " << it->height << endl;
+      start = clock();
+      PanoramaMap map(&polarcloud, it->width, it->height, method, d, n, r);
+      end = clock();
+      double time = ((double)((int) end - (int) start)) / CLOCKS_PER_SEC;
+      cout << "Finished creating map with time: " << time << endl;
+      if (writeMaps)
 	{
-	  cout << "Creating Map " << it->width << " " << it->height << endl;
-	  start = clock();
-	  PanoramaMap map(&polarcloud, it->width, it->height, method, d, n, r);
-	  end = clock();
-	  double time = ((double)((int) end - (int) start)) / CLOCKS_PER_SEC;
-	  cout << "Finished creating map with time: " << time << endl;
-	  if (writeMaps)
-	    {
-	      sprintf(outputmap, "%s_%dx%d.map", input, it->width, it->height);
-	      map.serialize(outputmap);
-	      printf("Wrote map to: %s\n", outputmap);
-	    }
-	  if (writeRangeImage)
-	    {
-	      sprintf(outputjpg, "%s_%dx%d_Range_%d.jpg", input, it->width, it->height, method);
-	      map.toJpeg(outputjpg, RANGE);
-	      printf("Wrote image to: %s\n", outputjpg);
-	    }
-	  if (writeRefelectanceJpeg)
-	    {
-	      sprintf(outputjpg, "%s_%dx%d_Refelectance_%d.jpg", input, it->width, it->height, method);
-	      map.toJpeg(outputjpg, REFELCTANCE);
-	      printf("Wrote image to: %s\n", outputjpg);
-	    }
+	  sprintf(outputmap, "%s_%dx%d.map", input.c_str(), it->width, it->height);
+	  map.serialize(outputmap);
+	  printf("Wrote map to: %s\n", outputmap);
+	}
+      if (writeRangeImage)
+	{
+	  sprintf(outputjpg, "%s_%dx%d_Range_%d.jpg", input.c_str(), it->width, it->height, method);
+	  map.toJpeg(outputjpg, RANGE);
+	  printf("Wrote image to: %s\n", outputjpg);
+	}
+      if (writeRefelectanceJpeg)
+	{
+	  sprintf(outputjpg, "%s_%dx%d_Refelectance_%d.jpg", input.c_str(), it->width, it->height, method);
+	  map.toJpeg(outputjpg, REFELCTANCE);
+	  printf("Wrote image to: %s\n", outputjpg);
 	}
     }
   return 0;
