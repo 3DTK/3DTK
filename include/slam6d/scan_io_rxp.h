b898f284102e075c202718ef7028bd28277110f5
Jan Elseberg <jelseberg@users.sourceforge.net>
2011-10-17 15:26:53 +0000

    and the new header as well
    git-svn-id: svn://svn.code.sf.net/p/slam6d/code/trunk@521 51f1e6e7-7289-49ad-8748-f09e43fc6daa

--- include/slam6d/scan_io_rxp.h
+++ include/slam6d/scan_io_rxp.h
@@ -19,13 +19,19 @@ using namespace scanlib;
 using namespace std;
 using namespace std::tr1;
 
+class importer;
+
 /**
  * @brief 3D scan loader for Riegl scans in the binary rxp format
  *
  * The compiled class is available as shared object file
  */
 class ScanIO_rxp : public ScanIO {
+  shared_ptr<basic_rconnection> rc;
+  decoder_rxpmarker *dec;
+  importer *imp;
 public:
+  ScanIO_rxp() : dec(0), imp(0) {}
   virtual int readScans(int start, int end, string &dir, int maxDist, int mindist,
 				    double *euler, vector<Point> &ptss);
 };
@@ -42,19 +48,31 @@ class importer
     vector<Point> *o;
 
 public:
-    importer(vector<Point> *o_, int maxDist, int minDist)
+    importer(vector<Point> *o_, int maxDist, int minDist, int _s = 0 )
         : pointcloud(false) // set this to true if you need gps aligned timing
-        , o(o_)
+        , o(o_), start(_s)
     {
       maxD = maxDist;
       minD = minDist;
+      currentscan = 0;
     }
 
+    int getCurrentScan() {return currentscan;};
+   
 protected:
+    int start;
     int maxD;
     int minD;
+    int currentscan;
     // overridden from pointcloud class
     void on_echo_transformed(echo_type echo);
+
+    void on_frame_stop(const frame_stop<iterator_type>& arg) {
+      basic_packets::on_frame_stop(arg);
+      currentscan++;
+    }
+
+
 };
 
 // Since this shared object file is  loaded on the fly, we
