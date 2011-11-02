8cf8a549f4d4043ec1debd6464e1bbcc0c542f0f
dborrman <dborrman@51f1e6e7-7289-49ad-8748-f09e43fc6daa>
2011-11-02 14:22:36 +0000

    calibration from ThermalMapper project
    git-svn-id: svn+ssh://svn.code.sf.net/p/slam6d/code/trunk@530 51f1e6e7-7289-49ad-8748-f09e43fc6daa

---
+++ 3rdparty/cvblob/CMakeFiles/cvblob.dir/DependInfo.cmake
@@ -0,0 +1,18 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/dborrman/Jacobs/software/slam6d/trunk/3rdparty/cvblob/cvaux.cpp" "/home/dborrman/Jacobs/software/slam6d/trunk/3rdparty/cvblob/CMakeFiles/cvblob.dir/cvaux.cpp.o"
+  "/home/dborrman/Jacobs/software/slam6d/trunk/3rdparty/cvblob/cvblob.cpp" "/home/dborrman/Jacobs/software/slam6d/trunk/3rdparty/cvblob/CMakeFiles/cvblob.dir/cvblob.cpp.o"
+  "/home/dborrman/Jacobs/software/slam6d/trunk/3rdparty/cvblob/cvcolor.cpp" "/home/dborrman/Jacobs/software/slam6d/trunk/3rdparty/cvblob/CMakeFiles/cvblob.dir/cvcolor.cpp.o"
+  "/home/dborrman/Jacobs/software/slam6d/trunk/3rdparty/cvblob/cvcontour.cpp" "/home/dborrman/Jacobs/software/slam6d/trunk/3rdparty/cvblob/CMakeFiles/cvblob.dir/cvcontour.cpp.o"
+  "/home/dborrman/Jacobs/software/slam6d/trunk/3rdparty/cvblob/cvlabel.cpp" "/home/dborrman/Jacobs/software/slam6d/trunk/3rdparty/cvblob/CMakeFiles/cvblob.dir/cvlabel.cpp.o"
+  "/home/dborrman/Jacobs/software/slam6d/trunk/3rdparty/cvblob/cvtrack.cpp" "/home/dborrman/Jacobs/software/slam6d/trunk/3rdparty/cvblob/CMakeFiles/cvblob.dir/cvtrack.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
