3b8f38e1ffdbe086fa7c78ea69e924c6e0d8756b
dborrman <dborrman@51f1e6e7-7289-49ad-8748-f09e43fc6daa>
2011-11-17 09:44:28 +0000

    clean up of thermalmapper code
    git-svn-id: svn+ssh://svn.code.sf.net/p/slam6d/code/trunk@537 51f1e6e7-7289-49ad-8748-f09e43fc6daa

--- include/thermo/thermo.h
+++ include/thermo/thermo.h
@@ -9,7 +9,7 @@ using namespace std;
 typedef vector<vector<float> > Float2D[1200][1600];
 
 void calcBoard(double point_array[][2], int board_n, double &x, double &y, double &cx, double &cy, bool pc);    
-void sortCluster(double point_array[][2], int board_n, int board_h, int board_w, bool quiet); 
+void sortBlobs(double point_array[][2], int board_n, int board_h, int board_w, bool quiet); 
 IplImage* detectBlobs(IplImage *org_image, int &corner_exp, int board_h, int board_w, bool quiet, double point_array2[][2]);
 void drawLines(double point_array2[][2], int corner_exp, IplImage *image, bool color=false);
 IplImage* resizeImage(IplImage *source, int scale);
@@ -17,7 +17,9 @@ IplImage* detectCorners(IplImage *orgimage, int corner_exp, int board_h, int boa
 void CalibFunc(int board_w, int board_h, int start, int end, bool optical, bool chess, bool quiet, string dir, int scale=1);
 void writeCalibParam(int images, int corner_exp, int board_w, CvMat* image_points, CvSize size, string dir);
 
-void ProjectAndMap(int start, int end, bool optical, bool quiet, string dir, reader_type type, int scale, double rot_angle, double minDist, double maxDist, bool correction, int neighborhood);
+void ProjectAndMap(int start, int end, bool optical, bool quiet, string dir,
+reader_type type, int scale, double rot_angle, double minDist, double maxDist,
+bool correction, int neighborhood, int method=0);
 
 bool readPoints(string filename, CvPoint3D32f *corners, int size) ;
 void sortElementByElement(CvMat * vectors, int nr_elems, int nr_vectors); 
@@ -26,7 +28,7 @@ points3D, CvMat * rotation_vectors_temp, CvMat * translation_vectors_temp, CvMat
 * distortions, CvMat * instrinsics, int corners, int successes, string dir, bool quiet=true, string substring = "") ;
 void calculateExtrinsics(CvMat * rotation_vectors_temp, CvMat * translation_vectors_temp, int successes, string dir, bool quiet=true, string substring = "") ;
 void CorrectErrorAndWrite(Float2D &data, fstream &outfile, CvSize size);
-bool clusterSearch(float ** points, int size, double thresh1, double thresh2, fstream &outfile);
+void clusterSearch(float ** points, int size, double thresh1, double thresh2, fstream &outfile);
 void sortDistances(float ** points, int size);
 void ExtrCalibFunc(int board_w, int board_h, int start, int end, bool optical, bool chess, bool quiet, string dir, int scale=1);
 
