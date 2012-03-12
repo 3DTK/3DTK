2c5fed47c3ee1032a8fa3757036632b7eb7136b4
nuechter <nuechter@51f1e6e7-7289-49ad-8748-f09e43fc6daa>
2012-03-12 14:24:43 +0000

    because Kai starts to crying
    git-svn-id: https://svn.code.sf.net/p/slam6d/code/trunk@590 51f1e6e7-7289-49ad-8748-f09e43fc6daa

--- include/slam6d/point.h
+++ include/slam6d/point.h
@@ -16,15 +16,6 @@ using std::istream;
 #include <stdexcept>
 using std::runtime_error;
 
-/** ÎÞÐ§µã£¬´òµ½¾àÀëÍâ£¬»òÕßÔÚÎÒÃÇ¶¨ÒåµÄ¾àÀëÖ®Íâ*/
-#define POINT_TYPE_INVALID       0x00000001
-#define POINT_TYPE_ABOVE_DELTA_R 0x00000002
-#define POINT_TYPE_BELOW_DELTA_R 0x00000004
-#define POINT_TYPE_ABOVE_DELTA_Y 0x00000008
-#define POINT_TYPE_BELOW_DELTA_Y 0x00000010
-#define POINT_TYPE_ABOVE_R       0x00000020
-#define POINT_TYPE_BELOW_R       0x00000040
-
 /**
  * @brief Representation of a point in 3D space
  */
