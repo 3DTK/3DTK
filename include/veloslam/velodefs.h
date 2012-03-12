2c6ad1eabff211e27113207213aeada1c119bd34
nuechter <nuechter@51f1e6e7-7289-49ad-8748-f09e43fc6daa>
2012-03-12 11:57:06 +0000

    because Kai starts to crying
    git-svn-id: https://svn.code.sf.net/p/slam6d/code/trunk@586 51f1e6e7-7289-49ad-8748-f09e43fc6daa

---
+++ include/veloslam/velodefs.h
@@ -0,0 +1,13 @@
+#ifndef __SCAN_VELODEFS_H__
+#define __SCAN_VELODEFS_H__
+
+/** ÎÞÐ§µã£¬´òµ½¾àÀëÍâ£¬»òÕßÔÚÎÒÃÇ¶¨ÒåµÄ¾àÀëÖ®Íâ*/
+#define POINT_TYPE_INVALID       0x00000001
+#define POINT_TYPE_ABOVE_DELTA_R 0x00000002
+#define POINT_TYPE_BELOW_DELTA_R 0x00000004
+#define POINT_TYPE_ABOVE_DELTA_Y 0x00000008
+#define POINT_TYPE_BELOW_DELTA_Y 0x00000010
+#define POINT_TYPE_ABOVE_R       0x00000020
+#define POINT_TYPE_BELOW_R       0x00000040
+
+#endif
