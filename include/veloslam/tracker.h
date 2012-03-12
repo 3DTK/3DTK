8346820079453f5708e0d9f9a7411500503e84b3
leeajax <leeajax@users.sourceforge.net>
2012-03-12 04:54:43 +0000

    git-svn-id: svn://svn.code.sf.net/p/slam6d/code/trunk@584 51f1e6e7-7289-49ad-8748-f09e43fc6daa

---
+++ include/veloslam/tracker.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "veloslam/veloscan.h"
+#include <deque>
+
+using namespace std;
+/** 
+跟踪器，每一个跟踪器代表一个被跟踪对象，并记录了被跟踪对象的信息
+*/
+class Tracker
+{
+public:
+	Tracker(void);
+	~Tracker(void);
+	deque<clusterFeature> statusList;
+	deque<cluster> dataList;
+	bool missMatch;
+	bool Matched;
+	int matchClusterID;
+	int colorIdx;
+	int frameNO;
+
+};
+
