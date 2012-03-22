65abe62e7569d928db76583cc46fd3a0f901fece
leeajax <leeajax@users.sourceforge.net>
2012-03-22 03:46:30 +0000

    Slam + DATMO by liming
    git-svn-id: svn://svn.code.sf.net/p/slam6d/code/trunk@607 51f1e6e7-7289-49ad-8748-f09e43fc6daa

--- include/veloslam/debugview.h
+++ include/veloslam/debugview.h
@@ -1,5 +1,5 @@
 #pragma once
-
+ 
 #include <list>
 #include <vector>
 #include "veloslam/veloscan.h"
@@ -9,11 +9,11 @@ void GetCurrecntdelteMat(Scan& CurrentScan ,  Scan& firstScan,  double *deltaMat
 
 void DrawText(float x, float y, float z, char * outputstring);
 void  DrawTextRGB(float x, float y, float z, float r, float g, float b, char * outputstring);
+void  DrawTextRGB(Point P, float r, float g, float b, char * outputstring);
 
-void   Draw_Line_GL_RGB(float x1, float y1, float z1,
-	                                    float x2, float y2, float z2,
-										float r,float g,float b);
+void   Draw_Line_GL_RGB(float x1, float y1, float z1,  float x2, float y2, float z2, float r,float g,float b);
 void  Draw_Line_GL(float x1, float y1, float z1, float x2, float y2, float z2);
+void   Draw_Line_GL_RGB(Point P1, Point P2, int width,	float r,float g,float b, bool arrow);
 
 void Draw_Cube_GL_RGB(float min_x, float min_y, float min_z,
 	                                    float max_x, float max_y, float max_z,
