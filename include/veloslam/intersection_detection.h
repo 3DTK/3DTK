49c94530c70fbcb84fbd26feafdf8b858dbafd28
zhuquanwen <zhuquanwen@51f1e6e7-7289-49ad-8748-f09e43fc6daa>
2012-03-24 10:28:14 +0000

    add intersection detection algorithm in veloslam
    git-svn-id: svn+ssh://svn.code.sf.net/p/slam6d/code/trunk@611 51f1e6e7-7289-49ad-8748-f09e43fc6daa

---
+++ include/veloslam/intersection_detection.h
@@ -0,0 +1,58 @@
+
+#include "veloslam/gridcell.h"
+#include "veloslam/gridcluster.h"
+#include "veloslam/svm.h"
+
+float absf(float a);
+
+struct IntersectionFeature
+{
+	float angle;
+	float slashLength;
+	int slashEndColumn;
+	int slashEndRow;
+};
+
+class intersectionDetection
+{
+public:
+	intersectionDetection();
+	~intersectionDetection();
+
+	
+	int CalcRadAndTheta();
+	int CalPointCellPos(double x,double y,double z ,int * column,int * row);
+	int CalCellMinDis();
+	int TransferToCellArray();
+
+	int CalcAllCellFeature();
+	int CalcCellFeature(cell& cellobj, cellFeature& f);
+	int CalWideSlashEdge_For_RoadShape(float Angle,int startColumID,int startRowID,float maxLength,float wide);
+	int DetectIntersection();
+
+
+private:
+
+  int cellSize;
+  int columnNum;
+  int cellNum;
+  int MinRad;		
+  int MaxRad;
+
+
+  vector <Point> allPoints_AfterRegstn;
+  cellArray cellArray_AfterRegstn;
+  cellFeatureArray cellFeatureArray_AfterRegstn;
+   float slashWide;
+  float slashMaxLength;
+  float slashAngleDefinition;
+  vector <float> minCellDisList;
+  vector <IntersectionFeature> intersectionFeature;
+
+  
+ /* clusterArray clusterArrayAfterRegstn;
+  clusterFeatureArray clusterFeatureArray_AfterRegstn;
+*/
+
+
+};
