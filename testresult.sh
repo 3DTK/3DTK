1b24971874b15bd4e904481d023d244bb730e781
liming751218 <liming751218@51f1e6e7-7289-49ad-8748-f09e43fc6daa>
2012-04-11 13:18:53 +0000

    git-svn-id: https://svn.code.sf.net/p/slam6d/code/trunk@646 51f1e6e7-7289-49ad-8748-f09e43fc6daa

--- testresult.sh
+++ testresult.sh
@@ -1,1 +1,1 @@
-bin/veloslam -s 495 -e 508 -r 15 -d 100 -f velodyne ~/velodynedata/wuhuang -i 300 -m 3000 -M 1000 -O 1
+bin/veloslam -s 495 -e 1596 -b 2 -t 10 -y 1 -r 15 -d 100 -f velodyne ~/velodynedata/wuhuang -i 300 -m 8000 -M  500 -O 1
