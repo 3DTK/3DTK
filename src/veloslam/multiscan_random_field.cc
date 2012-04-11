2a5ad49572dcde7e65c300c0c5307843de0ab61a
Li Ming <liming751218@users.sourceforge.net>
2012-04-11 13:14:45 +0000

    git-svn-id: svn://svn.code.sf.net/p/slam6d/code/trunk@645 51f1e6e7-7289-49ad-8748-f09e43fc6daa

---
+++ src/veloslam/multiscan_random_field.cc
@@ -0,0 +1,14 @@
+#include "veloslam/veloscan.h"
+#include "veloslam/svm.h"
+#include "veloslam/multiscan_random_field.h"
+
+Random_field::Random_field(void)
+{
+
+}
+
+Random_field::~Random_field(void)
+{
+
+}
+
