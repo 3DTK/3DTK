9c9f6dfadf45aa26bd6ca27022e062a2146a9571
liming751218 <liming751218@51f1e6e7-7289-49ad-8748-f09e43fc6daa>
2012-05-08 03:18:55 +0000

    git-svn-id: https://svn.code.sf.net/p/slam6d/code/trunk@658 51f1e6e7-7289-49ad-8748-f09e43fc6daa

---
+++ include/scanio/scan_io_velodyne.h
@@ -0,0 +1,30 @@
+/**
+ * @file
+ * @brief IO of a 3D scan in velodyne raw file format
+ * @author Liwei. Computer School of Wuhan University, China.
+ */
+
+#ifndef __SCAN_IO_VELODYNE_H__
+#define __SCAN_IO_VELODYNE_H__
+
+#include <string>
+using std::string;
+#include <vector>
+using std::vector;
+
+#include "scan_io.h"
+
+/**
+ * @brief 3D scan loader for VELODYNE scans
+ *
+ * The compiled class is available as shared object file
+ */
+class ScanIO_velodyne : public ScanIO {
+public:
+  virtual std::list<std::string> readDirectory(const char* dir_path, unsigned int start, unsigned int end);
+  virtual void readPose(const char* dir_path, const char* identifier, double* pose);
+  virtual void readScan(const char* dir_path, const char* identifier, PointFilter& filter, std::vector<double>* xyz, std::vector<unsigned char>* rgb, std::vector<float>* reflectance, std::vector<float>* amplitude, std::vector<int>* type, std::vector<float>* deviation);
+  virtual bool supports(IODataType type);
+};
+
+#endif
