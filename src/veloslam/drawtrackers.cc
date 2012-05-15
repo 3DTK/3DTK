b280f44ed0306ff1a2143bbed0d65321b384662a
liming751218 <liming751218@51f1e6e7-7289-49ad-8748-f09e43fc6daa>
2012-05-15 01:05:10 +0000

    git-svn-id: svn+ssh://svn.code.sf.net/p/slam6d/code/trunk@674 51f1e6e7-7289-49ad-8748-f09e43fc6daa

--- src/veloslam/drawtrackers.cc
+++ src/veloslam/drawtrackers.cc
@@ -74,31 +74,8 @@ int TrackerManager::DrawTrackersMovtion_Long_Number_All(vector <Scan *> allScans
 
     				colorIdx=tracker.colorIdx%8;
 
-        //            if(tracker.moving_distance < constant_static_or_moving)
-    				//{
-    				//	DrawObjectPoint(gluData1, 1,
-    				//			0.2,
-    				//			0.2,
-    				//			0.2,
-    				//			deltaMat);
-    				//}
-        //            else
-        //   			{
-    				//	  DrawObjectPoint(gluData1, 1,
-        //						ColorTableShot[colorIdx][0],
-        //						ColorTableShot[colorIdx][1],
-        //						ColorTableShot[colorIdx][2] ,
-    				//			deltaMat);
-    				//}
-
                 }
-			 //     if(firstNO <0)
-    		//		    continue;
-
-         //           sprintf(object_moving_distance, "%d %d %4.2f ",tracker.trackerID, tracker.matchClusterID,  tracker.moving_distance);
-          //          DrawTextRGB(p1text, 1, 0, 0, object_moving_distance );
-           //         DrawPoint(p1,4,0,0,1);
-                    continue;
+                  continue;
     		}
             ////////////////////////////////////////////////////////////
 
@@ -141,49 +118,6 @@ int TrackerManager::DrawTrackersMovtion_Long_Number_All(vector <Scan *> allScans
 
 				colorIdx=tracker.colorIdx%8;
 
-           /*     if(tracker.moving_distance < constant_static_or_moving)
-				{
-					DrawObjectPoint(gluData1, 1,
-							0.2,
-							0.2,
-							0.2,
-							deltaMat);
-					DrawObjectPoint(gluData2, 1,
-							0.2,
-							0.2,
-							0.2,
-							deltaMat);
-				}
-                else
-       			{
-					  DrawObjectPoint(gluData1, 1,
-    						ColorTableShot[colorIdx][0],
-    						ColorTableShot[colorIdx][1],
-    						ColorTableShot[colorIdx][2] ,
-							deltaMat);
-
-					  DrawObjectPoint(gluData2, 1,
-    						ColorTableShot[colorIdx][0],
-    						ColorTableShot[colorIdx][1],
-    						ColorTableShot[colorIdx][2] ,
-							deltaMat);
-				}*/
-
-
-
-			//	char objectID[256];
-		//		sprintf(objectID, "%d", glu1.trackNO);
-			//	DrawTextRGB(p1text, 0,1,0, objectID );
-
-				//colorIdx=(tracker.colorIdx+1)%8;
-				//DrawObjectPoint(gluData2, 1,
-				//	ColorTableShot[colorIdx][0],
-				//	ColorTableShot[colorIdx][1],
-				//	ColorTableShot[colorIdx][2] ,
-				//	deltaMatNext);
-
-			//	sprintf(objectID, "%d", glu2.trackNO);
-			//	DrawTextRGB(p2text, 0,1,0,objectID );
 
 				DrawPoint(p1,4,0,1,1);
 				DrawPoint(p2,4,0,1,1);
@@ -201,72 +135,6 @@ int TrackerManager::DrawTrackersMovtion_Long_Number_All(vector <Scan *> allScans
 	return 0;
 }
 
-
-int TrackerManager::DrawTrackersContrailAfterFilted(vector<Scan *> allScans)
-{
-	list<Tracker>::iterator it;
-	for(it=tracks.begin();  it!=tracks.end();  it++)
-	{
-		Tracker &tracker=*it;
-		int size=tracker.moveStateList.size();
-		int firstNO= -1;
-		int secondNO= -1;
-
-		if (size<3||tracker.moving_distance<constant_static_or_moving)
-		{
-			continue;
-		}
-
-		for (int i=0;i<size-1;i++)
-		{
-			firstNO = GetScanID_in_SlidingWindow(tracker.moveStateList[i].frameNo,
-				current_sliding_window_pos,
-				sliding_window_size);
-			secondNO = GetScanID_in_SlidingWindow(tracker.moveStateList[i+1].frameNo,
-				current_sliding_window_pos,
-				sliding_window_size);
-
-			if(firstNO <0 || secondNO< 0 )
-				continue;
-
-			Scan *firstScan = (Scan *)g_pfirstScan;
-			Scan *CurrentScan = allScans[firstNO];
-			Scan *CurrentScanNext = allScans[secondNO];
-
-			double  deltaMat[16];
-			double  deltaMatNext[16];
-
-			GetCurrecntdelteMat(*CurrentScan , *firstScan,  deltaMat);
-			GetCurrecntdelteMat(*CurrentScanNext , *firstScan,  deltaMatNext);
-
-			clusterFeature &glu1=tracker.statusList[i];
-			cluster &gluData1=tracker.dataList[i];
-			clusterFeature &glu2=tracker.statusList[i+1];
-			cluster &gluData2=tracker.dataList[i+1];
-
-			if (glu1.size < 8) continue;
-			if (glu2.size < 8) continue;
-
-			Point p1(tracker.moveStateList[i].targetState.x_position, glu1.avg_y,tracker.moveStateList[i].targetState.z_positon);
-			Point p2(tracker.moveStateList[i+1].targetState.x_position, glu2.avg_y, tracker.moveStateList[i+1].targetState.z_positon);
-			Point p1text(tracker.moveStateList[i].targetState.x_position, glu1.avg_y+50,tracker.moveStateList[i].targetState.z_positon);
-			Point p2text(tracker.moveStateList[i+1].targetState.x_position, glu2.avg_y+50,tracker.moveStateList[i+1].targetState.z_positon);
-
-			p1.transform(deltaMat);
-			p2.transform(deltaMatNext);
-			p1text.transform(deltaMat);
-			p2text.transform(deltaMatNext);
-
-			DrawPoint(p1,4,1,0,1);
-			DrawPoint(p2,4,1,0,1);
-
-			Draw_Line_GL_RGB(p1,p2,2,1,0,1,false);
-		}
-
-	}
-
-	return 0;
-}
 
 
 int TrackerManager::DrawEgoTrajectory()
