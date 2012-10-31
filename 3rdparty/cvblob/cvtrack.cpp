4d64309841fddb1f9d771c10173c3d43eeb07f20
Jan Elseberg <jelseberg@users.sourceforge.net>
2012-10-31 13:07:05 +0000

    fixed all includes
    git-svn-id: svn://svn.code.sf.net/p/slam6d/code/trunk@745 51f1e6e7-7289-49ad-8748-f09e43fc6daa

--- 3rdparty/cvblob/cvtrack.cpp
+++ 3rdparty/cvblob/cvtrack.cpp
@@ -24,7 +24,7 @@ using namespace std;
 
 #if (defined(_WIN32) || defined(__WIN32__) || defined(__TOS_WIN__) || defined(__WINDOWS__) || (defined(__APPLE__) & defined(__MACH__)))
 #include <cv.h>
-#else
+#elif (CV_MAJOR_VERSION == 2) && (CV_MINOR_VERSION < 2)
 #include <opencv/cv.h>
 #endif
 
