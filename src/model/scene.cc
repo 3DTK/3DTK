53ad51ad1abcf9fcc177106ee9be3710ad3061e8
Andreas Nüchter <nuechter@users.sourceforge.net>
2013-06-18 18:22:01 +0000

    small fix (header included
    git-svn-id: svn://svn.code.sf.net/p/slam6d/code/trunk@833 51f1e6e7-7289-49ad-8748-f09e43fc6daa

--- src/model/scene.cc
+++ src/model/scene.cc
@@ -18,6 +18,7 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/ml/ml.hpp>
+#include "opencv2/photo/photo.hpp" 
 
 #include <boost/algorithm/string.hpp>
 
