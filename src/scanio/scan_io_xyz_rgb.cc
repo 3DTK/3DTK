7c1c1353f23598f7f209d07b0648a9340098c77c
hhoushiar <hhoushiar@51f1e6e7-7289-49ad-8748-f09e43fc6daa>
2013-08-08 13:34:38 +0000

    added scan_io_xyz_rgb
    git-svn-id: svn+ssh://svn.code.sf.net/p/slam6d/code/trunk@842 51f1e6e7-7289-49ad-8748-f09e43fc6daa

---
+++ src/scanio/scan_io_xyz_rgb.cc
@@ -0,0 +1,207 @@
+/*
+ * scan_io_xyz_rgb implementation
+ *
+ * Copyright (C) Andreas Nuechter
+ *
+ * Released under the GPL version 3.
+ *
+ */
+
+
+/**
+ * @file scan_io_xyz_rgb.cc
+ * @brief IO of a 3D scan in xyz file format plus a reflectance/intensity
+ * @author Andreas Nuechter. Jacobs University Bremen, Germany.
+ */
+
+#include "scanio/scan_io_xyz_rgb.h"
+
+#include <iostream>
+using std::cout;
+using std::cerr;
+using std::endl;
+#include <vector>
+
+#ifdef _MSC_VER
+#include <windows.h>
+#endif
+
+#include <boost/filesystem/operations.hpp>
+#include <boost/filesystem/fstream.hpp>
+using namespace boost::filesystem;
+
+#include "slam6d/globals.icc"
+
+#define DATA_PATH_PREFIX "scan"
+#define DATA_PATH_SUFFIX ".xyz"
+#define POSE_PATH_PREFIX "scan"
+#define POSE_PATH_SUFFIX ".pose"
+
+std::list<std::string> ScanIO_xyz_rgb::readDirectory(const char* dir_path, 
+						  unsigned int start, 
+						  unsigned int end)
+{
+  std::list<std::string> identifiers;
+  for(unsigned int i = start; i <= end; ++i) {
+    // identifier is /d/d/d (000-999)
+    std::string identifier(to_string(i,3));
+    // scan consists of data (.3d) and pose (.pose) files
+    path data(dir_path);
+    data /= path(std::string(DATA_PATH_PREFIX) + identifier + DATA_PATH_SUFFIX);
+    path pose(dir_path);
+    pose /= path(std::string(POSE_PATH_PREFIX) + identifier + POSE_PATH_SUFFIX);
+    cout << data << endl;
+    // stop if part of a scan is missing or end by absence is detected
+    cout << i << endl;
+    if(!exists(data)) {
+      cerr << "No data found!" << endl;
+      break;
+    }
+    if(!exists(pose)) {
+      cerr << "No pose files found!" << endl; 
+      break;
+    }
+    identifiers.push_back(identifier);
+  }
+  return identifiers;
+}
+
+void ScanIO_xyz_rgb::readPose(const char* dir_path, 
+			   const char* identifier, 
+			   double* pose)
+{
+  unsigned int i;
+
+  path pose_path(dir_path);
+  pose_path /= path(std::string(POSE_PATH_PREFIX) 
+		    + identifier + 
+		    POSE_PATH_SUFFIX);
+  if(!exists(pose_path))
+    throw std::runtime_error(std::string("There is no pose file for [") 
+			     + identifier + "] in [" + dir_path + "]");
+
+  // open pose file
+  ifstream pose_file(pose_path);
+
+  // if the file is open, read contents
+  if(pose_file.good()) {
+    // read 6 plain doubles
+    for(i = 0; i < 6; ++i) pose_file >> pose[i];
+    pose_file.close();
+
+    // convert angles from deg to rad
+    for(i = 3; i < 6; ++i) pose[i] = rad(pose[i]);
+  } else {
+    throw std::runtime_error(std::string("Pose file could not be opened for [")
+			     + identifier + "] in [" 
+			     + dir_path + "]");
+  }
+}
+
+bool ScanIO_xyz_rgb::supports(IODataType type)
+{
+  return !!(type & ( DATA_XYZ | DATA_RGB));
+}
+
+void ScanIO_xyz_rgb::readScan(const char* dir_path, 
+			   const char* identifier, 
+			   PointFilter& filter, 
+			   std::vector<double>* xyz, 
+			   std::vector<unsigned char>* rgb, 
+			   std::vector<float>* reflectance, 
+			   std::vector<float>* temperature, 
+			   std::vector<float>* amplitude, 
+			   std::vector<int>* type, 
+			   std::vector<float>* deviation)
+{
+  unsigned int i;
+
+  // error handling
+  path data_path(dir_path);
+  data_path /= path(std::string(DATA_PATH_PREFIX) 
+		    + identifier 
+		    + DATA_PATH_SUFFIX);
+  if(!exists(data_path))
+    throw std::runtime_error(std::string("There is no scan file for [") 
+			     + identifier + "] in [" 
+			     + dir_path + "]");
+
+  if(xyz != 0 && rgb != 0) {
+    // open data file
+    ifstream data_file(data_path);
+    data_file.exceptions(ifstream::eofbit|ifstream::failbit|ifstream::badbit);
+
+    // overread the first line ignoring the header information
+    char dummy[255];
+    data_file.getline(dummy, 255);
+
+    // read points and reflectance/intensity/temperature value
+    double point[3];
+    //float reflection;
+    unsigned int color[3];
+    while(data_file.good()) {
+      try {
+        data_file >> point[2];
+        data_file >> point[0];
+        data_file >> point[1];
+        point[0] *= -1.0;
+        //data_file >> reflection;
+	for(i = 0; i < 3; ++i) data_file >> color[i];
+/*
+	point[0] -= 485531.0;
+	point[1] -= 5882078.400;
+	point[2] -= 52;
+*/
+      } catch(std::ios_base::failure& e) {
+        break;
+      }
+
+      // apply filter then insert point and reflectance
+      if(filter.check(point)) {
+        for(i = 0; i < 3; ++i) xyz->push_back(point[i]);
+        //reflectance->push_back(reflection);
+	for(i = 0; i < 3; ++i) rgb->push_back(static_cast<unsigned char>(color[i]));  
+      }
+    }
+    data_file.close();
+  }
+}
+
+
+
+/**
+ * class factory for object construction
+ *
+ * @return Pointer to new object
+ */
+#ifdef _MSC_VER
+extern "C" __declspec(dllexport) ScanIO* create()
+#else
+extern "C" ScanIO* create()
+#endif
+{
+  return new ScanIO_xyz_rgb;
+}
+
+
+/**
+ * class factory for object construction
+ *
+ * @return Pointer to new object
+ */
+#ifdef _MSC_VER
+extern "C" __declspec(dllexport) void destroy(ScanIO *sio)
+#else
+extern "C" void destroy(ScanIO *sio)
+#endif
+{
+  delete sio;
+}
+
+#ifdef _MSC_VER
+BOOL APIENTRY DllMain(HANDLE hModule, DWORD dwReason, LPVOID lpReserved)
+{
+    return TRUE;
+}
+#endif
+
