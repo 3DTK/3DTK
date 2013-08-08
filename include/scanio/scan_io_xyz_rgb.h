f77c346cde80458c7557a9894788dab2c50e1218
hhoushiar <hhoushiar@51f1e6e7-7289-49ad-8748-f09e43fc6daa>
2013-08-08 13:34:38 +0000

    added scan_io_xyz_rgb
    git-svn-id: https://svn.code.sf.net/p/slam6d/code/trunk@842 51f1e6e7-7289-49ad-8748-f09e43fc6daa

---
+++ include/scanio/scan_io_xyz_rgb.h
@@ -0,0 +1,42 @@
+/**
+ * @file scan_io_xyz_rgb.h
+ * @brief IO of a 3D scan in xyz file format plus an intensity
+
+ * @author Hamidreza Houshiar
+ */
+
+#ifndef __SCAN_IO_XYZ_RGB_H__
+#define __SCAN_IO_XYZ_RGB_H__
+
+#include "scan_io.h"
+
+
+
+/**
+ * @brief IO of a 3D scan in uos file format plus a
+ *        reflectance/intensity/temperature value
+ *
+ * The compiled class is available as shared object file
+ */
+class ScanIO_xyz_rgb : public ScanIO {
+public:
+  virtual std::list<std::string> readDirectory(const char* dir_path, 
+					       unsigned int start, 
+					       unsigned int end);
+  virtual void readPose(const char* dir_path, 
+			const char* identifier, 
+			double* pose);
+  virtual void readScan(const char* dir_path, 
+			const char* identifier, 
+			PointFilter& filter, 
+			std::vector<double>* xyz, 
+			std::vector<unsigned char>* rgb, 
+			std::vector<float>* reflectance, 
+			std::vector<float>* temperature, 
+			std::vector<float>* amplitude, 
+			std::vector<int>* type, 
+			std::vector<float>* deviation);
+  virtual bool supports(IODataType type);
+};
+
+#endif
