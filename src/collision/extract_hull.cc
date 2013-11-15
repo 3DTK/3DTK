314ad5bb5b6e9f53aad1f21dec03c5e0059b5450
josch1337 <josch1337@51f1e6e7-7289-49ad-8748-f09e43fc6daa>
2013-11-15 17:28:58 +0000

    surface extraction based on walking along it
    git-svn-id: svn+ssh://svn.code.sf.net/p/slam6d/code/trunk@863 51f1e6e7-7289-49ad-8748-f09e43fc6daa

--- src/collision/extract_hull.cc
+++ src/collision/extract_hull.cc
@@ -10,6 +10,8 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <math.h>
+#include <unordered_set>
 #include <boost/program_options.hpp>
 namespace po = boost::program_options;
 
@@ -47,14 +49,14 @@ void parse_options(int argc, char **argv, std::string &filename)
 
     // display help
     if (vm.count("help")) {
-        std::cout << cmdline_options;
-        std::cout << "\nExample usage:\n"
+        std::cerr << cmdline_options;
+        std::cerr << "\nExample usage:\n"
              << "\t" << argv[0] << " input.obj\n";
         exit(0);
     }
 
     if (!vm.count("input")) {
-        std::cout << "you have to specify an input file" << std::endl;
+        std::cerr << "you have to specify an input file" << std::endl;
         exit(1);
     }
 }
@@ -157,8 +159,6 @@ int intersect_triangle(double *orig, double *dir,
 
 void get_concave_hull(std::vector<double*> const &vertices, std::vector<size_t*> const &faces)
 {
-    std::cout << vertices.size() << std::endl;
-    std::cout << faces.size() << std::endl;
     std::vector<bool> hull(vertices.size());
     /* for all vertices, test them against all faces */
     for (size_t i = 0; i < vertices.size(); ++i) {
@@ -182,12 +182,254 @@ void get_concave_hull(std::vector<double*> const &vertices, std::vector<size_t*>
     }
 }
 
+/*
+struct edge_hash {
+    inline std::size_t operator()(const std::pair<size_t,size_t> & v) const {
+        return v.first^v.second; // this works because v.first != v.second
+    }
+};
+*/
+
+void fill_datastructures(std::vector<double*> const &vertices,
+        std::vector<size_t*> const &faces,
+        std::map<std::pair<size_t, size_t>, std::vector<std::pair<size_t,size_t>>> &edgetofaces,
+        std::vector<std::unordered_set <size_t>> &vertextoneighbors)
+{
+    for (auto it = vertices.begin(); it != vertices.end(); ++it) {
+        std::unordered_set<size_t> m;
+        vertextoneighbors.push_back(m);
+    }
+    for (size_t i = 0; i < faces.size(); ++i) {
+        // since edges are undirected, create edges for both directions
+        std::pair<size_t, size_t> edge1a = std::make_pair(faces[i][0], faces[i][1]);
+        std::pair<size_t, size_t> edge2a = std::make_pair(faces[i][1], faces[i][2]);
+        std::pair<size_t, size_t> edge3a = std::make_pair(faces[i][2], faces[i][0]);
+        std::pair<size_t, size_t> edge1b = std::make_pair(faces[i][1], faces[i][0]);
+        std::pair<size_t, size_t> edge2b = std::make_pair(faces[i][2], faces[i][1]);
+        std::pair<size_t, size_t> edge3b = std::make_pair(faces[i][0], faces[i][2]);
+        // fill edgetofaces
+        edgetofaces[edge1a].push_back(std::make_pair(i,faces[i][2]));
+        edgetofaces[edge2a].push_back(std::make_pair(i,faces[i][0]));
+        edgetofaces[edge3a].push_back(std::make_pair(i,faces[i][1]));
+        edgetofaces[edge1b].push_back(std::make_pair(i,faces[i][2]));
+        edgetofaces[edge2b].push_back(std::make_pair(i,faces[i][0]));
+        edgetofaces[edge3b].push_back(std::make_pair(i,faces[i][1]));
+        // fill vertextoneighbors
+        vertextoneighbors[faces[i][0]].insert(faces[i][1]);
+        vertextoneighbors[faces[i][0]].insert(faces[i][2]);
+        vertextoneighbors[faces[i][1]].insert(faces[i][0]);
+        vertextoneighbors[faces[i][1]].insert(faces[i][2]);
+        vertextoneighbors[faces[i][2]].insert(faces[i][1]);
+        vertextoneighbors[faces[i][2]].insert(faces[i][0]);
+    }
+}
+
+double* calculatenormalofface(std::vector<double*> const &vertices, std::vector<size_t*> const &faces, size_t face)
+{
+    // n = e1 x e2 = (v0-v1) x (v0-v2)
+    // we assume that vertices are arranged counterclockwise
+    double e10 = vertices[faces[face][0]][0] - vertices[faces[face][1]][0];
+    double e11 = vertices[faces[face][0]][1] - vertices[faces[face][1]][1];
+    double e12 = vertices[faces[face][0]][2] - vertices[faces[face][1]][2];
+    double e20 = vertices[faces[face][0]][0] - vertices[faces[face][2]][0];
+    double e21 = vertices[faces[face][0]][1] - vertices[faces[face][2]][1];
+    double e22 = vertices[faces[face][0]][2] - vertices[faces[face][2]][2];
+    return new double[3]{e11*e22-e12*e21, e12*e20-e10*e22, e10*e21-e11*e20};
+}
+
+double calculateanglebetweenfaces(std::vector<double*> const &vertices,
+        std::vector<size_t*> const &faces,
+        size_t face1, size_t face2, std::pair<size_t,size_t> &edge)
+{
+    // calculate normal vector of faces
+    double *n1 = calculatenormalofface(vertices, faces, face1);
+    double *n2 = calculatenormalofface(vertices, faces, face2);
+    // calculate angle between two normals
+    double a = acos(
+            (n1[0]*n2[0]+n1[1]*n2[1]+n1[2]*n2[2])/
+            (sqrt(n1[0]*n1[0]+n1[1]*n1[1]+n1[2]*n1[2])*
+             sqrt(n2[0]*n2[0]+n2[1]*n2[1]+n2[2]*n2[2])));
+    // calculate cross product of normals
+    double *c = new double[3]{
+            n1[1]*n2[2]-n1[2]*n2[1],
+            n1[2]*n2[0]-n1[0]*n2[2],
+            n1[0]*n2[1]-n1[1]*n2[0]
+    };
+    // compare that cross product to one of the edges
+    // the direction of the edge determines which way round the angle is
+    // generated
+    double *vn = new double[3]{
+            vertices[edge.first][0] - vertices[edge.second][0],
+            vertices[edge.first][1] - vertices[edge.second][1],
+            vertices[edge.first][2] - vertices[edge.second][2]
+    };
+    double b = c[0]*vn[0]+c[1]*vn[1]+c[2]*vn[2];
+    if (b < 0) {
+        a = 2*M_PI-a;
+    }
+    delete[] n1;
+    delete[] n2;
+    delete[] c;
+    delete[] vn;
+    return a;
+}
+
+void highestfacesanitycheck(std::vector<double*> const &vertices,
+        std::vector<size_t*> const &faces,
+        size_t highestface,
+        int axis)
+{
+    // since we carefully picked the face with the vertices with the highest x
+    // values, we now know which side of the face faces "outward"
+    // we also rely on the fact that the vertices of each triangle are given
+    // in counter clockwise order when the front is facing the viewer - this
+    // is the case in the wavefront obj format
+    // we check whether the "outward" we think we know agrees with the
+    // triangle vertex order
+    double *normal = calculatenormalofface(vertices, faces, highestface);
+    // now the dot product with the normal and a vector just pointing in x
+    // direction. Because x=[1,0,0], the result is just the x component;
+    double numerator = normal[axis]; // x*normal
+    // the magnitude of x=1, therefore the denominator is only the magnitude
+    // of normal
+    double denominator = sqrt(normal[0]*normal[0]+normal[1]*normal[1]+normal[2]*normal[2]); // ||x||*||normal||
+    // the angle is the inverse cosine of the division
+    double angle = acos(numerator/denominator)*180.0/M_PI;
+    std::cout << "angle between normal and \"up\" vector (should be less than 90 deg): " << angle << std::endl;
+
+}
+
+// this method cannot extract the surface in case that the volumes defined by
+// two meshes intersect without sharing vertices
+void surfacewalker(std::vector<double*> const &vertices,
+        std::vector<size_t*> const &faces,
+        std::map<std::pair<size_t, size_t>, std::vector<std::pair<size_t,size_t>>> const &edgetofaces,
+        std::vector<std::unordered_set <size_t>> const &vertextoneighbors)
+{
+    // axis for which to search for the max
+    int axis = 0;
+    // find an outer vertex
+    // take the highest vertex
+    size_t highestidx = 0;
+    for (size_t i = 1; i < vertices.size(); ++i) {
+        if (vertices[i][axis] > vertices[highestidx][axis])
+            highestidx = i;
+    }
+    // of that vertex, find the edge to the vertex which is second highest
+    auto vnit = vertextoneighbors[highestidx].begin();
+    size_t sndhighestidx = *vnit;
+    ++vnit;
+    for (; vnit != vertextoneighbors[highestidx].end(); ++vnit) {
+        if (vertices[*vnit][axis] > vertices[sndhighestidx][axis])
+            sndhighestidx = *vnit;
+    }
+    // of all faces that this edge is part of, find the one with the highest
+    // third vertex
+    std::pair<size_t, size_t> highestedge = std::make_pair(highestidx,sndhighestidx);
+    auto efit = edgetofaces.at(highestedge).begin();
+    size_t highestface = efit->first;
+    size_t trdhighestidx = efit->second;
+    ++efit;
+    for (; efit != edgetofaces.at(highestedge).end(); ++efit) {
+        if (vertices[efit->second][axis] > vertices[trdhighestidx][axis]) {
+            highestface = efit->first;
+            trdhighestidx = efit->second;
+        }
+    }
+    highestfacesanitycheck(vertices, faces, highestface, axis);
+    // now that we have the seed, fill the following unordered_set of faces
+    // with the result of the following walk
+    std::vector<size_t> surface;
+    std::vector<size_t> tocheck;
+    std::unordered_set<size_t> checked;
+    surface.push_back(highestface);
+    tocheck.push_back(highestface);
+    checked.insert(highestface);
+    while (tocheck.size()) {
+        size_t currentface = tocheck.back();
+        tocheck.pop_back();
+        // go through all edges (counterclockwise) and for each edge add the
+        // face with the lowest angle to the current face
+        
+        size_t v0 = faces[currentface][0];
+        size_t v1 = faces[currentface][1];
+        size_t v2 = faces[currentface][2];
+
+        std::vector<std::pair<size_t,size_t>> edges = {
+            std::make_pair(v0,v1),
+            std::make_pair(v1,v2),
+            std::make_pair(v2,v0)
+        };
+
+        for (auto it = edges.begin(); it != edges.end(); ++it) {
+            std::vector<std::pair<size_t,size_t>> edgefaces = edgetofaces.at(*it);
+            auto it1 = edgefaces.begin();
+            size_t face = it1->first;
+            if (face == currentface) {
+                ++it1;
+                if (it1 == edgefaces.end()) { // this edge is only connected to a single face
+                    continue;
+                }
+                face = it1->first;
+            }
+            // only start calculating the angle if this edge is attached to
+            // more than two faces - we then need to calculate angles to make
+            // a decision
+            if (edgefaces.size() > 2) {
+                double angle = calculateanglebetweenfaces(vertices, faces, currentface, face, *it);
+                ++it1;
+                for (;it1 != edgefaces.end();++it1) {
+                    if (it1->first == currentface) // skip the current face
+                        continue;
+                    double newangle = calculateanglebetweenfaces(vertices, faces, currentface, it1->first, *it);
+                    if (newangle < angle) {
+                        angle = newangle;
+                        face = it1->first;
+                    }
+                }
+            }
+            if (checked.count(face)) {
+                // this face was already processed: do nothing
+            } else {
+                surface.push_back(face);
+                tocheck.push_back(face);
+                checked.insert(face);
+            }
+        }
+    }
+    std::cout << "number of faces on surface: " << surface.size() << std::endl;
+}
+
 int main(int argc, char** argv)
 {
     std::string filename;
     parse_options(argc, argv, filename);
     std::vector<double*> vertices;
     std::vector<size_t*> faces;
+
     read_obj(filename, vertices, faces);
-    get_concave_hull(vertices, faces);
+    std::cerr << "number of vertices: " << vertices.size() << std::endl;
+    std::cerr << "number of faces: " << faces.size() << std::endl;
+
+    /* a map mapping an edge (index pair of vertex numbers) to a pair of a face
+     * (index of the face) and the vertex number of the face that the edge is
+     * not part of */
+    std::map<std::pair<size_t, size_t>, std::vector<std::pair<size_t,size_t>>> edgetofaces;
+    /* a vector assigning to each index representing a vertex number a set of
+     * indices representing the neighbor vertices to which it is connected by
+     * edges */
+    std::vector<std::unordered_set <size_t>> vertextoneighbors;
+
+    fill_datastructures(vertices, faces, edgetofaces, vertextoneighbors);
+
+    size_t edgeswithmorethantwofaces = 0;
+    for (auto it = edgetofaces.begin();
+            it != edgetofaces.end(); ++it) {
+        if (it->second.size() > 2)
+            edgeswithmorethantwofaces++;
+    }
+    std::cerr << "size of map from edge to face (both edge directions): " << edgetofaces.size() << std::endl;
+    std::cerr << "edges with more than two faces: " << edgeswithmorethantwofaces << std::endl;
+    surfacewalker(vertices, faces, edgetofaces, vertextoneighbors);
+    //get_concave_hull(vertices, faces);
 }
