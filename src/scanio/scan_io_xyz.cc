fea600667593fc765aa3436c9f9fc0a5c3877b47
josch1337 <josch1337@51f1e6e7-7289-49ad-8748-f09e43fc6daa>
2014-02-18 18:27:32 +0000

    First batch of changes for new scanio: unifying functionality
     - added helper library which implements common functionality
         * transform functions for different input formats
         * common readDirectory
         * common readPose
         * test function for the uos header
             - only skip the first line if it really contains the uos header
         * generalized ASCII data file reading
             - do not read over newlines
             - understand multiple number formats including scientific and
               hexadecimal floating point
             - understand mixed and multiple whitespace separators
             - understand unix and windows line terminators
             - error out on garbage or trailing garbage
             - skip empty lines (including whitespace only)
             - allow leading and trailing whitespace
             - allow comments after a # character
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/slam6d/code/trunk@887 51f1e6e7-7289-49ad-8748-f09e43fc6daa

--- src/scanio/scan_io_xyz.cc
+++ src/scanio/scan_io_xyz.cc
@@ -17,6 +17,7 @@
  */
 
 #include "scanio/scan_io_xyz.h"
+#include "scanio/helper.h"
 
 #include <iostream>
 using std::cout;
@@ -43,53 +44,15 @@ std::list<std::string> ScanIO_xyz::readDirectory(const char* dir_path,
 						  unsigned int start, 
 						  unsigned int end)
 {
-  std::list<std::string> identifiers;
-  for(unsigned int i = start; i <= end; ++i) {
-    // identifier is /d/d/d (000-999)
-    std::string identifier(to_string(i,3));
-    // scan consists of data (.3d) and pose (.pose) files
-    path data(dir_path);
-    data /= path(std::string(DATA_PATH_PREFIX) + identifier + DATA_PATH_SUFFIX);
-    path pose(dir_path);
-    pose /= path(std::string(POSE_PATH_PREFIX) + identifier + POSE_PATH_SUFFIX);
-    // stop if part of a scan is missing or end by absence is detected
-    if(!exists(data) || !exists(pose))
-      break;
-    identifiers.push_back(identifier);
-  }
-  return identifiers;
+    const char* suffixes[2] = { DATA_PATH_SUFFIX, NULL };
+    return readDirectoryHelper(dir_path, start, end, suffixes);
 }
 
 void ScanIO_xyz::readPose(const char* dir_path, 
 			   const char* identifier, 
 			   double* pose)
 {
-  unsigned int i;
-
-  path pose_path(dir_path);
-  pose_path /= path(std::string(POSE_PATH_PREFIX) 
-		    + identifier + 
-		    POSE_PATH_SUFFIX);
-  if(!exists(pose_path))
-    throw std::runtime_error(std::string("There is no pose file for [") 
-			     + identifier + "] in [" + dir_path + "]");
-
-  // open pose file
-  ifstream pose_file(pose_path);
-
-  // if the file is open, read contents
-  if(pose_file.good()) {
-    // read 6 plain doubles
-    for(i = 0; i < 6; ++i) pose_file >> pose[i];
-    pose_file.close();
-
-    // convert angles from deg to rad
-    for(i = 3; i < 6; ++i) pose[i] = rad(pose[i]);
-  } else {
-    throw std::runtime_error(std::string("Pose file could not be opened for [")
-			     + identifier + "] in [" 
-			     + dir_path + "]");
-  }
+    readPoseHelper(dir_path, identifier, pose);
 }
 
 bool ScanIO_xyz::supports(IODataType type)
@@ -108,48 +71,29 @@ void ScanIO_xyz::readScan(const char* dir_path,
 			   std::vector<int>* type, 
 			   std::vector<float>* deviation)
 {
-  unsigned int i;
+    // error handling
+    path data_path(dir_path);
+    data_path /= path(std::string(DATA_PATH_PREFIX) 
+            + identifier 
+            + DATA_PATH_SUFFIX);
+    if(!exists(data_path))
+        throw std::runtime_error(std::string("There is no scan file for [") 
+                + identifier + "] in [" 
+                + dir_path + "]");
 
-  // error handling
-  path data_path(dir_path);
-  data_path /= path(std::string(DATA_PATH_PREFIX) 
-		    + identifier 
-		    + DATA_PATH_SUFFIX);
-  if(!exists(data_path))
-    throw std::runtime_error(std::string("There is no scan file for [") 
-			     + identifier + "] in [" 
-			     + dir_path + "]");
+    if(xyz != 0) {
+        // open data file
+        ifstream data_file(data_path);
+        data_file.exceptions(ifstream::eofbit|ifstream::failbit|ifstream::badbit);
 
-  if(xyz != 0) {
-    // open data file
-    ifstream data_file(data_path);
-    data_file.exceptions(ifstream::eofbit|ifstream::failbit|ifstream::badbit);
+        uosHeaderTest(data_file);
 
-    // overread the first line ignoring the header information
-    char dummy[255];
-    data_file.getline(dummy, 255);
+        IODataType spec[4] = { DATA_XYZ, DATA_XYZ, DATA_XYZ, DATA_TERMINATOR };
+        ScanDataTransform_xyz transform;
+        readASCII(data_file, spec, transform, filter, xyz);
 
-    // read points and reflectance/intensity/temperature value
-    double point[3];
-    float reflection;
-    while(data_file.good()) {
-      try {
-        data_file >> point[2];
-        data_file >> point[0];
-        data_file >> point[1];
-        point[0] *= -1.0;
-      } catch(std::ios_base::failure& e) {
-        break;
-      }
-
-      // apply filter then insert point and reflectance
-      if(filter.check(point)) {
-        for(i = 0; i < 3; ++i) xyz->push_back(point[i]);
-        reflectance->push_back(reflection);
-      }
+        data_file.close();
     }
-    data_file.close();
-  }
 }
 
 
