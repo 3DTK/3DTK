1fd87f568cbaee6c80879b75dd0862c214216e68
Johannes 'josch' Schauer <josch@mister-muffin.de>
2014-12-06 11:06:10 +0000

    update frames2camera.py - this script should be allow more parameters
    git-svn-id: svn://svn.code.sf.net/p/slam6d/code/trunk@949 51f1e6e7-7289-49ad-8748-f09e43fc6daa

--- src/collision/utils/frames2camera.py
+++ src/collision/utils/frames2camera.py
@@ -4,8 +4,9 @@ from math import sqrt
 
 def pairwise(iterable):
     a, b = tee(iterable)
-    for i in range(100):
-        next(b, None)
+    #for i in range(100):
+    #    next(b, None)
+    next(b, None)
     return izip(a, b)
 
 import sys
@@ -13,6 +14,8 @@ import sys
 poses = list()
 
 for line in sys.stdin:
+    if line.startswith('#'):
+        continue
     x,y,z = line.split()[12:15]
     poses.append((float(x),float(y),-float(z)))
 
@@ -25,7 +28,7 @@ for pos1,pos2 in pairwise(poses):
         continue
     lines += 1
     back = ((pos1[0]-pos2[0])/length,(pos1[1]-pos2[1])/length,(pos1[2]-pos2[2])/length)
-    outdata += "%s\n%s\n%s\n"%(pos1[0]+20*back[0],pos1[1]+20*back[1]+3.0,pos1[2]+20*back[2])
+    outdata += "%s\n%s\n%s\n"%(pos1[0]+800*back[0],pos1[1]+800*back[1]+400.0,pos1[2]+800*back[2])
     outdata += "%s\n%s\n%s\n"%pos1
     outdata += "%s\n%s\n%s\n"%(pos1[0],pos1[1]+1,pos1[2])
 
