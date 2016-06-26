8ad5ee66b15a746249c4f747bbe3381a73ace7cc
Johannes 'josch' Schauer <josch@mister-muffin.de>
2016-06-26 14:57:18 +0000

    add python bindings
    git-svn-id: svn://svn.code.sf.net/p/slam6d/code/trunk@1158 51f1e6e7-7289-49ad-8748-f09e43fc6daa

---
+++ bindings/python/test.py
@@ -0,0 +1,22 @@
+import py3dtk
+from math import sqrt
+
+query = (10,0,0)
+dist2 = 4
+def dist(p):
+    a = query[0] - p[0]
+    b = query[1] - p[1]
+    c = query[2] - p[2]
+    return sqrt(a*a+b*b+c*c)
+
+py3dtk.openDirectory(False, "dat", py3dtk.IOType.UOS, 0, -1)
+points=list()
+for s in py3dtk.allScans:
+    xyz = py3dtk.DataXYZ(s.getByString("xyz"))
+    points.extend(xyz)
+
+kdtree = py3dtk.KDtreeIndexed(points)
+res = kdtree.fixedRangeSearch(query, dist2)
+
+res = sorted([points[i] for i in res], key=dist)
+print(["%.03f"%dist(p) for p in res])
