c7b8ff2743e944ff08b258ac8bc8d0ace4495e1b
josch1337 <josch1337@51f1e6e7-7289-49ad-8748-f09e43fc6daa>
2016-08-22 14:26:13 +0000

    peopleremover: make C++ implementation functionally equivalent to the correct Python implementation
    Lessons:
    
     - The C/C++ % operator does *not* calculate the modulo but instead the
       remainder. This it behaves differently depending on the sign of the
       input...
    
     - Integer division in C/C++ behaves differently depending on the sign
       of the result and the sign of the divisor. There is thus a
       discontinuity around zero...
    
     - Don't just naively expect that when writing (a - b) in Python you
       could do the same in C/C++ and get the same result. Depending on the
       datatypes of a and b you might actually underflow the types...
    
     - Choose your data types wisely. Your compiler will not complain when you
       store negative numbers in an unsigned integer...
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/slam6d/code/trunk@1191 51f1e6e7-7289-49ad-8748-f09e43fc6daa

--- src/peopleremover/peopleremover.cc
+++ src/peopleremover/peopleremover.cc
@@ -27,20 +27,41 @@ namespace std
 	};
 };
 
-void voxel_of_point(double x, double y, double z, double voxel_size, ssize_t *X, ssize_t *Y, ssize_t *Z)
+/*
+ * Integer division and modulo in C/C++ suck. We were told that the % operator
+ * is the modulo but it is actually not. It calculates the remainder and not
+ * the modulo. Furthermore, the % operator is incapable of operating on
+ * floating point values. Lastly, even integer division is screwed up because
+ * it rounds into different directions depending on whether the result is
+ * positive or negative...
+ *
+ * We thus implement the sane and mathematically more correct behaviour of
+ * Python here.
+ */
+ssize_t py_div(double a, double b)
 {
-	*X = x/voxel_size;
-	*Y = y/voxel_size;
-	*Z = z/voxel_size;
+	ssize_t q = a/b;
+	double r = fmod(a,b);
+	if ((r != 0) && ((r < 0) != (b < 0))) {
+		q -= 1;
+	}
+	return q;
 }
 
-double get_fraction(double x)
+double py_mod(double a, double b)
 {
-	if (x < 0) {
-		return (long)x-x-1;
-	} else {
-		return x-(long)x;
+	double r = fmod(a, b);
+	if (r!=0 && ((r<0) != (b<0))) {
+		r += b;
 	}
+	return r;
+}
+
+void voxel_of_point(double x, double y, double z, double voxel_size, ssize_t *X, ssize_t *Y, ssize_t *Z)
+{
+	*X = py_div(x, voxel_size);
+	*Y = py_div(y, voxel_size);
+	*Z = py_div(z, voxel_size);
 }
 
 std::set<std::tuple<ssize_t,ssize_t,ssize_t>> walk_voxels(
@@ -79,11 +100,13 @@ std::set<std::tuple<ssize_t,ssize_t,ssize_t>> walk_voxels(
 	}
 	ssize_t X, Y, Z;
 	voxel_of_point(start[0],start[1],start[2],voxel_size, &X, &Y, &Z);
+	ssize_t startX = X, startY = Y, startZ = Z;
 	ssize_t endX, endY, endZ;
 	voxel_of_point(end[0],end[1],end[2],voxel_size, &endX, &endY, &endZ);
-	double tDeltaX, stepX, tMaxX;
-	double tDeltaY, stepY, tMaxY;
-	double tDeltaZ, stepZ, tMaxZ;
+	double tDeltaX, tMaxX;
+	double tDeltaY, tMaxY;
+	double tDeltaZ, tMaxZ;
+	char stepX, stepY, stepZ;
 	if (direction[0] == 0) {
 		tDeltaX = 0;
 		stepX = 0;
@@ -91,7 +114,7 @@ std::set<std::tuple<ssize_t,ssize_t,ssize_t>> walk_voxels(
 	} else {
 		stepX = direction[0] > 0 ? 1 : -1;
 		tDeltaX = stepX*voxel_size/direction[0];
-		tMaxX = tDeltaX * (1.0 - get_fraction(stepX*start[0]/voxel_size));
+		tMaxX = tDeltaX * (1.0 - py_mod(stepX*(start[0]/voxel_size), 1.0));
 	}
 	if (direction[1] == 0) {
 		tDeltaY = 0;
@@ -100,7 +123,7 @@ std::set<std::tuple<ssize_t,ssize_t,ssize_t>> walk_voxels(
 	} else {
 		stepY = direction[1] > 0 ? 1 : -1;
 		tDeltaY = stepY*voxel_size/direction[1];
-		tMaxY = tDeltaY * (1.0 - get_fraction(stepY*start[1]/voxel_size));
+		tMaxY = tDeltaY * (1.0 - py_mod(stepY*(start[1]/voxel_size), 1.0));
 	}
 	if (direction[2] == 0) {
 		tDeltaZ = 0;
@@ -109,7 +132,7 @@ std::set<std::tuple<ssize_t,ssize_t,ssize_t>> walk_voxels(
 	} else {
 		stepZ = direction[2] > 0 ? 1 : -1;
 		tDeltaZ = stepZ*voxel_size/direction[2];
-		tMaxZ = tDeltaZ * (1.0 - get_fraction(stepZ*start[2]/voxel_size));
+		tMaxZ = tDeltaZ * (1.0 - py_mod(stepZ*(start[2]/voxel_size), 1.0));
 	}
 	if (stepX == -1 && tMaxX == tDeltaX) {
 		tMaxX = 0.0;
@@ -120,6 +143,7 @@ std::set<std::tuple<ssize_t,ssize_t,ssize_t>> walk_voxels(
 	if (stepZ == -1 && tMaxZ == tDeltaZ) {
 		tMaxZ = 0.0;
 	}
+	size_t multX = 0, multY = 0, multZ = 0;
 	std::set<std::tuple<ssize_t,ssize_t,ssize_t>> empty_voxels;
 	double epsilon = 1e-13;
 	while (X != endX || Y != endY || Z != endZ) {
@@ -132,18 +156,22 @@ std::set<std::tuple<ssize_t,ssize_t,ssize_t>> walk_voxels(
 		}
 		if (tMaxX < tMaxY) {
 			if (tMaxX < tMaxZ) {
-				X += stepX;
+				multX += 1;
+				X = startX + multX*stepX;
 				tMaxX += tDeltaX;
 			} else {
-				Z += stepZ;
+				multZ += 1;
+				Z = startZ + multZ*stepZ;
 				tMaxZ += tDeltaZ;
 			}
 		} else {
 			if (tMaxY < tMaxZ) {
-				Y += stepY;
+				multY += 1;
+				Y = startY + multY*stepY;
 				tMaxY += tDeltaY;
 			} else {
-				Z += stepZ;
+				multZ += 1;
+				Z = startZ + multZ*stepZ;
 				tMaxZ += tDeltaZ;
 			}
 		}
@@ -160,7 +188,10 @@ std::set<std::tuple<ssize_t,ssize_t,ssize_t>> walk_voxels(
 		} else {
 			bool found = false;
 			for (std::set<size_t>::iterator it = scanslices->second.begin(); it != scanslices->second.end(); ++it) {
-				if (*it >= current_slice - diff && *it <= current_slice + diff) {
+				// it is important that we do not subtract values from *it or
+				// current_slice because both are unsigned values and if they
+				// are too small, then subtraction might underflow them
+				if ((*it + diff >= current_slice) && (*it <= (current_slice + diff))) {
 					found = true;
 					break;
 				}
@@ -177,17 +208,21 @@ std::set<std::tuple<ssize_t,ssize_t,ssize_t>> walk_voxels(
 	return empty_voxels;
 }
 
-int main()
+int main(int argc, char* argv[])
 {
+	size_t start = atoi(argv[1]);
+	size_t end = atoi(argv[2]);
+	char *dir = argv[3];
+
 	double voxel_size = 5;
 	double max_search_distance = 250;
 	size_t diff = 285;
 	double max_target_distance = 1000;
 	double max_target_proximity = 30;
 
-	Scan::openDirectory(false, "../wolfsburg_mim_registered", UOSR, 120000, 155000);
+	Scan::openDirectory(false, dir, UOSR, start, end);
 	if(Scan::allScans.size() == 0) {
-		cerr << "No scans found. Did you use the correct format?" << endl;
+		std::cerr << "No scans found. Did you use the correct format?" << std::endl;
 		exit(-1);
 	}
 
