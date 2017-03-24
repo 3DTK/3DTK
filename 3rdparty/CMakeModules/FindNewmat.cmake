4695ba2910dbc14fe28b8f90e4c7118bae885587
Johannes 'josch' Schauer <josch@mister-muffin.de>
2017-03-24 14:01:19 +0000

    3rdparty/CMakeModules/FindNewmat.cmake: add code to find newmat
    git-svn-id: svn://svn.code.sf.net/p/slam6d/code/trunk@1317 51f1e6e7-7289-49ad-8748-f09e43fc6daa

---
+++ 3rdparty/CMakeModules/FindNewmat.cmake
@@ -0,0 +1,21 @@
+# - Find newmat
+# Find the native newmat includes and library
+# This module defines
+#  NEWMAT_INCLUDE_DIR, where to find newmat/newmat.h, etc.
+#  NEWMAT_LIBRARIES, the libraries needed to use newmat.
+#  NEWMAT_FOUND, If false, do not try to use newmat.
+# also defined, but not for general use are
+#  NEWMAT_LIBRARY, where to find the newmat library.
+
+include(FindPackageHandleStandardArgs)
+
+find_path(NEWMAT_INCLUDE_DIR newmat/newmat.h)
+find_library(NEWMAT_LIBRARY newmat)
+
+find_package_handle_standard_args(NEWMAT DEFAULT_MSG
+	NEwMAT_LIBRARY NEWMAT_INCLUDE_DIR)
+
+if(NEWMAT_FOUND)
+	set(NEWMAT_INCLUDE_DIRS ${NEWMAT_INCLUDE_DIR})
+	set(NEWMAT_LIBRARIES ${NEWMAT_LIBRARY})
+endif()
