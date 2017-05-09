009c1faa74a8e262f5d0cff0bad351718eac7421
bleier <bleier@51f1e6e7-7289-49ad-8748-f09e43fc6daa>
2017-05-09 08:25:53 +0000

    tools: Added rxp tools.
    git-svn-id: svn+ssh://svn.code.sf.net/p/slam6d/code/trunk@1362 51f1e6e7-7289-49ad-8748-f09e43fc6daa

---
+++ src/tools/rxp2gps.cc
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <iomanip>
+#include <boost/filesystem.hpp>
+#include <boost/program_options.hpp>
+#include <Eigen/Eigen>
+
+#define private public
+#define protected public
+
+#include "riegl/scanlib.hpp"
+
+using namespace std;
+using namespace scanlib;
+
+
+class GPS : public scanlib::pointcloud
+{
+public:
+    GPS()
+        : scanlib::pointcloud(true)
+    {}
+protected:
+    void on_hk_gps_hr(const scanlib::hk_gps_hr<iterator_type>& arg) {
+        scanlib::pointcloud::on_hk_gps_hr(arg);
+
+        cout.precision(std::numeric_limits<double>::max_digits10);
+        cout << (double) arg.TOWms * 1e-3 << "," << (double) arg.LAT * 1e-9 << "," << (double) arg.LONG * 1e-9 << "," << (double) arg.HGT_ELL * 1e-3 << endl;
+    }
+};
+
+int main(int argc, char* argv[])
+{
+    boost::program_options::options_description desc("Allowed options");
+    desc.add_options()
+            ("help,h", "produce help message")
+            ("input,i", boost::program_options::value<std::string>(), "input folder")
+            ;
+
+    boost::program_options::variables_map vm;
+    boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
+    boost::program_options::notify(vm);
+
+    if (vm.count("help")) {
+        std::cout << desc << std::endl;
+        return EXIT_SUCCESS;
+    }
+
+    boost::filesystem::path dir(vm["input"].as<std::string>());
+
+    for (boost::filesystem::directory_iterator it(dir); it != boost::filesystem::directory_iterator(); it++) {
+        if (it->path().extension() != ".rxp") { continue; }
+
+        std::string filename = it->path().string();
+
+        cout << filename << endl;
+
+        std::shared_ptr<basic_rconnection> rc;
+        rc = basic_rconnection::create(filename);
+        rc->open();
+        decoder_rxpmarker dec(rc);
+        buffer buf;
+
+        GPS gps;
+
+        for ( dec.get(buf); !dec.eoi() ; dec.get(buf) ) {
+            gps.dispatch(buf.begin(), buf.end());
+        }
+
+        rc->close();
+
+        cout << endl;
+    }
+
+    return 0;
+}
