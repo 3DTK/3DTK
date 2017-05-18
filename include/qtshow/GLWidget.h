4b8dc6fcf6b07021fda03688a85523dfdc2f6659
Johannes Barthelmes <johannes.barthelmes@stud-mail.uni-wuerzburg.de>
2017-05-18 10:23:52 +0000

    qtshow: Add actual functionality to GUI elements
    git-svn-id: svn://svn.code.sf.net/p/slam6d/code/trunk@1389 51f1e6e7-7289-49ad-8748-f09e43fc6daa

--- include/qtshow/GLWidget.h
+++ include/qtshow/GLWidget.h
@@ -7,8 +7,33 @@
 #include <QWidget>
 
 class GLWidget : public QOpenGLWidget, protected QOpenGLFunctions {
+  Q_OBJECT
+
 public:
   GLWidget(QWidget *parent = 0);
+
+public slots:
+  void setDrawPoints(bool drawPoints);
+  void setDrawCameras(bool drawCameras);
+  void setDrawPath(bool drawPath);
+  void setPointSize(int pointSize);
+
+  void setFogType(int fogType);
+  void setFogInverted(bool fogInverted);
+  void setFogDensity(double fogDensity);
+
+  void setColorProperty(int colorProperty);
+  void setColorMinimumValue(double colorMinimum);
+  void setColorMaximumValue(double colorMaximum);
+  void setAutoRecalculateColor(bool autoRecalculate);
+  void recalculateColor();
+  void setColorType(int colorType);
+  void setColorMap(int colorMap);
+  void setColorInverted(bool colorInverted);
+
+signals:
+  void colorMinimumValueChanged(double);
+  void colorMaximumValueChanged(double);
 
 protected:
   void initializeGL();
@@ -17,9 +42,17 @@ protected:
 
   void mousePressEvent(QMouseEvent *event);
   void mouseMoveEvent(QMouseEvent *event);
+  void updateFogType();
+
 
 protected:
   QPoint lastMousePos;
+
+  int fogType;
+  bool fogInverted;
+
+  bool autoRecalculateColor;
+
 };
 
 #endif
--- src/qtshow/MainWindow.cc
+++ src/qtshow/MainWindow.cc
@@ -1,5 +1,4 @@
 #include "qtshow/MainWindow.h"
-#include "ui_MainWindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
   : QMainWindow(parent)
