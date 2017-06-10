2a296e132d9ae93d3e3548c64dd155ea41ef455f
pjanotta <pjanotta@51f1e6e7-7289-49ad-8748-f09e43fc6daa>
2017-06-10 13:26:53 +0000

    Removed b3d format from standard built - support depends on new WITH_B3D compile option now.
    git-svn-id: svn+ssh://svn.code.sf.net/p/slam6d/code/trunk@1420 51f1e6e7-7289-49ad-8748-f09e43fc6daa

--- include/scanio/scan_io_b3d.h
+++ include/scanio/scan_io_b3d.h
@@ -1,17 +1,16 @@
 /**
  * @file
- * @brief IO of a 3D scan in uos file format
+ * @brief IO of a 3D scan in b3d file format
  */
 
 #ifndef __SCAN_IO_B3D_H__
 #define __SCAN_IO_B3D_H__
 
 #include "scan_io.h"
-
-
+#include "b3dpsreader.h"
 
 /**
- * @brief 3D scan loader for UOS scans
+ * @brief 3D scan loader for B3D scans
  *
  * The compiled class is available as shared object file
  */
@@ -30,13 +29,17 @@ class ScanIO_b3d : public ScanIO {
     int temperatureidx;
     int typeidx;   
 
+    static B3DPSReader *inB3DPS;
+
 public:
-  virtual std::list<std::string> readDirectory(const char* dir_path, unsigned int start, unsigned int end);
+	virtual ~ScanIO_b3d();
+	virtual std::list<std::string> readDirectory(const char* dir_path, unsigned int start, unsigned int end);
   virtual void readPose(const char* dir_path, const char* identifier, double* pose);
+  virtual void readPose(const char* dir_path, const char* identifier, double* pose, double* timestamp);
   virtual time_t lastModified(const char* dir_path, const char* identifier);
-  virtual void readScan(const char* dir_path, const char* identifier, PointFilter& filter, std::vector<double>* xyz, std::vector<unsigned char>* rgb, std::vector<float>* reflectance, std::vector<float>* temperature, std::vector<float>* amplitude, std::vector<int>* type, std::vector<float>* deviation);
-  virtual bool supports(IODataType type);
-  bool readEOL(std::ifstream &f);
+	virtual void readScan(const char* dir_path, const char* identifier, PointFilter& filter, std::vector<double>* xyz, std::vector<unsigned char>* rgb, std::vector<float>* reflectance, std::vector<float>* temperature, std::vector<float>* amplitude, std::vector<int>* type, std::vector<float>* deviation);
+	virtual bool supports(IODataType type);
+	bool readEOL(std::ifstream &f);
 };
 
 #endif
