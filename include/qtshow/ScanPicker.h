596af55abaee46fd14041e6c82f65f162814388a
Johannes Barthelmes <johannes.barthelmes@stud-mail.uni-wuerzburg.de>
2017-06-18 22:02:57 +0000

    qtshow: Add basic settings to directory picker
    git-svn-id: svn://svn.code.sf.net/p/slam6d/code/trunk@1430 51f1e6e7-7289-49ad-8748-f09e43fc6daa

---
+++ include/qtshow/ScanPicker.h
@@ -0,0 +1,22 @@
+#ifndef __SCANPICKER_H__
+#define __SCANPICKER_H__
+
+#include <QDialog>
+
+#include "ui_ScanPicker.h"
+
+class ScanPicker : public QDialog, private Ui::ScanPicker {
+  Q_OBJECT
+
+public:
+  ScanPicker(const QString& current_directory, QWidget *parent = Q_NULLPTR, Qt::WindowFlags f = Qt::WindowFlags());
+
+signals:
+  void scanPicked(QString dir, QString format, int start, int end);
+
+public slots:
+  void accept();
+  void chooseScanDirectory();
+};
+
+#endif
---
+++ src/qtshow/ScanPicker.cc
@@ -0,0 +1,27 @@
+#include <QFileDialog>
+
+#include "qtshow/ScanPicker.h"
+
+
+ScanPicker::ScanPicker(const QString& current_directory, QWidget *parent, Qt::WindowFlags f)
+  : QDialog(parent, f)
+{
+  setupUi(this);
+  lineEditDirectory->setText(current_directory);
+}
+
+void ScanPicker::accept() {
+  emit scanPicked(
+    lineEditDirectory->text(),
+    comboBoxFormat->currentText(),
+    spinBoxStart->value(),
+    spinBoxEnd->value()
+  );
+  QDialog::accept();
+}
+
+void ScanPicker::chooseScanDirectory() {
+  QString dir = QFileDialog::getExistingDirectory(this, "Choose Scan Directory…",
+    lineEditDirectory->text());
+  lineEditDirectory->setText(dir);
+}
