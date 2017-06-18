31129360654c2543b1f43229d7b62bbc57ef9170
jbarthelmes <jbarthelmes@51f1e6e7-7289-49ad-8748-f09e43fc6daa>
2017-06-18 18:06:27 +0000

    qtshow: Implement directory picker
    It is now possible to pick a new scan to be displayed with QtShow.
    This feature is not fully tested and not fully functional yet. The
    new scan has to be in the same format as the old one, for instance.
    
    git-svn-id: https://svn.code.sf.net/p/slam6d/code/trunk@1429 51f1e6e7-7289-49ad-8748-f09e43fc6daa

--- src/qtshow/QtShow.cc
+++ src/qtshow/QtShow.cc
@@ -7,9 +7,6 @@ QtShow::QtShow(int &argc, char **argv)
 {
   glutInit(&argc, argv);
 
-  dataset_settings ds;
-  window_settings ws;
-
   try {
     parse_args(argc, argv, ds, ws);
   } catch (std::exception e) {
@@ -21,4 +18,18 @@ QtShow::QtShow(int &argc, char **argv)
 
   mainWindow = new MainWindow(ws);
   mainWindow->show();
+
+  connect(mainWindow, &MainWindow::scanDirectoryChanged, this, &QtShow::loadDifferentScans);
+}
+
+void QtShow::loadDifferentScans(QString dir) {
+  // TODO turn this into proper context handling logic for show
+  Scan::closeDirectory();
+  for (colordisplay* tree : octpts) {
+    delete tree;
+  }
+  octpts.clear();
+  ds.input_directory = dir.toStdString();
+  ds.scale = 1.0;
+  initShow(ds, ws);
 }
