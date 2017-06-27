9fc45cfe7a945699c1dc687b615e4d6419b4b328
jbarthelmes <jbarthelmes@51f1e6e7-7289-49ad-8748-f09e43fc6daa>
2017-06-27 21:47:54 +0000

    qtshow: Modify a dataset_settings directly in ScanPicker
    The resulting dataset_settings is then passed by value via signal to the ShowApplication object, which switches to the desired scan.
    Now we can easily extend this dialog.
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/slam6d/code/trunk@1434 51f1e6e7-7289-49ad-8748-f09e43fc6daa

--- include/qtshow/MainWindow.h
+++ include/qtshow/MainWindow.h
@@ -1,24 +1,25 @@
 #ifndef __MAINWINDOW_H__
 #define __MAINWINDOW_H__
 
-#include <QMainWindow>
+#include "ui_MainWindow.h"
 
 #include "show/program_options.h"
 
-#include "ui_MainWindow.h"
 
 class MainWindow : public QMainWindow, private Ui::MainWindow {
   Q_OBJECT
 
 public:
-  MainWindow(const window_settings& ws, QWidget *parent = Q_NULLPTR, Qt::WindowFlags flags = Qt::WindowFlags());
+  MainWindow(const dataset_settings& ds, const window_settings& ws, QWidget *parent = Q_NULLPTR, Qt::WindowFlags flags = Qt::WindowFlags());
 
 signals:
-  void scanDirectoryOpened(QString dir, QString format, int start, int end, double scale);
+  void scanDirectoryOpened(dataset_settings);
 
 public slots:
   void openScanDirectory();
-  void scanPicked(QString dir, QString format, int start, int end, double scale);
+
+protected:
+  dataset_settings ds;
 };
 
 #endif
