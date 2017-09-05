4bd956529cb56a173659a1dea3f80e017c16af38
Johannes 'josch' Schauer <josch@mister-muffin.de>
2017-09-05 08:03:22 +0000

    spherical_quadtree: there is no need to compute the triple product 12 times because we already know that the point is in the parent triangle
    git-svn-id: svn://svn.code.sf.net/p/slam6d/code/trunk@1511 51f1e6e7-7289-49ad-8748-f09e43fc6daa

--- src/spherical_quadtree/spherical_quadtree.cc
+++ src/spherical_quadtree/spherical_quadtree.cc
@@ -94,6 +94,23 @@ QuadNode::QuadNode(size_t v1, size_t v2, size_t v3, std::vector<size_t> const& _
 	std::vector<size_t> indices1, indices2, indices3, indices4;
 	for (size_t i : _indices) {
 		double p[3] = {_pts[i][0], _pts[i][1], _pts[i][2]};
+		/*
+		 * this version is nearly functionally identical to the more
+		 * computationally expensive version below except that the rare
+		 * situation that a point does not fit into any of the four
+		 * triangles due to floating point inaccuracy cannot arise
+		 * anymore.
+		 */
+		if (tripleproduct(w4,w6,p) >= 0) {
+			indices1.push_back(i);
+		} else if (tripleproduct(w5,w4,p) >= 0) {
+			indices2.push_back(i);
+		} else if (tripleproduct(w6,w5,p) >= 0) {
+			indices3.push_back(i);
+		} else {
+			indices4.push_back(i);
+		}
+		/*
 		if (tripleproduct(w1,w4,p) >= 0 && tripleproduct(w4,w6,p) >= 0 && tripleproduct(w6,w1,p) >= 0) {
 			indices1.push_back(i);
 		} else if (tripleproduct(w2,w5,p) >= 0 && tripleproduct(w5,w4,p) >= 0 && tripleproduct(w4,w2,p) >= 0) {
@@ -106,6 +123,7 @@ QuadNode::QuadNode(size_t v1, size_t v2, size_t v3, std::vector<size_t> const& _
 			std::cerr << "impossible for " << p[0] << "," << p[1] << "," << p[2] << std::endl;
 			exit(1);
 		}
+		*/
 	}
 	t1 = std::make_unique<QuadNode>(QuadNode(v1,v4,v6,indices1, _pts, vertices, middlemap));
 	t2 = std::make_unique<QuadNode>(QuadNode(v2,v5,v4,indices2, _pts, vertices, middlemap));
