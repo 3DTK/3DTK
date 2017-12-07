3da4b7f301509c888137de2042e79b9261c20907
bleier <bleier@51f1e6e7-7289-49ad-8748-f09e43fc6daa>
2017-12-07 12:53:33 +0000

    kitti2scan: do not use full path for include.
    git-svn-id: https://svn.code.sf.net/p/slam6d/code/trunk@1557 51f1e6e7-7289-49ad-8748-f09e43fc6daa

--- src/slam6d/kitti2scan.cc
+++ src/slam6d/kitti2scan.cc
@@ -21,7 +21,7 @@ using std::ios;
 #include "slam6d/globals.icc"
 #include <string.h>
 #include <string>
-#include </usr/include/c++/5/bits/stringfwd.h>
+#include <bits/stringfwd.h>
 #ifndef _MSC_VER
 #include <unistd.h>
 #else
