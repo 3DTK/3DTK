377093fcb6000c379cb537cbd366105e72e91965
jlucht <jlucht@51f1e6e7-7289-49ad-8748-f09e43fc6daa>
2018-03-05 20:09:15 +0000

    Detector: add function for chessboard detection
    git-svn-id: https://svn.code.sf.net/p/slam6d/code/trunk@1621 51f1e6e7-7289-49ad-8748-f09e43fc6daa

--- src/calibration/Detector.cpp
+++ src/calibration/Detector.cpp
@@ -6,7 +6,7 @@
 #include"calibration/Detector.h"
 
 namespace calibration {
-    void Detector::detectAprilTag(image_u8_t *image, std::vector<AprilTag::AprilTag2f> *tags, float decimate = 1.,
+    void Detector::detectAprilTag(image_u8_t *image, std::vector<AprilTag::AprilTag2f> &tags, float decimate = 1,
                                   float blur = 0.8,
                                   int threads = 4, bool debug = false, bool refine_edges = true,
                                   bool refine_decodes = true,
@@ -52,15 +52,30 @@ namespace calibration {
                 apriltag_detection_t *det;
                 zarray_get(detections, i, &det);
                 AprilTag::AprilTag2f aprilTag2f = AprilTag::AprilTag2f(det->id);
-                aprilTag2f.point4 = Point2f((float)((det->p)[0][0]), (float)((det->p)[0][1]));
-                aprilTag2f.point3 = Point2f((float)((det->p)[1][0]), (float)((det->p)[1][1]));
-                aprilTag2f.point2 = Point2f((float)((det->p)[2][0]), (float)((det->p)[2][1]));
-                aprilTag2f.point1 = Point2f((float)((det->p)[3][0]), (float)((det->p)[3][1]));
-                tags->push_back(aprilTag2f);
+                aprilTag2f.point4 = Point2f((float) ((det->p)[0][0]), (float) ((det->p)[0][1]));
+                aprilTag2f.point3 = Point2f((float) ((det->p)[1][0]), (float) ((det->p)[1][1]));
+                aprilTag2f.point2 = Point2f((float) ((det->p)[2][0]), (float) ((det->p)[2][1]));
+                aprilTag2f.point1 = Point2f((float) ((det->p)[3][0]), (float) ((det->p)[3][1]));
+                tags.push_back(aprilTag2f);
                 apriltag_detection_destroy(det);
             }
             zarray_destroy(detections);
         }
         apriltag_detector_destroy(apriltagDetector);
+    }
+
+    void Detector::detectChessboard(Mat &image, std::vector<cv::Point2f> &points, Size boardSize) {
+        bool found = findChessboardCorners(image, boardSize, points,
+                                           CV_CALIB_CB_ADAPTIVE_THRESH | CV_CALIB_CB_NORMALIZE_IMAGE |
+                                           CV_CALIB_CB_FILTER_QUADS);
+        timeval start, end;
+        if (found) {
+            std::cout << "Performing subpixel refinement of chessboard corners." << std::endl;
+            cornerSubPix(image, points, cvSize(11, 11), cvSize(-1, -1),
+                         cvTermCriteria(CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 30, 0.1));
+        }
+
+        gettimeofday(&end, 0);
+        std::cout << "Time to detect chessboard: " << end.tv_sec - start.tv_sec << " sec" << std::endl;
     }
 }
