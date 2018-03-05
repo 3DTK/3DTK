efa223c51a7851e94afb3fc46aea779ef08cd436
jlucht <jlucht@51f1e6e7-7289-49ad-8748-f09e43fc6daa>
2018-03-05 20:09:25 +0000

    Detector: add function for chessboard detection
    git-svn-id: svn+ssh://svn.code.sf.net/p/slam6d/code/trunk@1622 51f1e6e7-7289-49ad-8748-f09e43fc6daa

--- include/calibration/Detector.h
+++ include/calibration/Detector.h
@@ -21,9 +21,9 @@ namespace calibration {
     private:
 
     public:
-        void detectChessboard(image_u8_t *image);
+        void detectChessboard(Mat &image, std::vector<cv::Point2f> &points, Size boardSize);
 
-        void detectAprilTag(image_u8_t *image, std::vector<AprilTag::AprilTag2f> *tags, float decimate = 1.,
+        void detectAprilTag(image_u8_t *image, std::vector<AprilTag::AprilTag2f> &tags, float decimate = 1,
                             float blur = 0.8,
                             int threads = 4, bool debug = false, bool refine_edges = true, bool refine_decodes = true,
                             bool refine_pose = true, std::string tagFamily = "tag36h11");
