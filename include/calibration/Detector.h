4a72f11a248df1251fb315d7d921b6e82853465f
Joschka van der Lucht <joschka@vanderlucht.de>
2018-03-05 20:08:56 +0000

    calibraion: start develop caltools v2; add new class for detector
    git-svn-id: svn://svn.code.sf.net/p/slam6d/code/trunk@1619 51f1e6e7-7289-49ad-8748-f09e43fc6daa

---
+++ include/calibration/Detector.h
@@ -0,0 +1,32 @@
+//
+// Created by Joschka van der Lucht on 28.02.18.
+//
+
+#ifndef INC_3DTK_DETECTOR_H
+#define INC_3DTK_DETECTOR_H
+
+#include "AprilTag2f.h"
+#include <common/image_types.h>
+#include <apriltag.h>
+#include <tag36h10.h>
+#include <tag36h11.h>
+#include <tag25h9.h>
+#include <tag25h7.h>
+#include <tag16h5.h>
+#include <string>
+#include <iostream>
+
+namespace calibration {
+    class Detector {
+    private:
+
+    public:
+        void detectChessboard(image_u8_t *image);
+
+        void detectAprilTag(image_u8_t *image, std::vector<AprilTag::AprilTag2f> *tags, float decimate = 1.,
+                            float blur = 0.8,
+                            int threads = 4, bool debug = false, bool refine_edges = true, bool refine_decodes = true,
+                            bool refine_pose = true, std::string tagFamily = "tag36h11");
+    };
+}
+#endif //INC_3DTK_DETECTOR_H
