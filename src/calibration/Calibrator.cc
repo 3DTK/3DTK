683d7c0cddbe331e0115236438c06db5d76602c1
jlucht <jlucht@51f1e6e7-7289-49ad-8748-f09e43fc6daa>
2018-03-09 08:05:53 +0000

    calibration: Calibrator, add functions
    git-svn-id: https://svn.code.sf.net/p/slam6d/code/trunk@1642 51f1e6e7-7289-49ad-8748-f09e43fc6daa

--- src/calibration/Calibrator.cc
+++ src/calibration/Calibrator.cc
@@ -3,3 +3,22 @@
 //
 
 #include "Calibrator.cc"
+
+
+namespace calibration{
+    void Calibrator::calibrateIntrinsic2D() {
+
+    }
+
+    void Calibrator::calibrateExtrinsic2D() {
+
+    }
+
+    void Calibrator::calibrateIntrinsic3D() {
+
+    }
+
+    void Calibrator::calibrateExtrinsic3D() {
+
+    }
+}
