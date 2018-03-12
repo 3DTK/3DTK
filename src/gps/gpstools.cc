5e6d85399ffb1da0dffc9eb44edf8d8a96dd785a
dborrman <dborrman@51f1e6e7-7289-49ad-8748-f09e43fc6daa>
2018-03-12 15:44:27 +0000

    basic tools for gps related things
    git-svn-id: https://svn.code.sf.net/p/slam6d/code/trunk@1676 51f1e6e7-7289-49ad-8748-f09e43fc6daa

---
+++ src/gps/gpstools.cc
@@ -0,0 +1,115 @@
+/**
+ * @file 
+ * @brief Collection of functions to deal with GPS measurements 
+ * @author Dorit Borrmann. 
+ */
+
+#include "gps/gps.h"
+#include "slam6d/globals.icc"
+#include <fstream>
+#include <iostream>
+
+/**
+  * Conversion from latitude, longitude, altitude (=WGS84/ellipsoidal, in m) to 
+  * left-handed ECEF in cm
+  */
+void LLAtoECEF(double latitude, double longitude, double altitude, double& cx, double& cy, double& cz) {
+  double phi = rad(latitude);
+  double lambda = rad(longitude);
+  double h = altitude; 
+
+  double n = (gps::A/(sqrt(1-(gps::E2*(sin(phi)*sin(phi))))));
+  
+  cz = (n+h)*cos(phi)*cos(lambda) * 100;  // ECEF X 
+  cx = (n+h)*cos(phi)*sin(lambda) * -100; // ECEF -Y
+  cy= ((n*(1-gps::E2))+h)*sin(phi) * 100; // ECEF Z // !!
+}
+
+/**
+  * Conversion from left-handed ECEF in cm to
+  latitude, longitude, altitude (=WGS84/ellipsoidal, in m) 
+  */
+void ECEFtoLLA(double cx, double cy, double cz, double& latitude, double& longitude, double& altitude) {
+  double x, y, z;
+  x = cz * 0.01;
+  y = cx * -0.01;
+  z = cy * 0.01;
+  
+  //from ECEF to WGS84
+
+  double lambda = atan2(y,x);
+  double p = sqrt(x*x + y*y);
+  double theta = atan2(z*gps::A,p*gps::B);
+  double phi = atan2((z+gps::EP*gps::EP*gps::B*pow(sin(theta),3)), (p-gps::E2*gps::A*pow(cos(theta),3)));
+  double n = gps::A/sqrt(1-gps::com*gps::com*sin(phi)*sin(phi));
+  double h = p/cos(phi) - n;
+  latitude = deg(phi);
+  longitude = deg(lambda);
+  altitude = h;
+}
+
+/**
+  * Conversion from latitude, longitude, altitude (=WGS84/ellipsoidal, in m)
+  * to UTM in m (East, Height, North)
+  */
+void LLAtoUTM(double latitude, double longitude, double altitude, double& east, double& height, double& north) {
+  //cerr << latitude << " " << longitude << " " << altitude << endl;
+  height = altitude; // height over ellipsoid
+  double lambda_0 = (((int)((longitude + 180)/6)*6)-177);
+  double phi = rad(latitude);
+  double lambda = cos(phi) * rad(longitude - lambda_0);
+  
+  double n = (gps::A/(sqrt(1-(gps::E2*(sin(phi)*sin(phi))))));
+  double t = tan(phi);
+  double n2 = gps::E2/(1.0-gps::E2)*(cos(phi)*cos(phi));
+  
+  double x_0 = 500000; // False Easting
+  
+  // help variables
+  double tmpA = 1.0 + 3.0/4.0*gps::E2 + 45.0/64.0*pow(gps::E2,2) + 175.0/256.0*pow(gps::E2,3) + 11025/16384*pow(gps::E2,4);
+  double tmpB = 3.0/8.0*gps::E2 + 15.0/32.0*pow(gps::E2,2) + 525.0/1024.0*pow(gps::E2,3) + 2205.0/4096.0*pow(gps::E2,4);
+  double tmpC = 15.0/256.0*pow(gps::E2,2) + 105.0/1024.0*pow(gps::E2,3) + 2205.0/16384.0*pow(gps::E2,4); 
+  double tmpD = 35.0/3072.0*pow(gps::E2,3) + 105.0/4096.0*pow(gps::E2,4);
+
+  // Meridian arc
+  double y = gps::A*(1.0-gps::E2)*(tmpA*phi - tmpB*sin(2.0*phi) + tmpC*sin(4.0*phi) - tmpD*sin(6.0*phi));
+  
+  // help variables
+  double p = 1.0/12.0*(5.0 - pow(t,2) + 9.0*n2 + 4.0*pow(n2,2));
+  double q = 1.0/360.0*(61.0 - 58.0*pow(t,2) + pow(t,4) + 270.0*n2 - 330.0*pow(t,2)*n2);
+  double r = 1.0/6.0*(1.0 - pow(t,2) + n2);
+  double s = 1.0/120.0*(5.0 - 18.0*pow(t,2) + pow(t,4) + 14.0*n2 - 58.0*pow(t,2)*n2);
+  double u = 1.0/5040.0*(61.0 - 479.0*pow(t,2) + 179.0*pow(t,4) - pow(t,6));
+  // x and correction to y
+  double x = lambda*n*(1.0 + r*pow(lambda,2) + s*pow(lambda,4) + u*pow(lambda,6));
+  double dy = 0.5*pow(lambda,2)*n*t*(1.0 + p*pow(lambda,2) + q*pow(lambda,4));
+  // Northing and Easting
+  north = gps::K*(y+dy);
+  east = gps::K*x + x_0;
+   
+}
+
+/**
+  * Conversion from left-handed ECEF in cm to
+  * to UTM in m (East, Height, North)
+  */
+void ECEFtoUTM(double cx, double cy, double cz, double& east, double& height, double& north) {
+  double longitude, latitude, altitude;
+  ECEFtoLLA(cx, cy, cz, latitude, longitude, altitude);
+  LLAtoUTM(latitude, longitude, altitude, east, height, north);
+}
+
+/**
+  * Conversion from left-handed ECEF in cm to
+  * to UTM in m (East, Height, North)
+  */
+void ECEFtoUTM(DataXYZ &xyz)
+{
+  double north, east, height;
+  for(unsigned int j = 0; j < xyz.size(); j++) {
+    ECEFtoUTM(xyz[j][0], xyz[j][1], xyz[j][2], east, height, north);
+    xyz[j][0] = east;
+    xyz[j][1] = north;
+    xyz[j][2] = height;
+  }
+}
---
+++ src/gps/scan2utm.cc
@@ -0,0 +1,243 @@
+/**
+  * Exporting points to UTM coordinates
+  *
+  * Copyright (C) Dorit Borrmann 
+  *
+  * Released under the GPL version 3.
+  *
+  */
+
+/**
+  * @file
+  * @author Dorit Borrmann. Julius-Maximilian University Würzburg, Germany.
+  */
+
+#include <string>
+#include <iostream>
+#include <fstream>
+
+#include "slam6d/scan.h"
+#include "gps/gps.h"
+#include "scanio/writer.h"
+
+#ifndef _MSC_VER
+#include <getopt.h>
+#else
+#include "XGetopt.h"
+#endif
+
+#ifdef _MSC_VER
+#define strcasecmp _stricmp
+#define strncasecmp _strnicmp
+#else
+#include <strings.h>
+#endif
+
+void usage(char* prog)
+{
+#ifndef _MSC_VER
+  const std::string bold("\033[1m");
+  const std::string normal("\033[m");
+#else
+  const std::string bold("");
+  const std::string normal("");
+#endif
+  std::cout << std::endl
+	  << bold << "USAGE " << normal << std::endl
+	  << "   " << prog << " [options] directory" << std::endl << std::endl;
+  std::cout << bold << "OPTIONS" << normal << std::endl
+	  << std::endl;
+  exit(1);
+}
+
+int parseArgs(int argc, char **argv, std::string &dir, IOType &type, int &start, int &end, bool &use_color, bool &use_reflectance) {
+  int  c;
+  // from unistd.h:
+  extern char *optarg;
+  extern int optind;
+
+  /* options descriptor */
+  // 0: no arguments, 1: required argument, 2: optional argument
+  static struct option longopts[] = {
+    { "format",          required_argument,   0,  'f' },  
+    { "start",           required_argument,   0,  's' },
+    { "end",             required_argument,   0,  'e' },
+    { "reflectance",     no_argument,         0,  'R' },
+    { "color",           no_argument,         0,  'c' },
+    { "reflectivity",    no_argument,         0,  'R' },
+    { "help",            no_argument,         0,  'h' },
+    { 0,           0,   0,   0}                    // needed, cf. getopt.h
+  };
+  
+  int option_index = 0;
+  while ((c = getopt_long(argc, argv, "f:s:e:cRh", longopts, &option_index)) != -1)
+    switch (c)
+     {
+       case 'R':
+	    use_reflectance = true; 
+       break;
+	  case 'c':
+	    use_color = true;
+	    break;
+     case 's':
+       start = atoi(optarg);
+       if (start < 0) { std::cerr << "Error: Cannot start at a negative scan number.\n"; exit(1); }
+       break;
+     case 'e':
+       end = atoi(optarg);
+       if (end < 0)     { std::cerr << "Error: Cannot end at a negative scan number.\n"; exit(1); }
+       if (end < start) { std::cerr << "Error: <end> cannot be smaller than <start>.\n"; exit(1); }
+       break;
+     case 'f':
+    try {
+      type = formatname_to_io_type(optarg);
+    } catch (...) { // runtime_error
+      std::cerr << "Format " << optarg << " unknown." << std::endl;
+      abort();
+    }
+    break;
+      case 'h':
+      case '?':
+       usage(argv[0]);
+       return 1;
+      default:
+       abort ();
+      }
+
+  if (optind != argc-1) {
+    std::cerr << "\n*** Directory missing ***" << std::endl;
+    usage(argv[0]);
+  }
+  dir = argv[optind];
+
+#ifndef _MSC_VER
+  if (dir[dir.length()-1] != '/') dir = dir + "/";
+#else
+  if (dir[dir.length()-1] != '\\') dir = dir + "\\";
+#endif
+
+  return 0;
+}
+
+
+/**
+ * Converts scans in ECEF coordinates to UTM coordinates in meters (East, North, Height).
+ * The standard ECEF coordinate system is a right-handed cartesian coordinate
+ * system. The origin corresponds to the center of mass of the earth. The z-axis
+ * goes through the geodetic north pole. The x-axis goes through the
+ * intersection between the equator (0° latitude) and the prime meridian (0°
+ * longitude). 
+ * In the internal left-handed 3DTK coordinate system the y-axis goes through
+ * the geodetic north pole and the z-axis intersects (0°, 0°).
+ * Output will be: EAST NORTH HEIGHT ...
+ */
+int main(int argc, char **argv)
+{
+  if (argc <= 1) {
+    usage(argv[0]);
+  }
+
+  std::string dir;
+  IOType iotype    = UOS;
+  int    start = 0,   end = -1;
+  bool use_color = false, use_reflectance = false;
+  bool high_precision = false;
+  bool hexfloat = false;
+
+  parseArgs(argc, argv, dir, iotype, start, end, use_color, use_reflectance);
+  
+  Scan::openDirectory(false, dir, iotype, start, end);
+  
+  if(Scan::allScans.size() == 0) {
+    std::cerr << "No scans found. Did you use the correct format?" << std::endl;
+    exit(-1);
+  }
+  
+  unsigned int types = PointType::USE_NONE;
+  //TODO check if all file formats are included
+  if(use_reflectance) {
+    switch (iotype) {
+      case UOSR:
+      case UOS_RRGBT:
+      case UOS_RRGB:
+      case RIEGL_TXT:
+      case RIEGL_PROJECT:
+      case RXP:
+      case TXYZR:
+      case XYZR:
+      case XYZ_RRGB:
+      case FARO_XYZ_RGBR:
+      case LEICA_XYZR:
+        types |= PointType::USE_REFLECTANCE;
+        break;
+      default:
+        break;
+    }
+  }
+  
+  if(use_color) {
+    switch (iotype) {
+      case UOS_RGB:
+      case UOS_RRGBT:
+      case RIEGL_RGB:
+      case XYZ_RGB:
+      case KS_RGB:
+      case PLY:
+        types |= PointType::USE_COLOR;
+        break;
+      default:
+         break;
+    }
+  }
+ 
+  //No reduction for now
+  int red = 0;
+  
+  FILE *redptsout = fopen("points.utm", "w");
+  
+  for(unsigned int i = 0; i < Scan::allScans.size(); i++) {
+    Scan *source = Scan::allScans[i];
+    std::string red_string = red > 0 ? " reduced" : "";
+      
+    DataXYZ xyz  = source->get("xyz" + red_string);
+    
+    ECEFtoUTM(xyz);
+    write_uos(xyz, redptsout, hexfloat, high_precision);
+     
+    if(use_reflectance) {
+      DataReflectance xyz_reflectance = 
+          (((DataReflectance)source->get("reflectance" + red_string)).size() == 0) ?
+ 
+          source->create("reflectance" + red_string, sizeof(float)*xyz.size()) : 
+          source->get("reflectance" + red_string); 
+ 
+      if (!(types & PointType::USE_REFLECTANCE)) {
+        for(unsigned int i = 0; i < xyz.size(); i++) xyz_reflectance[i] = 255;
+      }
+      write_uosr(xyz, xyz_reflectance, redptsout, hexfloat, high_precision);
+      
+    } else if(use_color) {
+      std::string data_string = red > 0 ? "color reduced" : "rgb";
+      DataRGB xyz_color = 
+          (((DataRGB)source->get(data_string)).size() == 0) ?
+          source->create(data_string, sizeof(unsigned char)*3*xyz.size()) : 
+          source->get(data_string); 
+      if (!(types & PointType::USE_COLOR)) {
+          for(unsigned int i = 0; i < xyz.size(); i++) {
+            xyz_color[i][0] = 0;
+            xyz_color[i][1] = 0;
+            xyz_color[i][2] = 0;
+        }
+      }
+      write_uos_rgb(xyz, xyz_color, redptsout, hexfloat, high_precision);
+
+    } else {
+    write_uos(xyz, redptsout, hexfloat, high_precision);
+    
+    }
+    
+
+  }
+   
+  fclose(redptsout);
+}
