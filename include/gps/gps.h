b72944a24bd845f53b5f8ad2974ee784a0ba481c
Dorit Borrman <dborrman@users.sourceforge.net>
2018-03-12 15:44:27 +0000

    basic tools for gps related things
    git-svn-id: svn://svn.code.sf.net/p/slam6d/code/trunk@1676 51f1e6e7-7289-49ad-8748-f09e43fc6daa

---
+++ include/gps/gps.h
@@ -0,0 +1,31 @@
+/**
+ * @file 
+ * @brief Collection of functions to deal with GPS measurements 
+ * @author Dorit Borrmann. 
+ */
+#ifndef GPS_H
+#define GPS_H
+#include "slam6d/scan.h"
+
+namespace gps {
+  const double A = 6378137.0;
+  const double B = 6356752.31424518;    
+  const double com = sqrt((gps::A*gps::A - gps::B*gps::B)/(gps::A*gps::A));  
+  const double E2 = 0.00669437999014;
+  const double K = 0.9996;
+  const double F = 1.0/298.257223563;
+  const double EP = sqrt((gps::A*gps::A-gps::B*gps::B)/(gps::B*gps::B));
+}
+
+/*
+void calcAlignmentMatrix(double * n0, double * mat);
+void computeOrientation(double x, double y, double z, double *rPosTheta);
+void computeOrientation(double x, double y, double z, double ax, double ay, double az, double *rPosTheta);
+*/
+void LLAtoECEF(double latitude, double longitude, double altitude, double& cx, double& cy, double& cz);
+void ECEFtoLLA(double cx, double cy, double cz, double& latitude, double& longitude, double& altitude);
+void LLAtoUTM(double latitude, double longitude, double altitude, double& cx, double& cy, double& cz);
+
+void ECEFtoUTM(double cx, double cy, double cz, double& east, double& altitude, double& north); 
+void ECEFtoUTM(DataXYZ &xyz);
+#endif
