bbffd783e21b1d30829c6ada79695761fcc58ba1
jlucht <jlucht@51f1e6e7-7289-49ad-8748-f09e43fc6daa>
2018-03-15 15:58:17 +0000

    calibration.Calibrator: add function matchAprilTags
    git-svn-id: https://svn.code.sf.net/p/slam6d/code/trunk@1696 51f1e6e7-7289-49ad-8748-f09e43fc6daa

--- src/calibration/Calibrator.cc
+++ src/calibration/Calibrator.cc
@@ -5,7 +5,18 @@
 #include "calibration/Calibrator.h"
 
 
-namespace calibration{
+namespace calibration {
+
+    void Calibrator::matchAprilTags(std::vector<AprilTag::AprilTag2f> imagePoints,
+                                    std::vector<AprilTag::AprilTag3f> objectPoints,
+                                    std::vector<cv::Point2f> *matchedImagePoints,
+                                    std::vector<cv::Point3f> *matchedObjectPoints,
+                                    std::vector<int> estimationIDs,
+                                    std::vector<cv::Point2f> *matchedEstimationImagePoints,
+                                    std::vector<cv::Point3f> *matchedEstimationObjectPoints) {
+        
+    }
+
     void Calibrator::calibrateIntrinsic() {
 
     }
