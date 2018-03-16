a04502af27a99090034b3cc0d8d831c1b2d4f41a
josch1337 <josch1337@51f1e6e7-7289-49ad-8748-f09e43fc6daa>
2018-03-16 17:28:00 +0000

    windows/timeout.py: propagate return code of build.cmd to caller
    git-svn-id: https://svn.code.sf.net/p/slam6d/code/trunk@1722 51f1e6e7-7289-49ad-8748-f09e43fc6daa

--- windows/timeout.py
+++ windows/timeout.py
@@ -3,7 +3,7 @@ import sys
 
 p = subprocess.Popen(sys.argv[2:])
 try:
-    p.wait(timeout=int(sys.argv[1]))
+    exit(p.wait(timeout=int(sys.argv[1])))
 except subprocess.TimeoutExpired as e:
     # there is no psutil module installed on appveyor
     subprocess.call(['taskkill', '/F', '/T', '/PID', str(p.pid)])
