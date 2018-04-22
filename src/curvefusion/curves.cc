0d144ab175b286fb1f57ded0bf58daa373d9ad73
Shitong Du <dust_1028@163.com>
2018-04-22 17:53:18 +0000

    added 3D trajectory fusion and data sample method .
    git-svn-id: svn://svn.code.sf.net/p/slam6d/code/trunk@1762 51f1e6e7-7289-49ad-8748-f09e43fc6daa

--- src/curvefusion/curves.cc
+++ src/curvefusion/curves.cc
@@ -3,9 +3,15 @@
 #include <utility>
 #include <fstream>
 
-std::vector<Curves*> Curves::allpoints;
-std::vector<Curves*> Curves::Samplepoints;
+
+#include <iomanip>
+using std::ios;
+
 std::vector<Curves*> Curves::Oripoints;
+std::vector<Curves*> Curves::allpoints;
+std::vector<Curves*> Curves::newpoints;
+std::vector<Curves*> Curves::Samplepoints;
+
 Curves::Curves(const std::string& path,int type, int identifier) :
   m_path(path),m_type(type),identifiers(identifier)
 {
@@ -68,21 +74,23 @@ void Curves::Trans_Mat(Curves* CurrentPoint,Curves* NextPoint)
 {
   //computer transformation matrix from current point to next point.
    
-   //double point_c[6],point_n[6];
-   
-   MatrixXd traj_point1(2,2);
-   MatrixXd traj_mean1(2,2);
-   MatrixXd H1(2,2);
-   VectorXd colpoint1_1(2),colpoint1_2(2);
-   VectorXd col_vec1_1(2), col_vec1_2(2);
+   MatrixXd traj_point1(2,DIMENSIONS);
+   MatrixXd traj_mean1(2,DIMENSIONS);
+   MatrixXd H1(2,DIMENSIONS);
+
   
+   VectorXd colpoint1_1(DIMENSIONS),colpoint1_2(DIMENSIONS);
+   VectorXd col_vec1_1(DIMENSIONS), col_vec1_2(DIMENSIONS);
+   
   //trajectory 1
-   for(int i=0;i<2;i++){
+
+
+   for(int i=0;i<DIMENSIONS;i++){
    traj_point1(0,i)=(CurrentPoint->points1(i));
    col_vec1_1(i)=traj_point1(0,i);
    }
  
-   for(int j=0;j<2;j++){
+   for(int j=0;j<DIMENSIONS ;j++){
    traj_point1(1,j)=(NextPoint->points1(j));
    col_vec1_2(j)=traj_point1(1,j);
    }
@@ -90,58 +98,79 @@ void Curves::Trans_Mat(Curves* CurrentPoint,Curves* NextPoint)
   //caculate mean of currentpoint and nextpoint
    traj_mean1(0,0)=(traj_point1(0,0)+traj_point1(1,0))/2;
    traj_mean1(0,1)=(traj_point1(0,1)+traj_point1(1,1))/2;
+
+#ifdef POINT3D
+   traj_mean1(0,2)=(traj_point1(0,2)+traj_point1(1,2))/2;
+#endif
+ 
    //traj_mean1(1,3)=(traj_point1(1,3)+traj_point1(2,3))/2;
-   for(int i=0;i<2;i++)
+   for(int i=0;i<DIMENSIONS;i++)
    traj_mean1(1,i)=traj_mean1(0,i);
 
   //subtract the mean of trajecotry points
    for(int j = 0; j < 2; j++){
-    for(int k = 0; k < 2; k++){
+    for(int k = 0; k < DIMENSIONS; k++){
      H1(j, k) =(traj_point1(j, k)-traj_mean1(j, k)) ;     
     }
    }
   
-   // Make SVD
-   JacobiSVD<MatrixXd> svd1(H1, ComputeThinU | ComputeThinV);
-   Matrix2d U1 = svd1.matrixU(); 
-   Matrix2d V1 = svd1.matrixV();  
+  
+   JacobiSVD<MatrixXd>svd1(H1, ComputeFullU | ComputeFullV); 
+   MatrixXd U1 = svd1.matrixU();
+
+   MatrixXd V1 = svd1.matrixV();
+ 
+  // colpoint1_2=(V1.transpose())*col_vec1_2;
    colpoint1_1=(V1.transpose())*col_vec1_1;
    colpoint1_2=(V1.transpose())*col_vec1_2;
-
    MatrixXd R1=Opt_rot(colpoint1_1,colpoint1_2);
-   
+  
   // IdentityMatrix E1(2);
-   rot1 = V1*R1*(V1.transpose()); 
-   trans1 = col_vec1_2 - rot1*col_vec1_1;
+   MatrixXd E1= MatrixXd::Identity(DIMENSIONS,DIMENSIONS);
+   
+   for(int i = 0; i < 2; i++){
     for(int j = 0; j < 2; j++){
-    for(int k = 0; k < 2; k++){
+     E1(i, j) =R1(i,j);     
+    }
+   }
+
+   rot1=E1;
+   rot1 = V1*rot1*(V1.transpose());
+  
+   
+   trans1 = col_vec1_2 - rot1*col_vec1_1;
+
+
+    for(int j = 0; j < DIMENSIONS; j++){
+    for(int k = 0; k < DIMENSIONS; k++){
      transformation1(j, k) =rot1(j, k);     
     }
    }
 
-   for(int j = 0; j < 2; j++){
-   transformation1(j, 2)=trans1(j);
+   for(int j = 0; j < DIMENSIONS; j++){
+   transformation1(j, DIMENSIONS)=trans1(j);
    }
    
-   transformation1(2, 0)=0;
-   transformation1(2, 1)=0;
-   transformation1(2, 2)=1.0;
+   for(int k = 0; k < DIMENSIONS; k++)
+   transformation1(DIMENSIONS, k)=0;
+   
+   transformation1(DIMENSIONS, DIMENSIONS)=1.0;
 
-
+ 
   //trajectory 2
-   MatrixXd traj_point2(2,2);
-   MatrixXd traj_mean2(2,2);
-   MatrixXd H2(2,2);
-   VectorXd colpoint2_1(2),colpoint2_2(2);
-   VectorXd col_vec2_1(2), col_vec2_2(2);
+   MatrixXd traj_point2(2,DIMENSIONS);
+   MatrixXd traj_mean2(2,DIMENSIONS);
+   MatrixXd H2(2,DIMENSIONS);
+   VectorXd colpoint2_1(DIMENSIONS),colpoint2_2(DIMENSIONS);
+   VectorXd col_vec2_1(DIMENSIONS), col_vec2_2(DIMENSIONS);
   
-  //trajectory 1
-   for(int i=0;i<2;i++){
+
+   for(int i=0;i<DIMENSIONS;i++){
    traj_point2(0,i)=(CurrentPoint->points2(i));
    col_vec2_1(i)=traj_point2(0,i);
    }
  
-   for(int j=0;j<2;j++){
+   for(int j=0;j<DIMENSIONS;j++){
    traj_point2(1,j)=(NextPoint->points2(j));
    col_vec2_2(j)=traj_point2(1,j);
    }
@@ -149,42 +178,62 @@ void Curves::Trans_Mat(Curves* CurrentPoint,Curves* NextPoint)
   //caculate mean of currentpoint and nextpoint
    traj_mean2(0,0)=(traj_point2(0,0)+traj_point2(1,0))/2;
    traj_mean2(0,1)=(traj_point2(0,1)+traj_point2(1,1))/2;
-   //traj_mean1(1,3)=(traj_point1(1,3)+traj_point1(2,3))/2;
-   for(int i=0;i<2;i++)
+
+#ifdef POINT3D
+   traj_mean2(0,2)=(traj_point2(0,2)+traj_point2(1,2))/2;
+#endif
+   
+   for(int i=0;i<DIMENSIONS;i++)
    traj_mean2(1,i)=traj_mean2(0,i);
 
   //subtract the mean of trajecotry points
    for(int j = 0; j < 2; j++){
-    for(int k = 0; k < 2; k++){
+    for(int k = 0; k < DIMENSIONS; k++){
      H2(j, k) =(traj_point2(j, k)-traj_mean2(j, k)) ;     
     }
    }
   
    // Make SVD
-   JacobiSVD<MatrixXd> svd2(H2, ComputeThinU | ComputeThinV);
+   JacobiSVD<MatrixXd> svd2(H2, ComputeFullU | ComputeFullV);
    Matrix2d U2 = svd2.matrixU(); 
-   Matrix2d V2 = svd2.matrixV();  
+
+ 
+   MatrixXd V2 = svd2.matrixV();  
+   
+  // Matrix2d V2 = svd2.matrixV();  
    colpoint2_1=(V2.transpose())*col_vec2_1;
    colpoint2_2=(V2.transpose())*col_vec2_2;
 
    MatrixXd R2=Opt_rot(colpoint2_1,colpoint2_2);
    
-  // IdentityMatrix E1(2);
-   rot2 = V2*R2*(V2.transpose()); 
-   trans2 = col_vec2_2 - rot2*col_vec2_1;
+   MatrixXd E2= MatrixXd::Identity(DIMENSIONS,DIMENSIONS);
+   
+   for(int i = 0; i < 2; i++){
     for(int j = 0; j < 2; j++){
-    for(int k = 0; k < 2; k++){
+     E2(i, j) =R2(i,j);     
+    }
+   }
+
+   rot2=E2;
+   rot2 = V2*rot2*(V2.transpose()); 
+
+  // IdentityMatrix E1(2);
+  // rot2 = V2*R2*(V2.transpose()); 
+   trans2 = col_vec2_2 - rot2*col_vec2_1;
+    for(int j = 0; j < DIMENSIONS; j++){
+    for(int k = 0; k < DIMENSIONS; k++){
      transformation2(j, k) =rot2(j, k);     
     }
    }
 
-   for(int j = 0; j < 2; j++){
-   transformation2(j, 2)=trans2(j);
+   for(int j = 0; j < DIMENSIONS; j++){
+   transformation2(j, DIMENSIONS)=trans2(j);
    }
+
+   for(int k = 0; k < DIMENSIONS; k++)
+   transformation2(DIMENSIONS, k)=0;
    
-   transformation2(2, 0)=0;
-   transformation2(2, 1)=0;
-   transformation2(2, 2)=1.0;
+   transformation2(DIMENSIONS, DIMENSIONS)=1.0;
    
    //return 0;
 }
@@ -193,9 +242,14 @@ void Curves::Trans_Mat(Curves* CurrentPoint,Curves* NextPoint)
 MatrixXd Curves::Opt_rot(VectorXd A,VectorXd B)
 
 {
-   
+   Vector2d A1,B1;
+   for(int k = 0; k < 2; k++){
+     A1(k)=A(k);
+     B1(k)=B(k);
+   }
+
    MatrixXd R(2,2),rot(2,2);
-   Matrix2d corr=A*(B.transpose());
+   MatrixXd corr=A1*(B1.transpose());
    
    JacobiSVD<MatrixXd> svd(corr, ComputeThinU | ComputeThinV);
    MatrixXd U = svd.matrixU(); 
@@ -207,7 +261,6 @@ MatrixXd Curves::Opt_rot(VectorXd A,VectorXd B)
    rot = V*E*(U.transpose());  
    return rot;
 }
-
 
 
 
