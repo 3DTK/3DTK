9ffff83dff3c1bcdf35a8513e263ddb862bd1c14
Johannes 'josch' Schauer <josch@mister-muffin.de>
2018-05-04 04:58:15 +0000

    travisdockerbuild.sh: first try new way to install .deb packages with apt before falling back to --fix-broken
    git-svn-id: svn://svn.code.sf.net/p/slam6d/code/trunk@1775 51f1e6e7-7289-49ad-8748-f09e43fc6daa

--- travisdockerbuild.sh
+++ travisdockerbuild.sh
@@ -80,7 +80,7 @@ apt-get dist-upgrade --yes
 apt-get install --yes --no-install-recommends -o Debug::pkgProblemResolver=yes equivs ninja-build
 equivs-build doc/equivs/control.$DERIV.$DIST
 dpkg --install --force-depends ./3dtk-build-deps_1.0_all.deb
-apt-get install --yes --no-install-recommends --fix-broken -o Debug::pkgProblemResolver=yes
+apt-get install --no-install-recommends ./3dtk-build-deps_1.0_all.deb || apt-get install --yes --no-install-recommends --fix-broken -o Debug::pkgProblemResolver=yes
 echo "travis_fold:end:docker_setup"
 mkdir .build
 cmake -H. -B.build $CMAKEOPTS -G "Ninja"
