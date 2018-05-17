9a57f5ea8b15536e7bdf27110fd9b8fa4af89aa4
xiasun <hsia.sun@gmail.com>
2018-05-17 17:35:28 +0000

    Add mesh input, output and redering with sample data
    git-svn-id: svn://svn.code.sf.net/p/slam6d/code/trunk@1792 51f1e6e7-7289-49ad-8748-f09e43fc6daa

---
+++ include/mesh/mesh.h
@@ -0,0 +1,47 @@
+/* Mesh model object
+* Author: Xia Sun
+*/
+#ifndef __MESH_H__
+#define __MESH_H__
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <limits>
+#include <algorithm>
+
+#include <GL/glut.h>
+
+class Mesh {
+public:
+  // public attributes
+  bool empty;
+  int numVertices;
+  int numFaces;
+  int numNormals;
+  
+  // public methods
+  Mesh();
+  ~Mesh();
+  void load(std::string modelPath);
+  void save(std::string modelPath);
+  std::vector<float*> getOrderedVertices();
+  float* getBBox();
+
+private:
+  // private attributes
+  std::vector<float*> oVertices; // ordered vertices, each float* means 9 verices of a triangle face
+  std::vector<float*> vertices;
+  std::vector<int*> faces;
+  std::vector<float*> normals;
+  float *bbox; // x y z min/max
+
+  // private methods
+  void initialize();
+  void sortVertices();
+  void updateBBox(float vX, float vY, float vZ);
+};
+
+#endif
---
+++ src/mesh/testmesh.cc
@@ -0,0 +1,65 @@
+#include "mesh/mesh.h"
+
+using namespace std;
+
+// global
+Mesh mesh;
+
+void display();
+
+int main(int argc, char **argv) {
+  // load mesh
+  if (argc == 2) {
+    mesh.load(argv[1]);
+  }
+  else {
+    mesh.load("//home//summer//Porjects//slam6d-code//dat//test//sphere.obj");
+  }
+
+  // save mesh
+  mesh.save("//home//summer//Porjects//slam6d-code//dat//test//sphere_save.obj");
+
+  // glut related calls
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+  glutInitWindowPosition(100,100);
+  glutInitWindowSize(320,320);
+  glutCreateWindow("Mesh Renderer");
+  glutDisplayFunc(display);
+  glutMainLoop();
+
+  return 0;
+}
+
+// draw faces and poly lines
+void display() {
+  vector<float*> vertices = mesh.getOrderedVertices();
+  float *bbox = mesh.getBBox();
+
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  float centerX = (bbox[0] + bbox[1]) / 2, 
+    centerY = (bbox[2] + bbox[3]) / 2, 
+    centerZ = (bbox[4] + bbox[5]) / 2;
+
+	glLoadIdentity();
+  // gluLookAt( centerX, centerY, bbox[5],
+	// 		centerX, centerY, centerZ,
+	// 		0.0f, -1.0f, 0.0f);
+
+  for (float* face : vertices) {
+    glColor3f (0.25, 0.25, 0.25);
+    glBegin(GL_TRIANGLES);
+      glVertex3f(face[0], face[1], face[2]);
+      glVertex3f(face[3], face[4], face[5]);
+      glVertex3f(face[6], face[7], face[8]);
+    glEnd();
+    glColor3f (1.0, 1.0, 1.0);
+    glBegin(GL_LINES);
+      glVertex3f(face[0], face[1], face[2]);
+      glVertex3f(face[3], face[4], face[5]);
+      glVertex3f(face[6], face[7], face[8]);
+    glEnd();
+  }
+
+  glutSwapBuffers();
+}
