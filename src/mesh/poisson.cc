5a680a8c454ac17fd3b1d08b51776b398f480a4d
xiasun <hsia.sun@gmail.com>
2018-05-23 12:15:38 +0000

    Integrate KB's poisson reconstruction into src/mesh and add two sample test data
    git-svn-id: svn://svn.code.sf.net/p/slam6d/code/trunk@1795 51f1e6e7-7289-49ad-8748-f09e43fc6daa

---
+++ src/mesh/poisson.cc
@@ -0,0 +1,19 @@
+#include "mesh/poisson.h"
+
+using namespace std;
+
+int main(int argc, char **argv) {
+  return 1;
+}
+
+Poisson::Poisson() {
+  initialize();
+}
+
+Poisson::~Poisson() {
+  initialize();
+}
+
+void Poisson::initialize() {
+  return;
+}
