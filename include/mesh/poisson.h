60275d5f0b201b1f8a3a8e527eee10ddfbc15520
xiasun <hsia.sun@gmail.com>
2018-05-31 14:31:27 +0000

    mesh: integrate scanio and calc_normals together with current poisson
    git-svn-id: svn://svn.code.sf.net/p/slam6d/code/trunk@1813 51f1e6e7-7289-49ad-8748-f09e43fc6daa

--- include/mesh/poisson.h
+++ include/mesh/poisson.h
@@ -1,25 +1,42 @@
-/* Mesh model object
-* Author: Xia Sun
-*/
+/*------------------- 
+** Poisson object (prototype)
+** Author: Xia Sun
+--------------------*/
 #ifndef __MESH_H__
 #define __MESH_H__
+
+
+#include "slam6d/scan.h"
+#include "slam6d/normals.h"
 
 class Poisson {
 public:
   // public attributes
-  int writeAsXYZ();
+  
   
   // public methods
   Poisson();
   ~Poisson();
-  
+  int setVertices(vector<Point> v);
+  int setNormals(vector<Point> n);
+  int getMesh(CoredVectorMeshData *m);
+  int apply();
+  int exportMesh(const char *modelPath);
 
 private:
   // private attributes
+  int reconstructed;
+  PoissonParam params;
+  vector<Point3D<float>> vertices;
+  vector<Point3D<float>> normals;
+  CoredVectorMeshData mesh;
+  Point3D<float> center;
+  float scale;
+  vcg::CallBackPos *cb;
 
   // private methods
   void initialize();
-  
+  int ready();
 };
 
 #endif
