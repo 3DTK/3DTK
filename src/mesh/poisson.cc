a53d20b95b5000f7df62992b9212014acca7246a
xiasun <xiasun@51f1e6e7-7289-49ad-8748-f09e43fc6daa>
2018-06-06 10:10:10 +0000

    Add parameters for normal estimation
    git-svn-id: svn+ssh://svn.code.sf.net/p/slam6d/code/trunk@1817 51f1e6e7-7289-49ad-8748-f09e43fc6daa

--- src/mesh/poisson.cc
+++ src/mesh/poisson.cc
@@ -3,6 +3,13 @@
 #include <vector>
 #include <fstream>
 
+#include <boost/program_options.hpp>
+
+#include <slam6d/io_types.h>
+#include <slam6d/globals.icc>
+#include <slam6d/scan.h>
+#include <scanserver/clientInterface.h>
+
 #include "poisson/Geometry.h"
 #include "poisson/PoissonParam.h"
 #include "wrap/callback.h"
@@ -12,55 +19,120 @@
 using namespace std;
 using namespace vcg;
 
+namespace po = boost::program_options;
+
+enum normal_method {KNN, ADAPTIVE_KNN,
+				AKNN, ADAPTIVE_AKNN,
+#ifdef WITH_OPENCV
+				PANORAMA, PANORAMA_FAST
+#endif
+};
+
+// Call to excute poisson
 int Execute2(PoissonParam &Par, vector<Point3D<float> > Pts, vector<Point3D<float> > Nor, 	CoredVectorMeshData &mesh, Point3D<float> &newCenter, float &newScale, vcg::CallBackPos *cb );
+// Parse commandline options
+void parse_options(int argc, char **argv, int &start, int &end, bool &scanserver, int &max_dist, int &min_dist, string &dir, string &odir, IOType &iotype, int &k1, int &k2, normal_method &ntype, int &width, int &height);
+// validate normmal_method type (important for boost program_option)
+void validate(boost::any& v, const std::vector<std::string>& values, normal_method*, int);
+// validate IO types (important for boost program_option)
+void validate(boost::any& v, const std::vector<std::string>& values, IOType*, int);
 
 int main(int argc, char **argv) {
-  // fixed command supported now
-  // more parameters to be supported later
-  if (argc != 5) {
-    cout << "Parameters should be like: poisson -in path/of/scan -out path/of/output.obj" << endl;
-    return 0;
-  }
-  else if (strcmp(argv[1], "-in") != 0 || strcmp(argv[3], "-out") != 0) {
-    cout << "Parameters should be like: poisson -in path/of/scan -out path/of/output.obj" << endl;
-    return 0;
-  }
-  
-  vector<Point> points; 
+  int start, end;
+  bool scanserver;
+  int max_dist, min_dist;
+  string dir, odir; // directory of input scan and output model
+  IOType iotype;
+  int k1, k2;
+  normal_method ntype;
+  int width, height;
+
+  vector<Point> points;
   vector<Point> normals;
+  
+  // parse input arguments
+  parse_options(argc, argv, start, end, scanserver, max_dist, min_dist,
+                dir, odir, iotype, k1, k2, ntype, width, height);
 
-  string inputPath = argv[2];
-  string outputPath = argv[4];
-  IOType scanType = IOType::XYZ;
-  int start = 0, end = 1;
+  if (scanserver) {
+    try {
+      ClientInterface::create();
+    } catch(std::runtime_error& e) {
+      cerr << "ClientInterface could not be created: " << e.what() << endl;
+      cerr << "Start the scanserver first." << endl;
+      exit(-1);
+    }
+  }
 
-  Scan::openDirectory(false, inputPath, scanType, start, end);
+  // Read the scans
+  Scan::openDirectory(scanserver, dir, iotype, start, end);
+  if(Scan::allScans.size() == 0) {
+    cerr << "No scans found. Did you use the correct format?" << endl;
+    exit(-1);
+  }
 
-  for(ScanVector::iterator it = Scan::allScans.begin();
-      it != Scan::allScans.end();
-      ++it) {
+  std::vector<Scan*>::iterator it = Scan::allScans.begin();
+  int scanNumber = start;
+
+  for( ; it != Scan::allScans.end(); ++it) {
     Scan* scan = *it;
-    const double* rPos = scan->get_rPos();
-    DataXYZ xyz = scan->get("xyz");
 
+    // apply optional filtering
+    scan->setRangeFilter(max_dist, min_dist);
+
+    const double* rPos = scan->get_rPos();
+    const double* rPosTheta = scan->get_rPosTheta();
+
+    // read scan into points
+    DataXYZ xyz(scan->get("xyz"));
     points.reserve(xyz.size());
     normals.reserve(xyz.size());
 
     for(unsigned int j = 0; j < xyz.size(); j++) {
       points.push_back(Point(xyz[j][0], xyz[j][1], xyz[j][2]));
     }
-    calculateNormalsKNN(normals, points, 10, rPos);
+
+    if (ntype == KNN)
+      calculateNormalsKNN(normals, points, k1, rPos);
+    else if (ntype == ADAPTIVE_KNN)
+      calculateNormalsAdaptiveKNN(normals, points, k1, k2, rPos);
+    else if (ntype == AKNN)
+      calculateNormalsApxKNN(normals, points, k1, rPos);
+    else if (ntype == ADAPTIVE_AKNN)
+      calculateNormalsAdaptiveApxKNN(normals, points, k1, k2, rPos);
+    else
+    {
+#ifdef WITH_OPENCV
+      // create panorama
+      fbr::panorama fPanorama(width, height, fbr::EQUIRECTANGULAR,
+                              1, 0, fbr::EXTENDED);
+      fPanorama.createPanorama(scan2mat(scan));
+
+      if(ntype == PANORAMA)
+        calculateNormalsPANORAMA(normals,
+                                 points,
+                                 fPanorama.getExtendedMap(),
+                                 rPos);
+      else if(ntype == PANORAMA_FAST)
+        calculateNormalsFAST(normals,
+                             points,
+                             fPanorama.getRangeImage(),
+                             fPanorama.getMaxRange(),
+                             rPos,
+                             fPanorama.getExtendedMap());
+#endif
+    }
+    scanNumber++;
   }
 
-  Scan::closeDirectory();
+  cout << "Normal calculation end" << endl;
+  cout << "Poisson reconstruction started" << endl;
 
-  //// Write calculated normal to file for test
-  // fstream fs("./dat/test/calc_norm_test.xyz", fstream::out);
-  // for (unsigned int i = 0; i < points.size(); ++i) {
-  //   fs << points[i].x << " " << points[i].y << " " << points[i].z << " " 
-  //     << normals[i].x << " " << normals[i].y << " " << normals[i].z << endl;
-  // }
-  // fs.close();
+  // shutdown everything
+  if (scanserver)
+    ClientInterface::destroy();
+
+  Scan::closeDirectory();
 
   Poisson poisson;
   poisson.setVertices(points);
@@ -68,10 +140,130 @@ int main(int argc, char **argv) {
   poisson.apply();
   // CoredVectorMeshData m;
   // poisson.getMesh(&m);
-  poisson.exportMesh(outputPath.c_str());
+  poisson.exportMesh(odir.c_str());
+  cout << "Poisson reconstruction end" << endl;
 
   return 0;
 }
+
+// validate normmal_method type (important for boost program_option)
+void validate(boost::any& v, const std::vector<std::string>& values,
+              normal_method*, int) {
+  if (values.size() == 0)
+    throw std::runtime_error("Invalid model specification");
+  string arg = values.at(0);
+  if (strcasecmp(arg.c_str(), "KNN") == 0) v = KNN;
+  else if (strcasecmp(arg.c_str(), "ADAPTIVE_KNN") == 0) v = ADAPTIVE_KNN;
+  else if (strcasecmp(arg.c_str(), "AKNN") == 0) v = AKNN;
+  else if (strcasecmp(arg.c_str(), "ADAPTIVE_AKNN") == 0) v = ADAPTIVE_AKNN;
+#ifdef WITH_OPENCV
+  else if (strcasecmp(arg.c_str(), "PANORAMA") == 0) v = PANORAMA;
+  else if (strcasecmp(arg.c_str(), "PANORAMA_FAST") == 0) v = PANORAMA_FAST;
+#endif
+  else throw std::runtime_error(std::string("normal calculation method ")
+                                + arg + std::string(" is unknown"));
+}
+
+// validate IO types (important for boost program_option)
+void validate(boost::any& v, const std::vector<std::string>& values,
+              IOType*, int) {
+  if (values.size() == 0)
+    throw std::runtime_error("Invalid model specification");
+  string arg = values.at(0);
+  try {
+    v = formatname_to_io_type(arg.c_str());
+  } catch (...) { // runtime_error
+    throw std::runtime_error("Format " + arg + " unknown.");
+  }
+}
+
+// Parse commandline options
+void parse_options(int argc, char **argv, int &start, int &end,
+			    bool &scanserver, int &max_dist, int &min_dist, string &dir, string &odir,
+                   IOType &iotype, int &k1, int &k2,
+			    normal_method &ntype, int &width, int &height)
+{
+  po::options_description cmd_options("Usage: calculateNormals <options> "
+                                      "where options are (default values "
+                                      "in brackets)");
+  cmd_options.add_options()
+      ("help,?", "Display this help message")
+      ("start,s",
+       po::value<int>(&start)->default_value(0),
+       "Start at scan number <arg>")
+      ("end,e",
+       po::value<int>(&end)->default_value(-1),
+       "Stop at scan number <arg>")
+      ("scanserver,S",
+       po::value<bool>(&scanserver)->default_value(false),
+       "Use the scanserver as an input method")
+      ("format,f",
+       po::value<IOType>(&iotype)->default_value(UOS),
+       "using shared library <arg> for input. (chose format from "
+       "[uos|uosr|uos_map|uos_rgb|uos_frames|uos_map_frames|old|rts|rts_map"
+       "|ifp|riegl_txt|riegl_rgb|riegl_bin|zahn|ply])")
+      ("max,M",
+       po::value<int>(&max_dist)->default_value(-1),
+       "neglegt all data points with a distance larger than <arg> 'units")
+      ("min,m",
+       po::value<int>(&min_dist)->default_value(-1),
+       "neglegt all data points with a distance smaller than <arg> 'units")
+      ("normal,g",
+       po::value<normal_method>(&ntype)->default_value(AKNN),
+       "normal calculation method "
+       "(KNN, ADAPTIVE_KNN, AKNN, ADAPTIVE_AKNN"
+#ifdef WITH_OPENCV
+       ", PANORAMA, PANORAMA_FAST"
+#endif
+       ")")
+      ("K1,k",
+       po::value<int>(&k1)->default_value(20),
+       "<arg> value of K value used in the nearest neighbor search of ANN or"
+       "kmin for k-adaptation")
+      ("K2,K",
+       po::value<int>(&k2)->default_value(20),
+       "<arg> value of Kmax for k-adaptation")
+#ifdef WITH_OPENCV
+      ("width,w",
+       po::value<int>(&width)->default_value(3600),
+       "width of panorama image")
+      ("height,h",
+       po::value<int>(&height)->default_value(1000),
+       "height of panorama image")
+#endif
+      ;
+
+  // input scan dir and output obj dir are hidden and mandatory
+  po::options_description hidden("Hidden options");
+  hidden.add_options()
+      ("input-dir", po::value<string>(&dir), "input dir")
+      ("output-dir", po::value<string>(&odir), "output dir");
+
+  po::positional_options_description pd;
+  pd.add("input-dir", 1);
+  pd.add("output-dir", 2);
+
+  po::options_description all;
+  all.add(cmd_options).add(hidden);
+
+  po::variables_map vmap;
+  po::store(po::command_line_parser(argc, argv).
+            options(all).positional(pd).run(), vmap);
+  po::notify(vmap);
+
+  if (vmap.count("help")) {
+    cout << cmd_options << endl << endl;
+    cout << "SAMPLE COMMAND FOR CALCULATING NORMALS" << endl;
+    cout << " bin/normals -s 0 -e 0 -f UOS -g AKNN -k 20 dat/ dat/test/output.obj" << endl;
+    cout << endl;
+    exit(-1);
+  }
+
+  // read scan path
+  if (dir[dir.length()-1] != '/') dir = dir + "/";
+
+}
+
 
 Poisson::Poisson() {
   initialize();
@@ -174,5 +366,6 @@ int Poisson::ready() {
 }
 
 int Poisson::apply() {
-  reconstructed = Execute2(params, vertices, normals, mesh, center, scale, cb);;
+  reconstructed = Execute2(params, vertices, normals, mesh, center, scale, cb);
+  return reconstructed;
 }
