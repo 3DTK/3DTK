320ae9d5b2b704e9e93c00bf902d92982d9ba82a
davidredondo <davidredondo@51f1e6e7-7289-49ad-8748-f09e43fc6daa>
2018-11-16 13:24:00 +0000

    Add graph_cut_segmentation
    git-svn-id: https://svn.code.sf.net/p/slam6d/code/trunk@1899 51f1e6e7-7289-49ad-8748-f09e43fc6daa

---
+++ src/segmentation/graph_cut/blob_color.cc
@@ -0,0 +1,141 @@
+
+#include "segmentation/graph_cut/blob_color.h"
+
+#include <fstream>
+
+void my_grid::put(const cv::Vec3f* v, int row, int col)
+{
+    grid[{row, col}] = v;
+    if(row < min_row) min_row = row;
+    if(row > max_row) max_row = row;
+    if(col < min_col) min_col = col;
+    if(col > max_col) max_col = col;
+}
+
+void my_grid::write_to_ppm(const std::string& path)
+{
+    std::ofstream picture(path);
+    picture << "P1\n" << (max_col-min_col)+1 << " " << (max_row-min_row)+1 << '\n';
+    for(int row = min_row; row <= max_row; ++row) {
+        for(int col = min_col; col <= max_col; ++col) {
+            int color = (grid[{row, col}] ? 1 : 0);
+            picture << color  << " ";
+        }
+        picture << '\n';
+    }
+    picture.close();
+    
+    
+}
+
+inline std::pair<int, int> grid_coordinates(const cv::Vec3d& p, const cv::Vec3d& pv1, const cv::Vec3d& pv2)
+{
+    /* Operations could be written manually if performance is a concern*/
+    return {int(p.dot(pv1)/(pv1.dot(pv1))), int(p.dot(pv2)/(pv2.dot(pv2)))};
+}
+
+
+
+
+
+std::vector<segment> segment_plane(const plane_candidate& plane, double cell_size)
+{   
+    
+    std::vector<cv::Vec3d> projected_points(plane.points.size());
+    /*Projects points onto the plane*/
+    auto project = [&](const cv::Vec3f* p){return cv::Vec3d{*p}- plane.normal*plane_point_distance(*p, plane.normal, plane.distance);};
+    std::transform(plane.points.begin(), plane.points.end(), projected_points.begin(), project);
+    my_grid grid;
+    /*get the vectors on the plane and scale them to the grid size*/
+    cv::Vec3d a = plane.normal*plane.distance - projected_points[0];
+    a = a * cell_size/ cv::norm(a);
+    cv::Vec3d b = a.cross(plane.normal); // b has now length grid_size, since <a,n = 90°
+    for(size_t i = 0; i < projected_points.size(); ++i) {
+        auto coordinates = grid_coordinates(projected_points[i] , a, b);
+        grid.put(plane.points[i], coordinates.first, coordinates.second);
+    }
+    std::map<std::pair<int, int>, int> region_map;
+    std::map<int, int> regions;
+    int next_region = 0;
+    int num_regions = 0;
+    for(int row = grid.min_row; row <= grid.max_row; ++row) {
+        for(int col = grid.min_col; col <= grid.max_col; ++col) {
+            const cv::Vec3f* xu  = grid.get(row-1, col);
+            const cv::Vec3f* xc  = grid.get(row, col);
+            const cv::Vec3f* xl  = grid.get(row, col-1);
+            if(xc){
+                if(!xl &&  !xu) {
+                    region_map[{row, col}] = next_region;
+                    regions[next_region] = next_region;
+                    ++next_region;
+                    ++num_regions;
+                } else if(!xl && xu) {
+                    region_map[{row, col}] = region_map[{row-1, col}];
+                } else if(xl && !xu) {
+                    region_map[{row, col}] = region_map[{row, col-1}];
+                } else {
+                    int l_region = regions[region_map[{row, col-1}]];
+                    int u_region = regions[region_map[{row-1, col}]];
+                    if(l_region != u_region) {
+                        --num_regions;
+                        if(l_region < u_region) std::swap(l_region, u_region);
+                        int tmp = l_region;
+                        region_map[{row, col-1}] = u_region;
+                        auto it = regions.begin();
+                        while(it != regions.end()) {
+                            if(it->second == tmp) regions[it->first] = regions[u_region];
+                            ++it;
+                        }
+                    }
+                    region_map[{row, col}] = u_region;
+                }
+            }
+        }
+    }
+    
+    std::map<int, int> indices;
+    int next_index = 0;
+    std::vector<segment> segments(num_regions, {{}, plane.normal, plane.distance});
+    /* Looping though all points is necessary because the the points in the grid represent not all points (
+     * if multiple points fall into the same cell) */
+    for(size_t i = 0; i < plane.points.size(); ++i){
+        int region = regions[region_map[grid_coordinates(projected_points[i], a, b)]];
+        if(indices.find(region) == indices.end()) {
+            indices[region] = next_index++;
+        }
+        segments[indices[region]].points.emplace_back(*plane.points[i]);
+        
+    }
+#ifndef NDEBUG
+    static int i=1;
+    grid.write_to_ppm("debug/grids/grid"+std::to_string(i)+".ppm");
+   /** std::ofstream r_pic("debug/gids/regions"+std::to_string(i)+".ppm");
+    r_pic << "P1\n" << (grid.max_col-grid.min_col)+1 << " " << (grid.max_row-grid.min_row)+1 << '\n';
+    for(int row = grid.min_row; row <= grid.max_row; ++row) {
+        for(int col = grid.min_col; col <= grid.max_col; ++col) {
+            int color = (region_map[{row, col}] ? 1 : 0);
+            r_pic << color  << " ";
+        }
+        r_pic << '\n';
+    }
+    r_pic.close();*/
+    std::ofstream picture("debug/grids/grid"+std::to_string(i)+"_labels.ppm", std::ios::binary);
+    picture << "P6\n" << (grid.max_col-grid.min_col)+1 << " " << (grid.max_row-grid.min_row)+1 << "\n255\n";
+    for(int row = grid.min_row; row <= grid.max_row; ++row) {
+        for(int col = grid.min_col; col <= grid.max_col; ++col) {
+            unsigned char c = 0xFF; std::array<unsigned char, 3> color{{c, c, c}};
+            if(grid.get(row, col)){
+                int region = regions[region_map[{row, col}]];
+                color =  get_color(indices[region]);
+            }
+            picture.write((char*)(&color[0]),3);
+            picture.flush();
+            
+        }
+    }
+    picture.close();
+    ++i;
+#endif
+    return segments;
+}
+
---
+++ src/segmentation/graph_cut/util.cc
@@ -0,0 +1,150 @@
+#include "segmentation/graph_cut/util.h"
+
+#include <newmat/newmatap.h> 
+
+double calc_plane(const std::set<std::pair<int, int>>& indizes, const cv::Mat& mat, double plane[4])
+{
+    using namespace NEWMAT;
+    SymmetricMatrix A(3);
+    A = 0;
+    int n;
+    n = indizes.size();
+    if(n < 3) return 0;
+    double cx, cy, cz;
+    cx = 0.0;
+    cy = 0.0;
+    cz = 0.0;
+
+    for (const auto& index : indizes) {
+        const cv::Vec3f& p = mat.at<cv::Vec3f>(index.first, index.second);
+        cx += p[0];
+        cy += p[1];
+        cz += p[2];
+    }
+    cx /= n;
+    cy /= n;
+    cz /= n;
+        
+   for (const auto& index : indizes) {
+        const cv::Vec3f& p = mat.at<cv::Vec3f>(index.first, index.second);
+        A(1, 1) += (p[0] - cx)*(p[0] - cx);
+        A(2, 2) += (p[1] - cy)*(p[1] - cy);
+        A(3, 3) += (p[2] - cz)*(p[2] - cz);
+        A(1, 2) += (p[0] - cx)*(p[1] - cy);
+        A(1, 3) += (p[0] - cx)*(p[2] - cz);
+        A(2, 3) += (p[1] - cy)*(p[2] - cz);
+    }
+
+    DiagonalMatrix D;
+    Matrix V;
+    try {
+        Jacobi(A,D,V);
+    } catch (ConvergenceException) {
+        cout << "couldn't find plane..." << endl;
+        return 0;
+    }
+    /*
+        cout << "A: "<< endl << A << endl;
+        cout << "D: "<< endl << D << endl;
+        cout << "V: "<< endl << V << endl;
+        */
+    int index;
+    D.MinimumAbsoluteValue1(index);
+        
+    plane[0] = V(1,index);
+    plane[1] = V(2,index);
+    plane[2] = V(3,index);
+    plane[3] = plane[0]*cx + plane[1]*cy + plane[2]*cz;
+
+    double sum = 0.0;
+    for(int i = 1; i < 4; i++) {
+        sum += D(i);
+    }
+    sum = D(index)/sum;
+    return sum;
+}
+
+double calc_plane(const std::vector<const cv::Vec3f*>& points, double plane[4])
+{
+    using namespace NEWMAT;
+    SymmetricMatrix A(3);
+    A = 0;
+    int n;
+    n = points.size();
+    if(n < 3) return 0;
+    double cx, cy, cz;
+    cx = 0.0;
+    cy = 0.0;
+    cz = 0.0;
+
+    for (const cv::Vec3f* p : points) {
+        cx += (*p)[0];
+        cy += (*p)[1];
+        cz += (*p)[2];
+    }
+    cx /= n;
+    cy /= n;
+    cz /= n;
+        
+   for (const cv::Vec3f* p : points) {
+        A(1, 1) += ((*p)[0] - cx)*((*p)[0] - cx);
+        A(2, 2) += ((*p)[1] - cy)*((*p)[1] - cy);
+        A(3, 3) += ((*p)[2] - cz)*((*p)[2] - cz);
+        A(1, 2) += ((*p)[0] - cx)*((*p)[1] - cy);
+        A(1, 3) += ((*p)[0] - cx)*((*p)[2] - cz);
+        A(2, 3) += ((*p)[1] - cy)*((*p)[2] - cz);
+    }
+
+    DiagonalMatrix D;
+    Matrix V;
+    try {
+        Jacobi(A,D,V);
+    } catch (ConvergenceException) {
+        cout << "couldn't find plane..." << endl;
+        return 0;
+    }
+    /*
+        cout << "A: "<< endl << A << endl;
+        cout << "D: "<< endl << D << endl;
+        cout << "V: "<< endl << V << endl;
+        */
+    int index;
+    D.MinimumAbsoluteValue1(index);
+        
+    plane[0] = V(1,index);
+    plane[1] = V(2,index);
+    plane[2] = V(3,index);
+    plane[3] = plane[0]*cx + plane[1]*cy + plane[2]*cz;
+
+    double sum = 0.0;
+    for(int i = 1; i < 4; i++) {
+        sum += D(i);
+    }
+    sum = D(index)/sum;
+    return sum;
+}
+
+std::array<unsigned char, 3> get_color(int index)
+{
+    constexpr int colors[] = {
+        0xFFFF00, 0x1CE6FF, 0xFF34FF, 0xFF4A46, 0x008941, 0x006FA6, 0xA30059,
+        0xFFDBE5, 0x7A4900, 0x0000A6, 0x63FFAC, 0xB79762, 0x004D43, 0x8FB0FF, 0x997D87,
+        0x5A0007, 0x809693, 0xFEFFE6, 0x1B4400, 0x4FC601, 0x3B5DFF, 0x4A3B53, 0xFF2F80,
+        0x61615A, 0xBA0900, 0x6B7900, 0x00C2A0, 0xFFAA92, 0xFF90C9, 0xB903AA, 0xD16100,
+        0xDDEFFF, 0x000035, 0x7B4F4B, 0xA1C299, 0x300018, 0x0AA6D8, 0x013349, 0x00846F,
+        0x372101, 0xFFB500, 0xC2FFED, 0xA079BF, 0xCC0744, 0xC0B9B2, 0xC2FF99, 0x001E09,
+        0x00489C, 0x6F0062, 0x0CBD66, 0xEEC3FF, 0x456D75, 0xB77B68, 0x7A87A1, 0x788D66,
+        0x885578, 0xFAD09F, 0xFF8A9A, 0xD157A0, 0xBEC459, 0x456648, 0x0086ED, 0x886F4C,
+        0x34362D, 0xB4A8BD, 0x00A6AA, 0x452C2C, 0x636375, 0xA3C8C9, 0xFF913F, 0x938A81,
+        0x575329, 0x00FECF, 0xB05B6F, 0x8CD0FF, 0x3B9700, 0x04F757, 0xC8A1A1, 0x1E6E00,
+        0x7900D7, 0xA77500, 0x6367A9, 0xA05837, 0x6B002C, 0x772600, 0xD790FF, 0x9B9700,
+        0x549E79, 0xFFF69F, 0x201625, 0x72418F, 0xBC23FF, 0x99ADC0, 0x3A2465, 0x922329,
+        0x5B4534, 0xFDE8DC, 0x404E55, 0x0089A3, 0xCB7E98, 0xA4E804, 0x324E72, 0x6A3A4C,
+        0x83AB58, 0x001C1E, 0xD1F7CE, 0x004B28, 0xC8D0F6, 0xA3A489, 0x806C66, 0x222800,
+        0xBF5650, 0xE83000, 0x66796D, 0xDA007C, 0xFF1A59, 0x8ADBB4, 0x1E0200, 0x5B4E51,
+        0xC895C5, 0x320033, 0xFF6832, 0x66E1D3, 0xCFCDAC, 0xD0AC94, 0x7ED379, 0x012C58};
+        const int color = colors[index % 127];
+        
+        return {{(unsigned char)(color>>16),  (unsigned char)(color>>8),  (unsigned char)(color)}};
+}
+
