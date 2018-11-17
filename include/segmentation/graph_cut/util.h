8c234834add0ee12a8ebd7dfd35b151235804560
davidredondo <davidredondo@51f1e6e7-7289-49ad-8748-f09e43fc6daa>
2018-11-17 09:24:27 +0000

    Rewrite write_* functions as they were to complicated and causing error; use ${Boost_Libraries} instead of Boost::program_options
    git-svn-id: svn+ssh://svn.code.sf.net/p/slam6d/code/trunk@1901 51f1e6e7-7289-49ad-8748-f09e43fc6daa

--- include/segmentation/graph_cut/util.h
+++ include/segmentation/graph_cut/util.h
@@ -1,6 +1,9 @@
 #ifndef __GRAPH_CUT_UTIL_H__
 #define __GRAPH_CUT_UTIL_H__
 
+#include "segmentation/graph_cut/david_graph.h"
+#include "segmentation/graph_cut/graph_cut.h"
+
 #include <opencv2/core/core.hpp>
 
 #include <array>
@@ -20,14 +23,6 @@
 #else
     #include <sys/stat.h>
 #endif
-
-struct identity {
-    template<typename T>
-    constexpr auto operator()(T&& v) const noexcept -> decltype(std::forward<T>(v))
-    {
-        return std::forward<T>(v);
-    }
-};
 
 template<typename T>
 void print_matrix_region(const cv::Mat& mat,int begin_row, int end_row, int begin_col, int end_col)
@@ -59,89 +54,12 @@ double plane_point_distance(const cv::Vec<T, 3>& x, const cv::Vec<U, 3>& n, doub
     return x[0]*n[0] + x[1]*n[1] + x[2]*n[2] - p;
 }
 
-/*The three following functions write_* are probably a bit overengineered*/
-template <typename PointStructureHolder, typename Header = const char* (*) (PointStructureHolder),
-            typename PointStructureAccessor = identity, typename PointAccessor = identity>
-void write_scan(const std::vector<PointStructureHolder>& scans, const std::string& path, size_t start_index=0,
-            Header head = [](const PointStructureHolder& scan){return "";}, PointAccessor&& get_point = identity(), 
-                PointStructureAccessor&& get_point_container = identity())
-{
-    mkdir(path.c_str(), S_IRWXU|S_IRWXG|S_IRWXO);
-    for(auto it = scans.cbegin(); it != scans.cend(); ++it) {
-        const auto& point_structure = get_point_container(*it);
-        std::ofstream file;
-        char numstr[6];
-        std::sprintf(numstr, "%03lu", start_index);
-        file.open(path + "/scan" + numstr + ".3d");
-        file << std::fixed << std::setprecision(4) << '#' << head(*it) << '\n';
-        std::ofstream pose_file(path + "/scan" + numstr + ".pose");
-        pose_file << "0 0 0\n0 0 0";
-        for(const auto& point_descriptor : point_structure) {
-            const auto& point = get_point(point_descriptor);
-            file << point[0] << ' ' << point[1] << ' ' << point[2] << '\n';
-        }
-        file.close();
-        ++start_index;
-    }
-}
+void write_segments_to_scans(const std::vector<segment>& segments, const std::string& path, size_t start_index);
 
+void write_segments_to_rgb_scan(const std::vector<segment>& segments, const std::string& path, size_t scan_num);
 
-template <typename PointStructureHolder, typename PointStructureAccessor = identity, typename PointAccessor = identity>
-void write_single_color_scan(const std::vector<PointStructureHolder>& scans, const std::string& path, size_t scan_num,
-            PointAccessor&& get_point = identity(), PointStructureAccessor&& get_point_container = identity())
-{
-    mkdir(path.c_str(), S_IRWXU|S_IRWXG|S_IRWXO);
-    char numstr[6];
-    std::sprintf(numstr, "%03lu", scan_num);
-    std::FILE* scan_file = std::fopen((path+"/scan" + numstr + ".3d").c_str(), "w");
-    std::ofstream pose_file(path + "/scan" + numstr + ".pose");
-    pose_file << "0 0 0\n0 0 0";
-    int counter = 0;
-    for(auto it = scans.cbegin(); it != scans.cend(); ++it) {
-        const auto& point_structure = get_point_container(*it);
-        std::array<unsigned char, 3> color = get_color(counter);
-        for(const auto& point_descriptor : point_structure) {
-            const auto& point = get_point(point_descriptor);
-            std::fprintf(scan_file, "%.4f %.4f %.4f %d %d %d\n", point[0], point[1], point[2], color[0], color[1], color[2]);
-        }
-        ++counter;
-    }
-    std::fclose(scan_file);
-}
-
-
-template <typename PointStructureHolder, typename PointStructureAccessor = identity, typename PointAccessor = identity>
-void write_components(const std::vector<PointStructureHolder>& components,  const std::string& path, size_t minimum_points,
-                      PointAccessor&& get_point = identity(),
-                      PointStructureAccessor&& get_point_container = identity())
-{
-    mkdir(path.c_str(), S_IRWXU|S_IRWXG|S_IRWXO);
-    std::ofstream(path + "/scan000.3d");
-    std::ofstream(path + "/scan000.pose");
-    size_t comp_num = 1;
-    for(const auto& component : components) {
-        const auto& point_structure = get_point_container(component);
-        size_t num_vertices = point_structure.size();
-        std::ofstream file;
-        if(num_vertices < minimum_points) {
-            file.open(path + "/scan000.3d", std::ofstream::app);
-        }
-        else {
-            char numstr[6];
-            std::sprintf(numstr, "%03lu", comp_num);
-            file.open(path + "/scan" + numstr + ".3d");
-            std::ofstream pose_file(path + "/scan" + numstr + ".pose");
-            pose_file << "0 0 0\n0 0 0";
-            ++comp_num;
-        }
-        file << std::fixed << std::setprecision(4);
-        for(const auto& point_descriptor : point_structure) {
-            const auto& point = get_point(point_descriptor);
-            file << point[0] << ' ' << point[1] << ' ' << point[2] << '\n';
-        }
-        file.close();
-    }
-}
+void write_components_to_scans(const cv::Mat& range_image, const std::vector<david_graph::grid_graph<double>>& components,
+                               const std::string& path, size_t minimum_points);
 
 template <typename T, int n>
 std::string cvVecnT_string(const cv::Vec<T, n>& v)
