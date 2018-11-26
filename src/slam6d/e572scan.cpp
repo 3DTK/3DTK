dcea4820c1cce7a8bbd7ce5b5fca348490203f90
jlucht <jlucht@51f1e6e7-7289-49ad-8748-f09e43fc6daa>
2018-11-26 09:45:18 +0000

    e572scan tool works fine now
    git-svn-id: svn+ssh://svn.code.sf.net/p/slam6d/code/trunk@1931 51f1e6e7-7289-49ad-8748-f09e43fc6daa

--- src/slam6d/e572scan.cpp
+++ src/slam6d/e572scan.cpp
@@ -21,7 +21,6 @@ int main(int argc, char *argv[]){
     int start = 0;
     int end = -1;
     double scale = 1.0;
-    int xDirection = 1;
 
     try {
         po::options_description generic("Generic options");
@@ -31,17 +30,15 @@ int main(int argc, char *argv[]){
         po::options_description output("Output options");
         output.add_options()
                 ("scale,m", po::value<double>(&scale)->default_value(1.0), "scale the point cloud")
-                ("invertX,x", "invert x-axes to convert from right to left handed system")
                 ("output,o", po::value<std::string>(&outputPath), "output directory");
         po::options_description input("Input options");
         output.add_options()
-                ("start,s", po::value<int>(&start)->default_value(1), "start scan")
+                ("start,s", po::value<int>(&start)->default_value(0), "start scan")
                 ("end,e", po::value<int>(&end)->default_value(-1), "end scan");
 
         po::options_description hidden("Hidden options");
         hidden.add_options()
                 ("input", po::value(&inputPath), "input .e57 file");
-        //TODO -s -e start, end scans
 
         // all options
         po::options_description all;
@@ -65,10 +62,6 @@ int main(int argc, char *argv[]){
             std::cout << "Helper tool to convert .e57 files to scan files\n"
                       << cmdline_options;
             exit(0);
-        }
-        if(vm.count("invertX")){
-            std::cout << "invert x axis." << std::endl;
-            xDirection = -1;
         }
         if(inputPath.length() == 0){
             std::cerr << "Missing input file!\n" << inputPath << std::endl;
@@ -100,24 +93,88 @@ int main(int argc, char *argv[]){
     return 0;
 };
 
+// transmat 3x3 rotation, x,y,z translation, mat output
+void setTransform(double *transmat, double *mat, double x, double y, double z) {
+    transmat[0] = mat[4];
+    transmat[1] = -mat[7];
+    transmat[2] = -mat[1];
+    transmat[3] = 0.0;
+
+    transmat[4] = -mat[5];
+    transmat[5] = mat[8];
+    transmat[6] = mat[2];
+    transmat[7] = 0.0;
+
+    transmat[8] = -mat[3];
+    transmat[9] = mat[6];
+    transmat[10] = mat[0];
+    transmat[11] = 0.0;
+
+    // translation
+    transmat[12] =  -y;
+    transmat[13] =  z;
+    transmat[14] =  x;
+    transmat[15] = 1;
+}
+
+void QuatToMatrix4RightHand(const double * qaut, const double * t, double * mat){
+    mat[0] = 1 - 2 * qaut[1] * qaut[1] - 2 * qaut[2] * qaut[2];
+    mat[1] = 2 * (qaut[0] * qaut[1] - qaut[3] * qaut[2]);
+    mat[2] = 2.0 * (qaut[0] * qaut[2] + qaut[3] * qaut[1]);
+    mat[3] = t[0];
+
+    mat[4] = 2 * (qaut[0] * qaut[1] + qaut[3] * qaut[2]);
+    mat[5] = 1.0 - 2.0 * qaut[0] * qaut[0] - 2.0 * qaut[2] * qaut[2];
+    mat[6] = 2.0 * (qaut[1] * qaut[2] - qaut[3] * qaut[0]);
+    mat[7] = t[1];
+
+    mat[8] = 2 * (qaut[0] * qaut[2] - qaut[3] * qaut[1]);
+    mat[9] = 2.0 * (qaut[1] * qaut[2] + qaut[3] * qaut[0]);
+    mat[10] = 1.0 - 2.0 * qaut[0] * qaut[0] - 2.0 * qaut[1] * qaut[1];
+    mat[11] = t[2];
+
+    mat[12] = 0;
+    mat[13] = 0;
+    mat[14] = 0;
+    mat[15] = 1;
+
+}
+
 bool writePose(double *translation, double *rotation, int scanid){
-    double mat [16];
-    QuatToMatrix4( rotation, translation, mat);
+    double inMatrix [16];
+    //TODO selbst bauen QuatToMatrix4( rotation, translation, inMatrix);
     double t [3];
     double r [3];
-    Matrix4ToEuler(mat, r, t);
+    double tMatrix[16];
+    double roation [9];
+
+    QuatToMatrix4RightHand( rotation, translation, inMatrix);
+
+    roation[0] = inMatrix[0];
+    roation[1] = inMatrix[1];
+    roation[2] = inMatrix[2];
+    roation[3] = inMatrix[4];
+    roation[4] = inMatrix[5];
+    roation[5] = inMatrix[6];
+    roation[6] = inMatrix[8];
+    roation[7] = inMatrix[9];
+    roation[8] = inMatrix[10];
+    setTransform(tMatrix, roation, translation[0], translation[1], translation[2]);
+    Matrix4ToEuler(tMatrix, r, t);
+
     std::stringstream ss;
     ss << std::setw(3) << std::setfill('0') << scanid;
     std::string filename = "scan" + ss.str() + ".pose";
     boost::filesystem::ofstream file (filename);
-    file << t[0] << " " << t[1] << " " << t[2] << "\n"
-            << r[0] << " " << r[1] << " " << r[2] << " ";
+    file << t[0] << " " << t[1] << " " << t[2] << "\n" << deg(r[0]) << " " << deg(r[1]) << " " << deg(r[2]) << " ";
+//    file << translation[0] << " " << translation[1] << " " << translation[2] << "\n" << deg(r[0]) << " " << deg(r[1]) << " " << deg(r[2]) << " ";
     file.close();
     return true;
 }
 
 void readPoints(const std::string inputPath, double scale, int start, int end){
     try {
+        //TODO check if RGB is available
         /// Read file from disk
         e57::ImageFile imf(inputPath, "r");
         e57::StructureNode root = imf.root();
@@ -159,8 +216,6 @@ void readPoints(const std::string inputPath, double scale, int start, int end){
             /// Get "points" field in scan.  Should be a CompressedVectorNode.
             e57::CompressedVectorNode points(scan.get("points"));
             std::cout << "point count: " <<points.childCount() << std::endl;
-            // Get pose
-            e57::StructureNode pose(scan.get("pose"));
 
             std::stringstream ss;
             ss << std::setw(3) << std::setfill('0') << scanIndex;
@@ -169,39 +224,52 @@ void readPoints(const std::string inputPath, double scale, int start, int end){
 
             double tr[3];
             double rq[4];
-            if(pose.isDefined("translation")){
-                e57::StructureNode translation (pose.get("translation"));
-                e57::FloatNode tx (translation.get("x"));
-                e57::FloatNode ty (translation.get("y"));
-                e57::FloatNode tz (translation.get("z"));
-                tr[0] = tx.value()*scale;
-                tr[1] = ty.value()*scale;
-                tr[2] = tz.value()*scale;
-                std::cout << "translation x:" << tr[0] << " y:" << tr[1] << " z:" << tr[2] << std::endl;
-            } else{
+            // Get pose
+            if(scan.isDefined("pose")){
+                e57::StructureNode pose(scan.get("pose"));
+                if(scan.isDefined("pose") && pose.isDefined("translation")){
+                    e57::StructureNode translation (pose.get("translation"));
+                    e57::FloatNode tx (translation.get("x"));
+                    e57::FloatNode ty (translation.get("y"));
+                    e57::FloatNode tz (translation.get("z"));
+                    tr[0] = tx.value()*scale;
+                    tr[1] = ty.value()*scale;
+                    tr[2] = tz.value()*scale;
+                    std::cout << "translation: x=" << tr[0] << " y=" << tr[1] << " z=" << tr[2] << std::endl;
+                } else{
+                    tr[0] = 0;
+                    tr[1] = 0;
+                    tr[2] = 0;
+                }
+                if(scan.isDefined("pose") && pose.isDefined("rotation")){
+                    e57::StructureNode rotation (pose.get("rotation"));
+                    e57::FloatNode rw (rotation.get("w"));
+                    e57::FloatNode rx (rotation.get("x"));
+                    e57::FloatNode ry (rotation.get("y"));
+                    e57::FloatNode rz (rotation.get("z"));
+                    rq[0] = rx.value();
+                    rq[1] = ry.value();
+                    rq[2] = rz.value();
+                    rq[3] = rw.value();
+                    std::cout << "rotation: w=" << rw.value() << " x=" << rx.value() << " y=" << ry.value() << " z=" << rz.value() << std::endl;
+                }else{
+                    rq[0] = 0;
+                    rq[1] = 0;
+                    rq[2] = 0;
+                    rq[3] = 0;
+                }
+            } else {
                 tr[0] = 0;
                 tr[1] = 0;
                 tr[2] = 0;
-            }
-            if(pose.isDefined("rotation")){
-                e57::StructureNode rotation (pose.get("rotation"));
-                e57::FloatNode rw (rotation.get("w"));
-                e57::FloatNode rx (rotation.get("x"));
-                e57::FloatNode ry (rotation.get("y"));
-                e57::FloatNode rz (rotation.get("z"));
-                rq[0] = rx.value();
-                rq[1] = ry.value();
-                rq[2] = rz.value();
-                rq[3] = rw.value();
-                std::cout << "rotation w:" << rw.value() << " x:" << rx.value() << " y:" << ry.value() << " z:" << rz.value() << std::endl;
-            }else{
                 rq[0] = 0;
                 rq[1] = 0;
                 rq[2] = 0;
                 rq[3] = 0;
             }
+
             if(writePose(tr, rq, scanIndex)){
-                std::cout << "write pose" << std::endl;
+                std::cout << "write pose..." << std::endl;
             }
 
 
@@ -229,8 +297,6 @@ void readPoints(const std::string inputPath, double scale, int start, int end){
                 for (unsigned i=0; i < gotCount; i++)
                     std::cout << "  " << i << ". columnIndex=" << columnIndex[i] << std::endl;
             }else if (proto.isDefined("sphericalRange")) {
-
-                //std::cout << "??? not implemented yet" << std::endl;
                 /// Make a list of buffers to receive the xyz values.
                 const int N = points.childCount();
                 std::vector<e57::SourceDestBuffer> destBuffers;
@@ -252,28 +318,26 @@ void readPoints(const std::string inputPath, double scale, int start, int end){
                 /// Each call to reader.read() will fill the xyz buffers until the points are exhausted.
                 e57::CompressedVectorReader reader = points.reader(destBuffers);
                 unsigned gotCount = reader.read();
-                //std::cout << "  got first " << gotCount << " points" << std::endl;
-                std::cout << "write scan" << std::endl;
+                std::cout << "write scan...0 % \r";
+                std::cout.flush();
 
-                /// Print the coordinates we got
+                /// convert the coordinates to cartesian and write to file
                 for (unsigned i=0; i < gotCount; i++) {
-//                    std::cout << "  " << i << ". range=" << range[i] << " elevation=" << elevation[i] << " azimuth="
-//                              << azimuth[i] << " r=" << r[i] << " g=" << g[i] << " b=" << b[i] << std::endl;
                     double cartesian [3];
                     double polar[3];
                     double rgb[3];
                     polar[2] = range[i]*scale;
                     polar[1] = azimuth[i];
                     polar[0] = elevation[i];
-                    // cartesian[elevation, azimuth, range]
-                    toCartesian2(polar, cartesian);
+                    toCartesianWithElevation(polar, cartesian);
                     rgb[0] = r[i];
                     rgb[1] = g[i];
                     rgb[2] = b[i];
-//                    std::cout << "\t" << cartesian[0] << " " << cartesian[1] << " " << cartesian[2] << std::endl;
-                    //TODO convert to left hand
 //                    file << cartesian[0] << " " << cartesian[1] << " " << cartesian[2] << " " << rgb[0] << " " << rgb[1] << " " << rgb[2] << "\n";
+                    /// convert to 3DTK coordinate system
                     file << (cartesian[1]*(-1)) << " " << cartesian[2] << " " << cartesian[0] << " " << rgb[0] << " " << rgb[1] << " " << rgb[2] << "\n";
+                    std::cout << "write scan..." << ((i+1)*100.0f/gotCount) << " % \r";
+                    std::cout.flush();
                 }
                 free(range);
                 free(elevation);
@@ -282,8 +346,8 @@ void readPoints(const std::string inputPath, double scale, int start, int end){
                 free(g);
                 free(b);
             } else
-                std::cout << "Error: couldn't find either Cartesian or spherical points in scan" << std::endl;
-            std::cout << std::endl;
+                std::cout << "Error: couldn't find either Cartesian or spherical points in scan! Other formats not implemented." << std::endl;
+            std::cout << std::endl << std::endl;
             file.close();
             if(end >= 0 && scanIndex == end) break;
         }
