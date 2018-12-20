cc05b3db63ba512e24d9cfe29c3a64f7ab02fae4
josch1337 <josch1337@51f1e6e7-7289-49ad-8748-f09e43fc6daa>
2018-12-20 18:32:15 +0000

    windows/timeout.py: add comment with rationale
    git-svn-id: https://svn.code.sf.net/p/slam6d/code/trunk@1995 51f1e6e7-7289-49ad-8748-f09e43fc6daa

--- windows/timeout.py
+++ windows/timeout.py
@@ -1,3 +1,20 @@
+#!/usr/bin/env python3
+
+# Run the build with our own timeout because the vcpkg cache is not saved if
+# we run into the appveyor timeout of one hour. Thus we abort after 80
+# minutes (4800 seconds). Ten minutes are left for the initial git clone plus
+# downloading/extracting/zipping/uploading of the cache.
+#
+# We use python to implement the process timeout - have fun trying to find
+# how to do it using Windows tools like cmd.exe or the powershell.
+#
+# With cmd.exe, the script is executed inside the same process and thus you
+# cannot kill it after a timeout.
+#
+# With powershell, Start-Job does not start the script in a way that directly
+# redirects its output to standard output but only allows to collect it in the
+# end.
+
 import subprocess
 import sys
 
