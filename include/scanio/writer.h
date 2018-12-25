bf73ff61f2cd9c36575455a36ee874e39ef33f14
Peter Janotta <pjanotta@users.sourceforge.net>
2018-12-25 19:05:23 +0000

    allow to abort point export externally
    git-svn-id: svn://svn.code.sf.net/p/slam6d/code/trunk@2016 51f1e6e7-7289-49ad-8748-f09e43fc6daa

--- include/scanio/writer.h
+++ include/scanio/writer.h
@@ -11,12 +11,12 @@ void write_uos(std::vector<cv::Vec4f> &points, std::string &dir, std::string id,
 void write_uosr(std::vector<cv::Vec4f> &points, std::string &dir, std::string id, bool high_precision=false);
 void write_uos_rgb(std::vector<cv::Vec4f> &points, std::vector<cv::Vec3b> &color, std::string &dir, std::string id, bool high_precision=false);
 
-void write_uos(DataXYZ &xyz, FILE *file, double scaleFac = 1.0, bool hexfloat = false, bool high_precision=false);
-void write_uosr(DataXYZ &xyz, DataReflectance &xyz_reflectance, FILE *file, double scaleFac = 1.0, bool hexfloat = false,bool high_precision=false);
-void write_uos_rgb(DataXYZ &xyz, DataRGB &rgb, FILE *file, double scaleFac = 1.0, bool hexfloat = false, bool high_precision=false);
-void write_xyz(DataXYZ &xyz, FILE *file, double scaleFac = 0.01, bool hexfloat = false, bool high_precision=false);
-void write_xyzr(DataXYZ &xyz, DataReflectance &xyz_reflectance, FILE *file, double scaleFac = 0.01, bool hexfloat = false, bool high_precision=false);
-void write_xyz_rgb(DataXYZ &xyz, DataRGB &rgb, FILE *file, double scaleFac = 0.01, bool hexfloat = false, bool high_precision=false);
+void write_uos(DataXYZ &xyz, FILE *file, double scaleFac = 1.0, bool hexfloat = false, bool high_precision=false, volatile bool *abort_flag = nullptr);
+void write_uosr(DataXYZ &xyz, DataReflectance &xyz_reflectance, FILE *file, double scaleFac = 1.0, bool hexfloat = false,bool high_precision=false, volatile bool *abort_flag = nullptr);
+void write_uos_rgb(DataXYZ &xyz, DataRGB &rgb, FILE *file, double scaleFac = 1.0, bool hexfloat = false, bool high_precision=false, volatile bool *abort_flag = nullptr);
+void write_xyz(DataXYZ &xyz, FILE *file, double scaleFac = 0.01, bool hexfloat = false, bool high_precision=false, volatile bool *abort_flag = nullptr);
+void write_xyzr(DataXYZ &xyz, DataReflectance &xyz_reflectance, FILE *file, double scaleFac = 0.01, bool hexfloat = false, bool high_precision=false, volatile bool *abort_flag = nullptr);
+void write_xyz_rgb(DataXYZ &xyz, DataRGB &rgb, FILE *file, double scaleFac = 0.01, bool hexfloat = false, bool high_precision=false, volatile bool *abort_flag = nullptr);
 void writeposefile(std::string &dir, const double* rPos, const double* rPosTheta, std::string id);
 void writeTrajectoryXYZ(std::ofstream &posesout, const double * transMat, bool mat, double scaleFac = 0.01);
 void writeTrajectoryUOS(std::ofstream &posesout, const double * transMat, bool mat, double scaleFac = 1.0);
