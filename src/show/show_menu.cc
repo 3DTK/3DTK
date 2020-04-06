3d103acd1e3485aac6f2c462d1e7f68d21e9fdd2
josch1337 <josch1337@51f1e6e7-7289-49ad-8748-f09e43fc6daa>
2020-04-06 07:56:42 +0000

    src/show/show_menu.cc: disable the right spinner depending on the viewmode argument
    git-svn-id: svn+ssh://svn.code.sf.net/p/slam6d/code/trunk@2210 51f1e6e7-7289-49ad-8748-f09e43fc6daa

--- src/show/show_menu.cc
+++ src/show/show_menu.cc
@@ -141,20 +141,27 @@ void newMenu()
   cangle_spinner->set_float_limits(1.0, 180.0);
   cangle_spinner->set_speed(20.0);
   cangle_spinner->set_alignment(GLUI_ALIGN_RIGHT);
+  if (showViewMode != 0) {
+    cangle_spinner->disable();
+  }
 
   pzoom_spinner = glui2->add_spinner_to_panel(settings_panel, "Parallel Zoom :", GLUI_SPINNER_FLOAT, &pzoom);
   pzoom_spinner->set_float_limits(0.001, 100000.0);
   pzoom_spinner->set_speed(5.0);
   pzoom_spinner->set_float_val(pzoom);
   pzoom_spinner->set_alignment(GLUI_ALIGN_RIGHT);
-  pzoom_spinner->disable();
+  if (showViewMode != 1) {
+    pzoom_spinner->disable();
+  }
 
   rzoom_spinner = glui2->add_spinner_to_panel(settings_panel, "Rotate Zoom :", GLUI_SPINNER_FLOAT, &rzoom);
   rzoom_spinner->set_float_limits(1.0, 100000.0);
   rzoom_spinner->set_speed(5.0);
   rzoom_spinner->set_float_val(rzoom);
   rzoom_spinner->set_alignment(GLUI_ALIGN_RIGHT);
-  rzoom_spinner->disable();
+  if (showViewMode != 2) {
+    rzoom_spinner->disable();
+  }
 
   glui2->add_column(true);
 
