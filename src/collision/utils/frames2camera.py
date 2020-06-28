fee4ab9867f27c5dffb94f11ce1effce8342314a
josch1337 <josch1337@51f1e6e7-7289-49ad-8748-f09e43fc6daa>
2020-06-28 21:08:33 +0000

    src/collision/utils/frames2camera.py: update to python3 and smooth trajectory using splines
    git-svn-id: svn+ssh://svn.code.sf.net/p/slam6d/code/trunk@2246 51f1e6e7-7289-49ad-8748-f09e43fc6daa

--- src/collision/utils/frames2camera.py
+++ src/collision/utils/frames2camera.py
@@ -1,37 +1,64 @@
-#!/usr/bin/env python
-from itertools import tee, izip
+#!/usr/bin/env python3
+from itertools import tee
 from math import sqrt
+import numpy
+from scipy import interpolate
+import sys
+
+segsize = float(sys.argv[1])
 
 def pairwise(iterable):
     a, b = tee(iterable)
-    #for i in range(100):
-    #    next(b, None)
     next(b, None)
-    return izip(a, b)
+    return zip(a, b)
 
-import sys
 
 poses = list()
+seen = set()
 
 for line in sys.stdin:
     if line.startswith('#'):
         continue
     x,y,z = line.split()[12:15]
-    poses.append((float(x),float(y),-float(z)))
+    val = (float(x),float(y),-float(z))
+    # when creating a spline there must be no duplicate values
+    if val in seen:
+        continue
+    poses.append(val)
+    seen.add(val)
 
-outdata = "0\n0\n0\n0\n0\n0\n0\n0\n0\n"
-outdata += "0\n0\n0\n0\n0\n0\n0\n0\n0\n"
-lines = 1
-for pos1,pos2 in pairwise(poses):
+length = 0
+for (x1,y1,z1),(x2,y2,z2) in pairwise(poses):
+    a = x2-x1
+    b = y2-y1
+    c = z2-z1
+    length += sqrt(a**2+b**2+c**2)
+
+if length < segsize:
+    print("length less then segsize: %f"%length)
+    exit(1)
+
+poses_x, poses_y, poses_z = zip(*poses)
+
+smoothing = 10
+subdiv = int(length/segsize)+1
+
+tck,u = interpolate.splprep([poses_x,poses_y,poses_z],s=smoothing)
+unew = numpy.linspace(0,1.0,subdiv)
+out = interpolate.splev(unew,tck)
+
+outdata = ""
+lines = 0
+for pos1,pos2 in pairwise(zip(*out)):
     length = sqrt((pos1[0]-pos2[0])*(pos1[0]-pos2[0])+(pos1[1]-pos2[1])*(pos1[1]-pos2[1])+(pos1[2]-pos2[2])*(pos1[2]-pos2[2]))
     if length == 0.0:
         continue
     lines += 1
     back = ((pos1[0]-pos2[0])/length,(pos1[1]-pos2[1])/length,(pos1[2]-pos2[2])/length)
     # FIXME: make the factor configurable
-    outdata += "%s\n%s\n%s\n"%(pos1[0]+8.0*back[0],pos1[1]+8.0*back[1]+4.0,pos1[2]+8.0*back[2])
+    outdata += "%s\n%s\n%s\n"%(pos1[0]+400*back[0],pos1[1]+400*back[1]+40,pos1[2]+400*back[2])
     outdata += "%s\n%s\n%s\n"%pos1
     outdata += "%s\n%s\n%s\n"%(pos1[0],pos1[1]+1,pos1[2])
 
-print lines
-print outdata,
+print(lines)
+print(outdata, end='')
