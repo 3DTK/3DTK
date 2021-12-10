58671eb995bcfcaaa51cfccd41d14ab99c0cb479
Fabian Arzberger <arzberger.fabi@web.de>
2021-12-10 08:39:58 +0000

    This time really, atomize.cc and atomize.h added, unfinished
    git-svn-id: svn://svn.code.sf.net/p/slam6d/code/trunk@2350 51f1e6e7-7289-49ad-8748-f09e43fc6daa

---
+++ include/slam6d/atomize.h
@@ -0,0 +1,154 @@
+#include <string>
+
+using std::string;
+
+#include <iostream>
+#include <fstream>
+#include <stdexcept>
+#include <boost/program_options.hpp>
+#include <boost/filesystem.hpp>
+
+#include "slam6d/scan.h"
+#include "globals.icc"
+
+namespace po = boost::program_options;
+
+// Enable bold characters on the outstream
+ostream& bold_on(ostream& os) { return os << "\e[1m"; }
+
+// Disable bold characters on the coutstream
+ostream& bold_off(ostream& os) { return os << "\e[0m"; }
+
+// Convert from string to IOType
+void validate(boost::any& v, const std::vector<std::string>& values,
+              IOType*, int) {
+  if (values.size() == 0)
+    throw std::runtime_error("Invalid model specification");
+  std::string arg = values.at(0);
+  try {
+    v = formatname_to_io_type(arg.c_str());
+  } catch (...) { // runtime_error
+    throw std::runtime_error("Format " + arg + " unknown.");
+  }
+}
+
+int parse_options(int argc, char **argv, std::string &cond_dir, std::string &orig_dir,
+            double &red, int &rand, int &start, int &end, int &maxDist, int &minDist, 
+            int &octree, IOType &type, std::string& customFilter, int &split)
+{
+po::options_description generic("Generic options");
+  generic.add_options()
+    ("help,h", "output this help message");
+
+  po::options_description input("Input options");
+  input.add_options()
+    ("format,f", po::value<IOType>(&type)->default_value(UOS, "uos"),
+     "using shared library <arg> for input. (chose F from {uos, uos_map, "
+     "uos_rgb, uos_frames, uos_map_frames, old, rts, rts_map, ifp, "
+     "riegl_txt, riegl_rgb, riegl_bin, zahn, ply, las})")
+    ("start,s", po::value<int>(&start)->default_value(0),
+     "start at scan <arg> (i.e., neglects the first <arg> scans) "
+     "[ATTENTION: counting naturally starts with 0]")
+    ("end,e", po::value<int>(&end)->default_value(-1),
+     "end after scan <arg>")
+    ("split,S", po::value<int>(&split)->default_value(end),
+    "Iterativley put <arg> scans together. If not used, condense will put all scans in one file.")
+    ("customFilter,u", po::value<string>(&customFilter),
+    "Apply a custom filter. Filter mode and data are specified as a semicolon-seperated string:"
+    "{filterMode};{nrOfParams}[;param1][;param2][...]\n"
+    "Multiple filters can be specified in a file (syntax in file is same as direct specification\n"
+    "FILE;{fileName}\n"
+    "See filter implementation in src/slam6d/pointfilter.cc for more detail.")
+    ("reduce,r", po::value<double>(&red)->default_value(-1.0),
+    "turns on octree based point reduction (voxel size=<NR>)")
+    ("octree,O", po::value<int>(&octree)->default_value(1),
+    "use randomized octree based point reduction (pts per voxel=<NR>)")
+    ("min,M", po::value<int>(&minDist)->default_value(-1),
+    "neglegt all data points with a distance smaller than NR 'units'")
+    ("max,m", po::value<int>(&maxDist)->default_value(-1),
+    "neglegt all data points with a distance larger than NR 'units'");
+
+  po::options_description hidden("Hidden options");
+  hidden.add_options()
+    ("condensed-dir", po::value<std::string>(&cond_dir), "condensed dir")
+    ("original-dir", po::value<std::string>(&orig_dir), "original dir");
+  // all options
+  po::options_description all;
+  all.add(generic).add(input).add(hidden);
+
+  // options visible with --help
+  po::options_description cmdline_options;
+  cmdline_options.add(generic).add(input);
+
+  // positional arguments
+  po::positional_options_description pd;
+  pd.add("condensed-dir", 1).add("original-dir", 1);
+
+  // process options
+  po::variables_map vm;
+  po::store(po::command_line_parser(argc, argv).
+            options(all).positional(pd).run(), vm);
+
+  // display help
+  if (vm.count("help")) {
+    std::cout << cmdline_options;
+    std::cout << std::endl
+         << "This programm is the opposing program to condense." << std::endl
+         << "Once you've condensed S subsequent scans into multiple MetaScans and matched them," << std::endl
+         << "each MetaScan has a .frames file. The purpose of this program is to split the" << std::endl
+         << ".frames files back and apply the relative transformation onto the corresponding single scans." << std::endl 
+         << bold_on << "Important: Use the same filters and reduction params as with condense!" << bold_off << std::endl
+         << "Example usage:" << std::endl
+         << "\tbin/atomize /condensed/dir /original/dir [params used for condense]" << std::endl
+         << "\tbin/atomize dat/test/cond dat/test -S 10 -s 200 -e 1000 --min 10 --max 100 -r 10 -O 1 " << std::endl;
+    exit(0);
+  }
+  po::notify(vm);
+
+#ifndef _MSC_VER
+  if (cond_dir[cond_dir.length()-1] != '/') cond_dir = cond_dir + "/";
+  if (orig_dir[orig_dir.length()-1] != '/') orig_dir = orig_dir + "/";
+#else
+  if (cond_dir[cond_dir.length()-1] != '\\') cond_dir = cond_dir + "\\";
+  if (orig_dir[orig_dir.length()-1] != '\\') orig_dir = orig_dir + "\\";
+#endif
+
+  return 0;
+}
+
+// Does what it says. outMat must be double[16]
+void readTransformFromPose(std::string dir, int fileCounter, double* outMat) {
+  // Setup filename and filestream
+  std::ifstream poseIn;
+  std::string poseFileName = dir + "scan" + to_string(fileCounter, 3) + ".pose";
+
+  // Read original transform from .pose file
+  poseIn.open(poseFileName);
+  double pose[6];
+  // read 6 plain doubles
+  for (int i = 0; i < 6; ++i) poseIn >> pose[i];
+  // convert angles from deg to rad
+  for (int i = 3; i < 6; ++i) pose[i] = rad(pose[i]);
+  
+  // Convert [x y z rotX rotY rotZ] to homogeneous transform matrix
+  double rPos[3] = { pose[0], pose[1], pose[2] };
+  double rPosTheta[3] = { pose[3], pose[4], pose[5] };
+  EulerToMatrix4(rPos, rPosTheta, outMat);
+}
+
+// Does what it says. outMat must be double[16]
+void readTransformFromFrames(std::string dir, int fileCounter, double* outMat) {
+  // Setup filename and filestream
+  std::ifstream frameIn;
+  std::string frameFileName = dir + "scan" + to_string(fileCounter, 3) + ".frames";
+
+  // Read transform from .frames file
+  int algoTypeDummy; // used as trash bin for last frames entry
+  while( frameIn.good() ) {
+    try {
+      frameIn >> outMat >> algoTypeDummy; 
+    } catch (const std::exception &e) {
+      break;
+    }
+  }
+}
---
+++ src/slam6d/atomize.cc
@@ -0,0 +1,167 @@
+/**
+ * @file
+ * @author Fabian Arzberger, JMU, Germany
+ * 
+ * atomize implementation
+ * 
+ * @brief
+ * This programm is the opposing program to condense.
+ * Once you've condensed S subsequent scans into multiple MetaScans and matched them,
+ * each MetaScan has a .frames file. The purpose of this program is to split the 
+ * .frames files back and apply them onto the corresponding single scans.
+ * 
+ */
+
+#include "slam6d/atomize.h"
+
+int main(int argc, char **argv)
+{
+    // init, default values if not specified
+    std::string cond_dir;
+    std::string orig_dir;
+    double red   = -1.0;
+    int    rand  = -1;
+    int    start = 0,   end = -1;
+    int    maxDist    = -1;
+    int    minDist    = -1;
+    int octree       = 0;  // employ randomized octree reduction?
+    IOType iotype    = UOS;
+    bool rangeFilterActive = false;
+    bool customFilterActive = false;
+    std::string customFilter;
+    int split = -1;
+
+    // parsing the command line parameters with boost::po, see header file
+    try {
+        parse_options(argc, argv, cond_dir, orig_dir, red, rand, start, end,
+            maxDist, minDist, octree, iotype, customFilter, split);
+    } catch (std::exception& e) {
+        std::cerr << "Error while parsing settings: " << e.what() << std::endl;
+        exit(1);
+    }
+    
+    // Read and set filter and reduction parameters
+    std::string red_string = red > 0 ? " reduced" : "";
+    rangeFilterActive = minDist > 0 || maxDist > 0;
+    if (rangeFilterActive) {
+        cout << "MinDist: " << minDist << ", MaxDist: " << maxDist << endl;
+    }
+    // custom filter set? quick check, needs to contain at least one ';'
+    // (proper chsecking will be done case specific in pointfilter.cc)
+    size_t pos = customFilter.find_first_of(";");
+    if (pos != std::string::npos){
+        customFilterActive = true;
+
+        // check if customFilter is specified in file
+        if (customFilter.find("FILE;") == 0){
+            std::string selection_file_name = customFilter.substr(5, customFilter.length());
+            std::ifstream selectionfile;
+            // open the input file
+            selectionfile.open(selection_file_name, std::ios::in);
+
+            if (!selectionfile.good()){
+                std::cerr << "Error loading custom filter file " << selection_file_name << "!" << std::endl;
+                std::cerr << "Data will NOT be filtered!" << std::endl;
+                customFilterActive = false;
+            }
+            else {
+                std::string line;
+                std::string custFilt;
+                while (std::getline(selectionfile, line)){
+                    if (line.find("#") == 0) continue;
+                    custFilt = custFilt.append(line);
+                    custFilt = custFilt.append("/");
+                }
+                if (custFilt.length() > 0) {
+                    // last '/'
+                    customFilter = custFilt.substr(0, custFilt.length() - 1);
+                }
+            }
+            selectionfile.close();
+        }
+    }
+    else {
+        // give a warning if custom filter has been inproperly specified
+        if (customFilter.length() > 0){
+            std::cerr << "Custom filter: specifying string has not been set properly, data will NOT be filtered." << std::endl;
+        }
+    }
+
+    // Open Scans from disk
+    Scan::openDirectory(false, orig_dir, iotype, start, end);
+    if(Scan::allScans.size() == 0) {
+        std::cerr << "No scans found. Did you use the correct format?" << std::endl;
+        exit(-1);
+    }
+
+    // Check which PointTypes the IOType supports 
+    unsigned int types = PointType::USE_NONE;
+    if(supportsReflectance(iotype)) types |= PointType::USE_REFLECTANCE;
+    if(supportsColor(iotype)) types |= PointType::USE_COLOR;
+    if(supportsType(iotype)) types |= PointType::USE_TYPE;
+
+    // If specified, filter scans
+    for (size_t i = 0; i < Scan::allScans.size(); i++)  {
+        if(rangeFilterActive) Scan::allScans[i]->setRangeFilter(maxDist, minDist);
+        if(customFilterActive) Scan::allScans[i]->setCustomFilter(customFilter);
+    }
+
+    // If specified, apply reduction
+    int end_reduction = (int)Scan::allScans.size();
+#ifdef _OPENMP
+    #pragma omp parallel for schedule(dynamic)
+#endif
+    for (int iterator = 0; iterator < end_reduction; iterator++) {
+        if (red > 0) {
+            PointType pointtype(types);
+            std::cout << "Reducing Scan No. " << iterator << std::endl;
+            Scan::allScans[iterator]->setReductionParameter(red, octree, pointtype);
+            Scan::allScans[iterator]->calcReducedPoints();
+        }
+    }
+
+    // Iterate in the same fashion as in condense.cc to reconstruct file association 
+    
+    int k = 0; // count subscans
+    uint seq = 0; // count condensed files
+    vector<Scan*> splitscans;
+    double transMat[16];
+    for (uint i = 0; i < Scan::allScans.size(); ++i)
+    {
+        Scan * source = Scan::allScans[i];
+        // In atomize, we need to push every scan, even if its empty
+        splitscans.push_back( source );
+        // In condense, empty scans got skipped 
+        if ( source->size<DataXYZ>("xyz") == 0 ) continue;
+        
+        // In condense, new files get opened here
+        // So in atomize, we have to read the .frames file of those 
+        if (0 == k++) {
+            readTransformFromFrames(cond_dir, seq, transMat);
+            cout << "FRAMES " << seq << endl;
+            for (int idx_mat = 0; idx_mat < 16; ++idx_mat)
+                cout << transMat[ idx_mat ] << " ";
+            cout << endl; 
+        }
+
+        // In condense, buffered scans get written to disk here
+        // So in atomize, we have to split the relative transformations on each original scan
+        if (k == split && split != -1) { 
+            for (int j = 0; j < splitscans.size(); ++j) {
+                const double* transMatOrig = splitscans[j]->get_transMatOrg();
+                //readTransformFromPose(orig_dir, j, transMatOrig);
+                cout << "POSE " << splitscans[j]->getIdentifier() << endl;
+                for (int idx_mat = 0; idx_mat < 16; ++idx_mat)
+                    cout << transMatOrig[ idx_mat ] << " ";
+                cout << endl; 
+            }
+
+
+            // prepare next iteration:
+            k = 0;
+            seq++;
+            splitscans.clear();
+        }
+    }
+    return 0;
+}
